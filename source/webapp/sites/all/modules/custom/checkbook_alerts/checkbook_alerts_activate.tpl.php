8bb701e990cf7cb1096e414528a996756f84b350
Alex Mach <alex.mach@gmail.com>
2013-08-28 14:28:18 -0400

    Fixing alerts.
    Signed-off-by: Alex Mach <alex.mach@gmail.com>

--- source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_activate.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_activate.tpl.php
@@ -1,14 +1,15 @@
 Please click the following link to activate your alert:
 <?php global $conf; ?>
-Alert description : <?=$alert->label ?>
-Alert frequency: <?=($alert->minimum_days==1?'Every Day':'Every '.$alert->minimum_days.' days') ?> 
-Number of new records that match alert criteria: <?=$alert->new_count ?>
-Link to Checkbook NYC alert results: <?=$alert->user_url ?>
-To activate to this alert click here: <?=$conf['check_book']['data_feeds']['site_url'].'alert/activate/'.$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient),array('absolute'=>true); ?>
+
+Thank you for creating an alert on checkbooknyc.com.
+The following alert has been created and will be sent to your email address:
+<?=$alertFrequency ?> - <?=$label ?>
+
+To complete the process, please click on the following link:
+
+<?=$conf['check_book']['data_feeds']['site_url'].'alert/activate/'.$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient),array('absolute'=>true); ?>
 
 
-To unsubscribe to this alert click here: <?=$conf['check_book']['data_feeds']['site_url'].'alert/unsubscribe/'.$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient),array('absolute'=>true); ?>
+(If link above is not active, please cut and paste full URL into your browser window.)
 
-To unsubscribe to all alerts sent to this email address click here: <?=$conf['check_book']['data_feeds']['site_url'].'alert/unsubscribe/'.md5($alert->recipient) ?>
-
-Office of the Comptroller - City of New York
+PLEASE DO NOT REPLY TO THIS MESSAGE!
--- source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_subscribe.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_subscribe.tpl.php
@@ -1,3 +1,10 @@
 <div id="dialog">
-You have been sent an email to confirm your alert.  Please follow the link in the email to activate your alert.
+<p>Thank you for your interest in checkbooknyc.com.  You will receive a confirmation email shortly.</p>
+<br />
+<p>NOTE: To complete the alert process, you must verify your email address by clicking on the link in the email, or cutting and pasting the full URL into your browser window.</p>
+<br />
+<p>You have created the following alert:<br />
+<?=$alertFrequency ?> - <?=$label ?>
+</p>
+
 </div>
