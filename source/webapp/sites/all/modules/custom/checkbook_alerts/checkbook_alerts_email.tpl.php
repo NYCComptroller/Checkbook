bac3f0f7582532e14a9df8e05951c8277c6aa844
Alex Mach <alex.mach@gmail.com>
2013-09-02 11:24:24 -0400

    Checking for slash at the end of the domain.

--- source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
@@ -5,7 +5,7 @@ Alert description : <?=$alert->label ?>
 Alert frequency: <?=($alert->minimum_days==1?'Every Day':'Every '.$alert->minimum_days.' days') ?> 
 Number of new records that match alert criteria: <?=$alert->new_count ?>
 Link to Checkbook NYC alert results: <?=$alert->user_url ?>
-To unsubscribe to this alert click here: <?=$conf['check_book']['data_feeds']['site_url'].'alert/unsubscribe/'.$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient); ?>
+To unsubscribe to this alert click here: <?=$conf['check_book']['data_feeds']['site_url'].(substr($conf['check_book']['data_feeds']['site_url'],-1)=="/"?'':"/").'alert/unsubscribe/'.$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient); ?>
 
 <?php endforeach; ?>
 To unsubscribe to all alerts sent to this email address click here: <?=$conf['check_book']['data_feeds']['site_url'].'alert/unsubscribe/'.md5($alert->recipient) ?>
