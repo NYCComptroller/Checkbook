34dc96ff562beee5b43291d7461d679ee1ba7401
rkalidindi <rkalidindi@reisys.com>
2013-09-03 11:05:55 -0400

    trends header changes

--- source/webapp/sites/all/modules/custom/checkbook_trends/templates/demographic_trends/emp_status_of_resident_population_csv.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_trends/templates/demographic_trends/emp_status_of_resident_population_csv.tpl.php
@@ -19,7 +19,7 @@
 */
 ?>
 <?php
-    $header = ",,,1997-2011,,,"."\n";
+    $header = ",,,1998-2012,,,"."\n";
     $header .= 'year';
 
     $header .=  ",New York City Employed - Civilian Labor Force (in thousands)" ;
