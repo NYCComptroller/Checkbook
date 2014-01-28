ad252f9b7041bfddfdb6df9bc177532807771c63
Ramaraju Kalidindi <rkalidindi@reisystems.com>
2014-01-28 12:14:03 -0500

    added - for zero value
    Former-commit-id: ca127ae979d0b9a4e77eb85ffb755e319c1c9e38 [formerly ca127ae979d0b9a4e77eb85ffb755e319c1c9e38 [formerly ca127ae979d0b9a4e77eb85ffb755e319c1c9e38 [formerly 9f15cf79b97ec1f4227074c16d13c6db23947965]]]
    Former-commit-id: 336e66b60f2217721ae7ae0b0f0e464a7c56f7a0
    Former-commit-id: bb975f3904c17c325957c670d4dc35990911a757
    Former-commit-id: 68c3e6c5bb2d0a39d4786d7d37eff2ad9b259caa

--- source/webapp/sites/all/modules/custom/checkbook_trends/templates/operational_trends/cap_assets_stats_by_program.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_trends/templates/operational_trends/cap_assets_stats_by_program.tpl.php
@@ -90,7 +90,6 @@ rsort($years);
                 
                 
                 $conditionCategory = $row['category'];
-                $hyphen = "-";
                 switch($conditionCategory){
                 	case "Correctional/Detention Centers<sup>(2)</sup><sup>(3)</sup>":
                 		$conditionCategory = "<div class='" . $cat_class . "'>Correctional/Detention<br><span style='padding-left:10px;'>Centers<sup>(2)</sup><sup>(3)</sup><span></div>";
@@ -109,11 +108,15 @@ rsort($years);
                 		break;
                 	default:
                 		$conditionCategory = "<div class='" . $cat_class . "' >" . $row['category'] . "</div>";
-                		$hyphen = "";
                 		break;
                 }
                 
 			    echo "<tr><td class='text'>" . /*$row['category']*/ $conditionCategory . "</td>";
+			    if(strpos($row['category'], ':')){
+			    	$hyphen = "";
+			    }else{
+			    	$hyphen = "-";
+			    }
 			    foreach ($years as $year)
 			        echo "<td><div>&nbsp;</div></td><td class='" . $amount_class . "'><div>" . (($row[$year]['amount'] > 0)?number_format($row[$year]['amount']):'&nbsp;' . $hyphen) . "</div></td>";			    
 			    echo "<td>&nbsp;</td>";
