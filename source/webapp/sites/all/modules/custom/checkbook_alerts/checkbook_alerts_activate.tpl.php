79c70a7e0523d6e24b1e168faee32069653ea473
Ramaraju Kalidindi <rkalidindi@reisystems.com>
2014-01-30 11:13:49 -0500

    Changes for merging CGI Code. bug fix for received data on pending contracts
    Former-commit-id: 555127678b85efb277253159a7e828d5d674addc [formerly 555127678b85efb277253159a7e828d5d674addc [formerly 555127678b85efb277253159a7e828d5d674addc [formerly 9bbfc2cdf80227603c01549d415ed2c6297b6b59]]]
    Former-commit-id: 2e5d35fc7909eb97432362be25e37f4e82ec303b
    Former-commit-id: bae3a2009d499b9046b01f7dd21053f8db163c3d
    Former-commit-id: 13c4a52e302bda435e8b12a95b73bce0365e4831

---
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_activate.tpl.php
@@ -0,0 +1,15 @@
+Please click the following link to activate your alert:
+<?php global $conf; ?>
+
+Thank you for creating an alert on checkbooknyc.com.
+The following alert has been created and will be sent to your email address:
+<?=$alertFrequency ?> - <?=$label ?>
+
+To complete the process, please click on the following link:
+
+<?=$conf['check_book']['data_feeds']['site_url'].(substr($conf['check_book']['data_feeds']['site_url'],-1)=="/"?'':"/").'alert/activate/'.$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient); ?>
+
+
+(If link above is not active, please cut and paste full URL into your browser window.)
+
+PLEASE DO NOT REPLY TO THIS MESSAGE!
---
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/script/sendAlerts.php
@@ -0,0 +1,62 @@
+<?php
+/**
+* This file is part of the Checkbook NYC financial transparency software.
+* 
+* Copyright (C) 2012, 2013 New York City
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/**
+ * Script to send notification emails
+ */
+
+_drush_bootstrap_drupal_full();
+
+$recipients = db_query("SELECT DISTINCT recipient FROM checkbook_alerts");
+
+foreach($recipients as $recipient){
+  $alerts=db_query(
+    _checkbook_alerts_getQuery().
+    "WHERE UPPER(recipient)=UPPER(:recipient) AND ".
+      "active = 'Y' AND ".
+      "date_end>CURRENT_TIMESTAMP AND ".
+      "DATE_ADD( date_last_new_results, INTERVAL minimum_days DAY) <= CURRENT_TIMESTAMP",
+        array(":recipient"=>$recipient->recipient));
+
+  $alertsToEmail=array();
+  foreach($alerts as $alert){
+    $_GET['refURL']=$alert->ref_url;
+
+    $count = _checkbook_alerts_get_data_count();
+    $newRecords = $count-$alert->number_of_results;
+
+    if($newRecords>$alert->minimum_results){
+      $alert->number_of_results=$count;
+      $alert->date_last_new_results=date("Y-m-d H:i:s");
+      drupal_write_record('checkbook_alerts',$alert,array('checkbook_alerts_sysid'));
+      $alert->new_count=$newRecords;      
+      if($alert->recipient_type=="EMAIL"){
+        $alertsToEmail[]=$alert;
+      }else if($alert->recipient_type=="TWITTER"){
+        _checkbook_alerts_process_twitter($alert);
+      }
+    }
+  }
+
+  if(count($alertsToEmail)>0){
+    _checkbook_alerts_process_email($alertsToEmail,md5($alertsToEmail[0]->recipient));
+  }
+}
