42ef88a636f4d08ae9bad1c59328ec684a1f0c5d
Ramaraju Kalidindi <rkalidindi@reisystems.com>
2014-03-11 16:50:36 -0400

    changes for merging alert module from sandbox branch. and adding apc caching module
    Former-commit-id: e2119a6e2616f643e70b1e31c14505bccce8ad6f [formerly e2119a6e2616f643e70b1e31c14505bccce8ad6f [formerly e2119a6e2616f643e70b1e31c14505bccce8ad6f [formerly ba6c164c4af54339f46f3643e3241e2030fafd63]]]
    Former-commit-id: e96873378cad827bdf5c88fe9944080808cf1f30
    Former-commit-id: d2b1bf42c0690b1ce6b47d4c4bc4d42c3e90bb8c
    Former-commit-id: c3dd7ef9aed00e70dc21193b7051dade7a05d2c0

---
+++ source/webapp/sites/all/modules/dashboard_platform/data_controller_cache/memcache/cache/handler/MemcacheHandler.php
@@ -0,0 +1,116 @@
+<?php
+/**
+*	GNU AFFERO GENERAL PUBLIC LICENSE 
+*	   Version 3, 19 November 2007
+* This software is licensed under the GNU AGPL Version 3
+* 	(see the file LICENSE for details)
+*/
+
+
+class MemcacheHandler extends AbstractSharedCacheHandler {
+
+    public static $CACHE__TYPE = 'Memcache';
+
+    public static $DEFAULT__COMPRESSION_THRESHOLD = 10240; // bytes
+    public static $DEFAULT__COMPRESSION_SAVINGS_MIN = 0.3; // 30% savings
+
+    /**
+     * @var Memcache | null
+     */
+    private $memcache = NULL;
+
+    protected function initialize($prefix, DataSourceMetaData $datasource) {
+        $result = TRUE;
+
+        if (class_exists('Memcache')) {
+            $this->memcache = new Memcache();
+
+            $successfulRegistrationCount = $unsuccessfulRegistrationCount = 0;
+
+            // adding servers
+            if (isset($datasource->host)) {
+                $serverResult = $this->registerServer($datasource->host, $datasource->port);
+                if ($serverResult) {
+                    $successfulRegistrationCount++;
+                }
+                else {
+                    $unsuccessfulRegistrationCount++;
+                }
+            }
+            if (isset($datasource->servers)) {
+                foreach ($datasource->servers as $server) {
+                    $serverResult = $this->registerServer($server->host, $server->port);
+                    if ($serverResult) {
+                        $successfulRegistrationCount++;
+                    }
+                    else {
+                        $unsuccessfulRegistrationCount++;
+                    }
+                }
+            }
+
+            if ($successfulRegistrationCount == 0) {
+                $this->memcache = NULL;
+            }
+            else {
+                $this->memcache->setCompressThreshold(self::$DEFAULT__COMPRESSION_THRESHOLD, self::$DEFAULT__COMPRESSION_SAVINGS_MIN);
+            }
+
+            if ($unsuccessfulRegistrationCount > 0) {
+                $result = FALSE;
+            }
+        }
+
+        return $result;
+    }
+
+    public function __destruct() {
+        if (isset($this->memcache)) {
+            $this->memcache->close();
+            $this->memcache = NULL;
+        }
+        parent::__destruct();
+    }
+
+    protected function registerServer($host, $port) {
+        $result = $this->memcache->addServer($host, $port);
+        if (!$result) {
+            LogHelper::log_error(t(
+                '[@cacheType] Could not add server (@host:@port)',
+                array(
+                    '@cacheType' => self::$CACHE__TYPE,
+                    '@host' => $host,
+                    '@port' => $port)));
+        }
+
+        return $result;
+    }
+
+    public function isAccessible() {
+        return parent::isAccessible() && isset($this->memcache);
+    }
+
+    protected function getInternalValue($entryName) {
+        return $this->memcache->get($entryName);
+    }
+
+    protected function getInternalValues($entryNames) {
+        return $this->memcache->get($entryNames);
+    }
+
+    protected function setInternalValue($entryName, $value, $expiration) {
+        return $this->memcache->set($entryName, $value, 0, $expiration);
+    }
+
+    protected function setInternalValues($values, $expiration) {
+        $errorEntryNames = array();
+
+        foreach ($values as $entryName => $value) {
+            if ($this->memcache->set($entryName, $value, 0, $expiration) === FALSE) {
+                $errorEntryNames[] = $entryName;
+            }
+        }
+
+        return $errorEntryNames;
+    }
+}
