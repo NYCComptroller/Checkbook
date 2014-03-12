de22fe9dcfbaddf3ac41197a84e2f367995a6fe6
Jeremy Styers <jeremy.styers@reisystems.com>
2014-03-12 11:12:19 -0400

    Fixed side facets becoming empty on user scroll
    Updated the inc file to use updated way of getting results instead of
    the old way.
    
    Also removed highlighting from the export widget sql

--- source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/budget.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/budget.tpl.php
@@ -24,11 +24,6 @@ $budget_parameter_mapping = _checkbook_smart_search_domain_fields('budget');
 $linkable_fields = array("agency_name" => "/budget/year/" . _getCurrentYearID() . "/yeartype/B/agency/".$budget_results["agency_id"],
                          "expenditure_object_name" => "/budget/year/". _getCurrentYearID() . "/yeartype/B/expcategory/".$budget_results["expenditure_object_id"],
                         );
-/*$highlighting_fields = array("agency_name" => "agency_name_text",
-                             "department_name" => "department_name_text",
-                             "expenditure_object_name" => "expenditure_object_name_text",
-                             "budget_code_name" => "budget_code_name_text"
-                            );*/
 
 $amount_fields = array("adopted_amount", "current_budget_amount", "total_expenditure","pre_encumbered_amount","encumbered_amount","accrued_expense_amount","cash_expense_amount","post_closing_adjustment_amount");
 
@@ -66,29 +61,6 @@ foreach ($budget_parameter_mapping as $key=>$title){
 
 }
 print theme('table',array('rows'=>$rows,'attributes'=>array('class'=>array('search-result-fields'))));
-
-/*print "<div class='search-result-fields'>";
-foreach($budget_results as $key => $value){
-    if(array_key_exists($key, $budget_parameter_mapping)){
-        print "<div class='search-result-row'>";
-        print "<div class='field-label'>". $budget_parameter_mapping[$key] . "</div>";
-
-        if($highlighting[$budget_results["id"]][$highlighting_fields[$key]]){
-            $value = $highlighting[$budget_results["id"]][$highlighting_fields[$key]][0];
-            $value = _checkbook_smart_search_str_html_entities($value);
-        }
-
-        if(in_array($key, $amount_fields)){
-            $value = custom_number_formatter_format($value, 2 , '$');
-        }else if(array_key_exists($key, $linkable_fields)){
-            $value = "<a href='" . $linkable_fields[$key]. "/year/" . $budget_results["fiscal_year_id"] . "'>". $value ."</a>";
-        }
-
-        print "<div class='field-content'>". $value . "</div>";
-        print "</div>";
-    }
-}
-print "</div>";*/
 
 
 
