035904b66b5493d455c3c6f4c9aec77e1b44df3b
Jeremy Styers <jeremy.styers@reisystems.com>
2014-03-24 17:09:02 -0400

    Fixed alert message not appearing correctly in windows

--- source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
@@ -1,31 +1,30 @@
-Below are the results of the triggered alert(s) from checkbooknyc.com:
-<?php global $conf; ?>
-<?php foreach($alerts as $alert): ?>
-Alert description : <?=$alert->label ?>
 
-Alert frequency: <?=($alert->minimum_days==1?'Every Day':'Every '.$alert->minimum_days.' days') ?>
+<?php
+global $conf;
+$message = "Below are the results of the triggered alert(s) from checkbooknyc.com:".PHP_EOL;
 
-Number of new records that match alert criteria: <?=$alert->new_count ?>
-
-Link to Checkbook NYC alert results: <?=$alert->user_url ?>
-
-To unsubscribe to this alert click here: <?=$conf['check_book']['data_feeds']['site_url'].(substr($conf['check_book']['data_feeds']['site_url'],-1)=="/"?'':"/").'alert/unsubscribe/'.$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient); ?>
-
-
-
-
-<?php endforeach; ?>
-
-To unsubscribe to all alerts sent to this email address click here: <?=$conf['check_book']['data_feeds']['site_url'].'/alert/unsubscribe/'.md5($alert->recipient) ?>
+foreach($alerts as $alert)
+{
+    $day = $alert-> minimum_days  == 1 ?  "Every Day" :  "Every " . $alert->minimum_days . " days";
+$message .= "Alert description : " . $alert->label . PHP_EOL;
+$message .= "Alert frequency: " . $day  . PHP_EOL;
+$message .= "Number of new records that match alert criteria: " . $alert->new_count. "
+Link to Checkbook NYC alert results: ". $alert->user_url. "
+To unsubscribe to this alert click here: " . $conf['check_book']['data_feeds']['site_url'].(substr($conf['check_book']['data_feeds']['site_url'],-1)=="/"?'':"/")."alert/unsubscribe/".$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient).
+    "
 
 
-
+    ";
+}
+$message .= "
+To unsubscribe to all alerts sent to this email address click here: " . $conf['check_book']['data_feeds']['site_url'].'/alert/unsubscribe/'.md5($alert->recipient)."
 
 
 
-Office of the Comptroller - City of New York
+Office of the Comptroller - City of New York";
 
 
+print($message);
 
 
 
