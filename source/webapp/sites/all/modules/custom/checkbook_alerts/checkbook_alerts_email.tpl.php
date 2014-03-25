ded018ebf448226643697a6ef507161921a8fdfc
Jeremy Styers <jeremy.styers@reisystems.com>
2014-03-25 14:51:51 -0400

    Made dem emails all purty lookin
    Former-commit-id: 0d746a98c5f756022daf7c7e0f7b9cbd47e5ef66 [formerly 0d746a98c5f756022daf7c7e0f7b9cbd47e5ef66 [formerly 0d746a98c5f756022daf7c7e0f7b9cbd47e5ef66 [formerly 96794592cc7351d79eb4238201549e0fab13fc5c]]]
    Former-commit-id: 848823864b68b4dd7604a24ee850df5d0f4df8d5
    Former-commit-id: 13b2a843110de045907915b2e9441ae9ba00aaa0
    Former-commit-id: 38674627719eb7aee2837103591a9ddb2f03d82a

--- source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_email.tpl.php
@@ -1,25 +1,31 @@
 
 <?php
 global $conf;
-$message = "Below are the results of the triggered alert(s) from checkbooknyc.com:".PHP_EOL;
+$message = "<b>Below are the results of the triggered alert(s) from checkbooknyc.com: </b><br><br>".PHP_EOL;
 
 foreach($alerts as $alert)
 {
     $day = $alert-> minimum_days  == 1 ?  "Every Day" :  "Every " . $alert->minimum_days . " days";
-$message .= "Alert description : " . $alert->label . PHP_EOL;
-$message .= "Alert frequency: " . $day  . PHP_EOL;
-$message .= "Number of new records that match alert criteria: " . $alert->new_count. "
-Link to Checkbook NYC alert results: ". $alert->user_url. "
+$message .= "Alert description : " . $alert->label . "<br>".PHP_EOL;
+$message .= "Alert frequency: " . $day  ."<br>". PHP_EOL;
+$message .= "Number of new records that match alert criteria: " . $alert->new_count. "<br>
+Link to Checkbook NYC alert results: ". $alert->user_url. "<br>
 To unsubscribe to this alert click here: " . $conf['check_book']['data_feeds']['site_url'].(substr($conf['check_book']['data_feeds']['site_url'],-1)=="/"?'':"/")."alert/unsubscribe/".$alert->checkbook_alerts_sysid.md5($alert->checkbook_alerts_sysid.$alert->label.$alert->recipient).
-    "
+    "<br><br><br>
 
 
-    ";
+
+
+
+";
 }
 $message .= "
 To unsubscribe to all alerts sent to this email address click here: " . $conf['check_book']['data_feeds']['site_url'].'/alert/unsubscribe/'.md5($alert->recipient)."
 
 
+
+
+<br><br><br>
 
 Office of the Comptroller - City of New York";
 
