92c88d4f02a5b29e977cc8a1fd197e0a86c2ef49
Saroja Gade <saru855@gmail.com>
2014-04-01 18:33:34 -0400

    Current amount sum
    Former-commit-id: db782e73c97b77a436c1e0384ddc60e2b616216c [formerly db782e73c97b77a436c1e0384ddc60e2b616216c [formerly db782e73c97b77a436c1e0384ddc60e2b616216c [formerly 34b8f88764e4a9b9c8cd8d3b561d5f80e80da509]]]
    Former-commit-id: 8d3b2d5f37bdc49758cab8370ea1c2725c2819eb
    Former-commit-id: f1e1525d0ecce50fd91a0129cd7c346208112008
    Former-commit-id: c6bd00b38123ebfd558f30acba5a7203f2241eed

--- source/webapp/sites/all/modules/custom/checkbook_project/gridview/contract_topamount_highchart.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/gridview/contract_topamount_highchart.tpl.php
@@ -42,7 +42,7 @@ include_once('export_link.php');
         if (isset($node->data) && is_array($node->data)) {
             foreach ($node->data as $datarow) {
                 $datarow['contract_number'] = _checkbook_check_isEDCPage() ? $datarow['contract_number_contract_number'] : $datarow['contract_number'];
-                $datarow['maximum_contract_amount'] = _checkbook_check_isEDCPage() ? $datarow['current_amount_sum'] : $datarow['current_amount_sum'];
+                $datarow['maximum_contract_amount'] = _checkbook_check_isEDCPage() ? $datarow['current_amount_sum'] : $datarow['maximum_contract_amount'];
                 $datarow['legal_name@checkbook:vendor'] = _checkbook_check_isEDCPage() ? $datarow['display_vendor_names'] : $datarow['legal_name@checkbook:vendor'];
                 $datarow['agency_name@checkbook:agency'] = _checkbook_check_isEDCPage() ? $datarow['display_agency_display_agency_agency_name'] : $datarow['agency_name@checkbook:agency'];
 
