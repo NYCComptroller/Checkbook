292b05bb89c7f078fa0573c66093ebee39988212
Alicia Torkelson <atorkelson@reisys.com>
2014-04-10 20:07:46 -0400

    Unit tested the advanced search fields for the enabling/disabling/reseting, ajax and toggling to fix all issues

--- source/webapp/sites/all/modules/custom/checkbook_advanced_search/js/checkbook_advanced_search_clear_button.js
+++ source/webapp/sites/all/modules/custom/checkbook_advanced_search/js/checkbook_advanced_search_clear_button.js
@@ -6,11 +6,6 @@
                 clearInputFields('#payroll-advanced-search','payroll');
                 e.preventDefault();
             });
-            $('#edit-spending-clear').click(function(e){
-                //$('#checkbook-advanced-search-form')[0].reset();
-                clearInputFields('#spending-advanced-search','spending');
-                e.preventDefault();
-            });
             $('#edit-budget-clear').click(function(e){
                 clearInputFields('#budget-advanced-search','budget');
                 e.preventDefault();
@@ -19,17 +14,56 @@
                 clearInputFields('#revenue-advanced-search','revenue');
                 e.preventDefault();
             });
-			$('#edit-contracts-clear').click(function(e){
-                clearInputFields('#contracts-advanced-search','contracts');
+            $('div.contracts-submit.checkbook-oge').find('input:submit[value="Clear All"]').click(function(e){
+                clearInputFieldByDataSource("#contracts-advanced-search",'contracts','checkbook_oge');
+                e.preventDefault();
+            });
+            $('div.contracts-submit.checkbook').find('input:submit[value="Clear All"]').click(function(e){
+                clearInputFieldByDataSource("#contracts-advanced-search",'contracts','checkbook');
+                e.preventDefault();
+            });
+            $('div.spending-submit.checkbook-oge').find('input:submit[value="Clear All"]').click(function(e){
+                clearInputFieldByDataSource("#spending-advanced-search",'spending','checkbook_oge');
+                e.preventDefault();
+            });
+            $('div.spending-submit.checkbook').find('input:submit[value="Clear All"]').click(function(e){
+                clearInputFieldByDataSource("#spending-advanced-search",'spending','checkbook');
                 e.preventDefault();
             });
         }
     }
 }(jQuery));
 
-/* Deprecated to pass data source optionally with default */
 function clearInputFields(enclosingDiv,domain){
-    clearInputFieldByDataSource(enclosingDiv,domain,'checkbook');
+    jQuery(enclosingDiv).find(':input').each(function() {
+        switch(this.type){
+            case 'select-one':
+                //jQuery('#edit-spending-fiscal-year').removeAttr("disabled");
+                var defaultoption = jQuery(this).attr('default_selected_value');
+                if(defaultoption == null)
+                    jQuery(this).find('option:first').attr("selected", "selected");
+                else
+                    jQuery(this).find('option[value=' + defaultoption + ']').attr("selected", "selected");
+                break;
+            case 'text':
+                jQuery(this).val('');
+                break;
+            case 'select-multiple':
+            case 'password':
+            case 'textarea':
+                jQuery(this).val('');
+                break;
+            case 'checkbox':
+            case 'radio':
+                switch(domain)
+                {
+                    case 'payroll':
+                        jQuery('#edit-payroll-amount-type-0').attr('checked','checked');
+                        break;
+                }
+                break;
+        }
+    })
 }
 
 function clearInputFieldByDataSource(enclosingDiv,domain,dataSource){
