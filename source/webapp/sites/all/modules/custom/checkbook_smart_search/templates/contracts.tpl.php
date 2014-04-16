9d3d14bc04bb84680008cafb3d66896f4014855d
Ramaraju Kalidindi <rkalidindi@reisystems.com>
2014-04-16 15:05:26 -0400

    changes for solar.
    Former-commit-id: bfcf1b08dd46f5cfc0540489b14a6d29997521dd [formerly bfcf1b08dd46f5cfc0540489b14a6d29997521dd [formerly bfcf1b08dd46f5cfc0540489b14a6d29997521dd [formerly 7f96c250468671136d7b232b0130a8a2b5ab7857]]]
    Former-commit-id: 3b2a2434d537f63ddaa96cd2bce097bd8d3b03d0
    Former-commit-id: 267f6b7bab7eca649fa6c2f8e366772bfd038ff1
    Former-commit-id: 13d1d8eeabe365d0700b2b0e5bf98f3ee198ba9a

--- source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/contracts.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/contracts.tpl.php
@@ -51,6 +51,8 @@ if(strtolower($contracts_results['contract_status']) == 'registered'){
         $contract_Id_link .= "/magid/".$contracts_results['original_agreement_id']."/doctype/".$contracts_results["document_code"];
     }else{
         $contract_Id_link .= "/agid/".$contracts_results['original_agreement_id']."/doctype/".$contracts_results["document_code"];
+        $master_contract_Id_link = $contract_Id_link . "/magid/".$contracts_results['master_agreement_id']."/doctype/".$contracts_results["document_code"];
+        
     }
     $contract_Id_link = ($IsOge) ? $contract_Id_link.'/datasource/checkbook_oge' : $contract_Id_link;
 
@@ -78,6 +80,7 @@ if(strtolower($contracts_results['contract_status']) == 'registered'){
             $contract_Id_link .= "/magid/".$contracts_results['original_agreement_id']."/doctype/".$contracts_results["document_code"];
         }else{
             $contract_Id_link .= "/agid/".$contracts_results['original_agreement_id']."/doctype/".$contracts_results["document_code"];
+            $master_contract_Id_link = $contract_Id_link . "/magid/".$contracts_results['master_agreement_id']."/doctype/".$contracts_results["document_code"];
         }
         $contracts_results['contract_number'] = "<a href='".$contract_Id_link ."'>".$contracts_results['contract_number']."</a>";
         $contracts_results['parent_contract_number'] = "<a href='". $contract_Id_link."'>".$contracts_results['parent_contract_number']."</a>";
