7797141192502497efc46c76580cadc2a6b615c8
Ramaraju Kalidindi <rkalidindi@reisystems.com>
2014-05-07 11:47:13 -0400

    special character issue with . in autocompletes
    Former-commit-id: 6944b1dd2e2ba98cca96a2bb4efd79996e1fea38 [formerly 6944b1dd2e2ba98cca96a2bb4efd79996e1fea38 [formerly 6944b1dd2e2ba98cca96a2bb4efd79996e1fea38 [formerly 5479fb39ba0654069641829db5fb2779a4ec38c1]]]
    Former-commit-id: d4eb490deb369f4e5d69355be21c01e7a278baa9
    Former-commit-id: 6942effa228767d5c22d1060876e3e6ad9ba0f61
    Former-commit-id: ae3ddb4b1a508ed54f642f0b370787151f031af9

--- source/webapp/sites/all/modules/custom/checkbook_faceted_search/js/narrow-down-filter.js
+++ source/webapp/sites/all/modules/custom/checkbook_faceted_search/js/narrow-down-filter.js
@@ -25,7 +25,8 @@ if (typeof Drupal != "undefined") {
 		    	            	return true;
 		    	            }
 		    	            request_term = replaceAllOccurrences('/','__',request_term);
-		    	            curl = '/faceted-search/ajax/autocomplete'  + curl + "/" + filter_column + "/" + encodeURIComponent(request_term) + "/xyz";
+		    	            request_term = replaceAllOccurrences('\\.','^^',request_term);
+		    	            curl = '/faceted-search/ajax/autocomplete'  + curl + "/" + filter_column + "/" + encodeURIComponent(request_term) ;
 
 		    	            var p = new RegExp('node\/[0-9]*');
 		    	            curl = curl.replace(p,'node/' + nid );
