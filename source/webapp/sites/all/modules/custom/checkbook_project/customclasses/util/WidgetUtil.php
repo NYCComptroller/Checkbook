09aa5c57175a2723630b69d9440f57f19844a99c
Alicia Torkelson <atorkelson@reisys.com>
2014-05-12 16:14:09 -0400

    NYCCHKBK-3571 - moved building of table to generic function, data remains the same and is now added to arrays instead of directly to html.

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/WidgetUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/WidgetUtil.php
@@ -179,4 +179,231 @@ class WidgetUtil
     	"tot_edc_con"=>"Total number of<br/>NYC EDC Contracts",
     	"vendor_address"=>"Vendor<br/>Address"
     );
+
+    //For number cols, need to find out if they are uniform number of digits for formatting
+    static function populateMaxColumnLength(&$rows) {
+
+        $number_rows = count($rows);
+        $number_columns = count($rows[0]['columns']);
+        $col_index = 0;
+
+        while ($col_index < $number_columns) {
+            $max_length = 0;
+            for ($row_index = 0; $row_index < $number_rows; $row_index++) {
+                if(isset($rows[$row_index]['columns'])) {
+                    $column_length = strlen($rows[$row_index]['columns'][$col_index]['value']);
+                    $max_length = $column_length > $max_length ? $column_length : $max_length;
+                }
+            }
+            for ($row_index = 0; $row_index < $number_rows; $row_index++)
+                if(isset($rows[$row_index]['columns']))
+                    $rows[$row_index]['columns'][$col_index]['max_column_length'] = $max_length;
+            $col_index++;
+        }
+    }
+
+    static function generateTable($table_definition, $is_main = true) {
+        $html = '';
+        $border = "";
+//        $border = 'border: 1px solid black !important; ';
+
+        $header_title = isset($table_definition["header"]["title"]) ? $table_definition["header"]["title"] : null;
+        $header_columns = isset($table_definition["header"]["columns"]) ? $table_definition["header"]["columns"] : null;
+        $table_body = isset($table_definition["body"]) ? $table_definition["body"] : null;
+        $table_rows = isset($table_definition["body"]["rows"]) ? $table_definition["body"]["rows"] : null;
+
+        //Title
+        if($header_title)
+            $html .= $header_title;
+
+        if($is_main)
+            $html .= "<table class='dataTable outerTable' style='border: 1px solid #CACACA;'>";
+        else
+            $html .= "<table class='sub-table dataTable'>";
+
+        //Header row
+        if($header_columns) {
+            $html .= "<thead>";
+            $html .= "<tr>";
+//            $html .= "<tr style='border: 1px solid black !important;'>";
+            $html .= WidgetUtil::generateHeaderColumns($header_columns);
+            $html .= "</tr>";
+            $html .= "</thead>";
+        }
+
+        //Table body
+        if(isset($table_body)) {
+            $html .= "<tbody>";
+            //Table rows
+            if(isset($table_rows)) {
+                $alternating_row_count = 0;
+                $inner_tbl_count = 0;
+                for ($row_index = 0; $row_index < count($table_rows); $row_index++) {
+
+                    if(isset($table_rows[$row_index]['columns'])) {
+                        if ($alternating_row_count % 2 == 0)
+                            $class = "even outer";
+                        else
+                            $class = "odd outer";
+                        $alternating_row_count++;
+                        WidgetUtil::populateMaxColumnLength($table_rows);
+//                        $html .= "<tr class='".$class."' style='border: 1px solid black !important;'>";
+                        $html .= "<tr class='".$class."'>";
+                        $html .= WidgetUtil::generateColumns($table_rows[$row_index]['columns']);
+                        $html .= "</tr>";
+                    }
+                    elseif(isset($table_rows[$row_index]['child_tables'])) {
+                        $col_span = count($table_rows[$row_index-1]['columns']);
+                        $html .= "<tr class='showHide'>";
+                        $html .= "<td colspan='".$col_span."'>";
+                        $html .= "<div>";
+                        for ($tbl_index = 0; $tbl_index < count($table_rows[$row_index]['child_tables']); $tbl_index++) {
+//                            $html .= "<tr class='showHide' style='border: 1px solid black !important;'>";
+                            $html .= WidgetUtil::generateTable($table_rows[$row_index]['child_tables'][$tbl_index]);
+                        }
+                        $html .= "</div>";
+                        $html .= "</td>";
+                        $html .= "</tr>";
+                    }
+                    elseif(isset($table_rows[$row_index]['inner_table'])) {
+                        $display = $inner_tbl_count > 0 ? "display: none;" : "";
+                        $inner_tbl_count++;
+                        $col_span = count($table_rows[$row_index-1]['columns']);
+//                        $html .= "<tr class='showHide' style='".$display." border: 1px solid black !important;'>";
+                        $html .= "<tr class='showHide' style='".$display."'>";
+                        $html .= "<td colspan='".$col_span."'>";
+                        $html .= "<div class='scroll'>";
+                        $html .= WidgetUtil::generateTable($table_rows[$row_index]['inner_table'],false);
+                        $html .= "</div>";
+                        $html .= "</td>";
+                        $html .= "</tr>";
+                    }
+                }
+            }
+            $html .= "</tbody>";
+        }
+        else {
+            $html .= "<tbody>";
+            $html .= "<tr class='odd'>";
+            $html .= "<td class='dataTables_empty' valign='top' colspan='3'>";
+            $html .= "<div id='no-records-datatable' class='clearfix'>";
+            $html .= "<span>No Matching Records Found</span>";
+            $html .= "</div>";
+            $html .= "</td>";
+            $html .= "</tr>";
+            $html .= "</tbody>";
+        }
+        $html .= "</table>";
+
+        return $html;
+    }
+
+    static function generateHeaderColumns($header_columns) {
+        $html = "";
+        $border = "";
+//        $border = 'border: 1px solid black !important; ';
+
+        foreach($header_columns as $index => $header_column) {
+
+            $value = $header_column['value'];
+            $type = $header_column['type'];
+            $first_column = $index == 0;
+
+            if($first_column) {
+                $html .= '<th style="'.$border.'text-align: left !important; vertical-align: middle;">';
+                $html .= '<span style="margin:8px 0px 8px 15px!important; display:inline-block; text-align: center !important;">'. $value .'</span></th>';
+            }
+            else {
+                if($type == 'number') {
+                    $html .= '<th style="'.$border.'text-align: center !important; vertical-align: middle; padding-right:6% !important">';
+                    $html .= '<span style="margin:8px 0px 8px 0px!important;display:inline-block; text-align: center !important;">'. $value .'</span></th>';
+                }
+                else {
+                    $html .= '<th style="'.$border.'text-align: left !important; vertical-align: middle; padding-left:10px !important">';
+                    $html .= '<span style="margin:8px 0px 8px 0px!important;display:inline-block; text-align: left !important;">'. $value .'</span></th>';
+                }
+            }
+        }
+        return $html;
+    }
+
+    static function generateColumns($columns) {
+
+        $html = "";
+        $border = "";
+//        $border = 'border: 1px solid black !important; ';
+
+        foreach($columns as $index => $column) {
+
+            $value = $column['value'];
+            $type = $column['type'];
+            $max_column_length = $column['max_column_length'];
+            $first_column = $index == 0;
+
+            if($first_column) {
+                $html .= '<td style="'.$border.'text-align: left !important; vertical-align: middle; padding: 10px 5px !important;">';
+                $html .= '<span style="margin:8px 0px 8px 15px!important; display:inline-block; text-align: left !important;">'. $value .'</span></td>';
+            }
+            else {
+                //Numbers are center aligned and padded to align right
+                if($type == 'number') {
+                    $column_length = strlen($value);
+                    if($column_length < $max_column_length) {
+                        $value = str_pad($value, $max_column_length, " ", STR_PAD_LEFT);
+                        $value = str_replace(" ", "&nbsp;", $value);
+                    }
+                    $html .= '<td style="'.$border.'text-align: center !important; vertical-align: middle; padding-right:6% !important">';
+                    $html .= '<span style="display:inline-block; text-align: right !important;">'. $value .'</span></td>';
+                }
+                else if($type == 'number_link') {
+                    $link_value = $column['link_value'];
+                    $actual_value = $value;
+                    $column_length = strlen($value);
+                    if($column_length < $max_column_length) {
+                        $value = str_pad($value, $max_column_length, " ", STR_PAD_LEFT);
+                        $value = str_replace(" ", "&nbsp;", $value);
+                        $value = str_replace($actual_value, $value, $link_value);
+                    } else {
+                        $value = $link_value;
+                    }
+                    $html .= '<td style="'.$border.'text-align: center !important; vertical-align: middle; padding-right:6% !important">';
+                    $html .= '<span style="display:inline-block; text-align: right !important;">'. $value .'</span></td>';
+                }
+                //Text is left aligned
+                else {
+                    $html .= '<td style="'.$border.'text-align: left !important; vertical-align: middle; padding-left:10px !important">';
+                    $html .= '<span style="display:inline-block; text-align: left !important;">'. $value .'</span></td>';
+                }
+            }
+        }
+        return $html;
+    }
+
+    static function generateLabelMappingNoDiv($labelAlias, $labelOnly = false){
+        $dynamic_labelAlias = array("current_modified","previous_modified","previous_1_modified","previous_2_modified",
+            "recognized_current","recognized_1","recognized_2","recognized_3");
+        if(in_array($labelAlias,$dynamic_labelAlias)){
+            $year = _getYearValueFromID(_getRequestParamValue('year'));
+            $dynamic_labels = array("current_modified" => "Modified<br/>".$year,
+                "previous_modified" => "Modified<br/>".($year-1),
+                "previous_1_modified" => "Modified<br/>".($year-2),
+                "previous_2_modified" => "Modified<br/>".($year-3),
+                "recognized_current" => "Recognized<br/>".$year,
+                "recognized_1" => "Recognized<br/>".($year+1),
+                "recognized_2" => "Recognized<br/>".($year+2),
+                "recognized_3" => "Recognized<br/>".($year+3));
+
+            $label = $dynamic_labels[$labelAlias];
+
+        }else{
+            $label = NULL;
+            if($labelOnly){
+                $label = self::getLabel($labelAlias);
+            }else{
+                $label  = self::$labels[$labelAlias];
+            }
+        }
+
+        return $label;
+    }
 }
--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contract/contracts_cta_spending_by_exp_cat.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contract/contracts_cta_spending_by_exp_cat.tpl.php
@@ -17,53 +17,46 @@
 * You should have received a copy of the GNU Affero General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-?>
-<h3>Spending by Expense Category</h3>
-  <?php
-  
-  if ( _getRequestParamValue("datasource") == "checkbook_oge") {
-  	$datasource ="/datasource/checkbook_oge";
-  }
-echo "<table class='dataTable spending-exp-cat outerTable'>";
-echo "<thead>
-        <tr>
-          <th class='text'>" . WidgetUtil::generateLabelMapping("expense_category") ."</th>
-          <th class='number'>" . WidgetUtil::generateLabelMapping("encumbered_amount") ."</th>
-          <th class='number endCol'>" . WidgetUtil::generateLabelMapping("spent_to_date") ."</th>
-        </tr>
-      </thead>
-      <tbody>";
+
+
+if ( _getRequestParamValue("datasource") == "checkbook_oge") {
+    $datasource ="/datasource/checkbook_oge";
+}
+
+//Main table header
+$tbl['header']['title'] = "<h3>Spending by Expense Category</h3>";
+$tbl['header']['columns'] = array(
+    array('value' => WidgetUtil::generateLabelMappingNoDiv("expense_category"), 'type' => 'text'),
+    array('value' => WidgetUtil::generateLabelMappingNoDiv("encumbered_amount"), 'type' => 'number'),
+    array('value' => WidgetUtil::generateLabelMappingNoDiv("spent_to_date"), 'type' => 'number')
+    );
+
 $count = 0;
 if (count($node->data) > 0) {
-  foreach ($node->data as $row) {
-    if ($count % 2 == 0) {
-      $class = "odd";
+    foreach ($node->data as $row) {
+
+        $spending_link = "/spending/transactions/expcategorycode/" . $row['expenditure_object_code'] . "/contnum/" .
+            $row['contract_number@checkbook:history_agreement/original_agreement_id@checkbook:aggregateon_contracts_expense']
+            . $datasource . "/newwindow";
+
+        if ($row['is_disbursements_exist'] == 'Y' && !preg_match("/newwindow/",current_path())) {
+            $spent_to_date_value = custom_number_formatter_format((isset($row['spending_amount_disb']))?$row['spending_amount_disb']:$row['spending_amount'], 2, '$');
+            $spent_to_date = "<a class=\"new_window\" href='" . $spending_link . "'>" . custom_number_formatter_format((isset($row['spending_amount_disb']))?$row['spending_amount_disb']:$row['spending_amount'], 2, '$') . "</a>";
+        }
+        else {
+            $spent_to_date_value = custom_number_formatter_format($row['spending_amount'], 2, '$');
+            $spent_to_date = custom_number_formatter_format($row['spending_amount'], 2, '$');
+        }
+        //Main table columns
+        $tbl['body']['rows'][$count]['columns'] = array(
+            array('value' => $row['expenditure_object_name'], 'type' => 'text'),
+            array('value' => custom_number_formatter_format($row['encumbered_amount'], 2, '$'), 'type' => 'number'),
+            array('value' => $spent_to_date_value, 'type' => 'number_link', 'link_value' => $spent_to_date)
+        );
+        $count += 1;
     }
-    else {
-      $class = "even";
-    }
-    $spending_link = "/spending/transactions/expcategorycode/" . $row['expenditure_object_code'] . "/contnum/" .
-      $row['contract_number@checkbook:history_agreement/original_agreement_id@checkbook:aggregateon_contracts_expense']
-      . $datasource . "/newwindow";
-    echo "<tr class='outer " . $class . "'>";
-    echo "<td class='text'><div>" . $row['expenditure_object_name'] . "</div></td>";
-    echo "<td class='number'><div>" . custom_number_formatter_format($row['encumbered_amount'], 2, '$') . "</div></td>";
-    if ($row['is_disbursements_exist'] == 'Y' && !preg_match("/newwindow/",current_path())) {
-      echo "<td class='number endCol'><div><a class=\"new_window\" href='" . $spending_link . "'>" . custom_number_formatter_format((isset($row['spending_amount_disb']))?$row['spending_amount_disb']:$row['spending_amount'], 2, '$') . "</a></div></td>";
-    }
-    else {
-      echo "<td class='number endCol'><div>" . custom_number_formatter_format($row['spending_amount'], 2, '$') . "</div></td>";
-    }
-    echo "</tr>";
-    $count += 1;
-  }
 }
-else {
-  echo "<tr class='odd'>";
-  echo '<td class="dataTables_empty" valign="top" colspan="3">' .
-           '<div id="no-records-datatable" class="clearfix">
-                 <span>No Matching Records Found</span>
-           </div>' . '</td>';
-  echo "</tr>";
-}
-echo "</tbody></table>";
+
+$html = WidgetUtil::generateTable($tbl);
+echo $html;
+?>
