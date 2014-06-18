c65d510c33c92fde1fa10abbca6f0b2269d90bfb
sgade <sgade@reisystems.com>
2014-06-18 11:30:51 -0400

    Merging EDC changes.
    Former-commit-id: 676254a5409a2d9f511252ff9cdeaa803c53f163 [formerly 676254a5409a2d9f511252ff9cdeaa803c53f163 [formerly 676254a5409a2d9f511252ff9cdeaa803c53f163 [formerly 26045ff218f0ccff04f39cddcd3edb0a036cad4b]]]
    Former-commit-id: 02e6a007b9fd72f85c85a46f3811b49b32857adb
    Former-commit-id: 3ebb33ec3f55e4e4f99dcc0db2a22fa5bc8df193
    Former-commit-id: ef98d423fa2191d7ae327979eeffa91bce20f455

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/agencies_list.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/agencies_list.tpl.php
@@ -1,33 +1,47 @@
 <?php
 /**
-* This file is part of the Checkbook NYC financial transparency software.
-* 
-* Copyright (C) 2012, 2013 New York City
-* 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-* 
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of the Checkbook NYC financial transparency software.
+ *
+ * Copyright (C) 2012, 2013 New York City
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 ?>
 <?php
+
+$city_agencies = array();
+$edc_agencies = array();
+foreach($node->data as $key=>$value){
+    if(array_key_exists('is_oge_agency', $value)){
+        $edc_agencies[$key] = $value;
+    }else{
+        $city_agencies[$key] = $value;
+    }
+}
+
+$oge_filter_highlight = (_checkbook_check_isEDCPage()) ? 'agency_filter_highlight' : '';
+$city_filter_highlight = (!_checkbook_check_isEDCPage()) ? 'agency_filter_highlight' : '';
+
 $current_fy_year = _getFiscalYearID();
 $current_cal_year = _getCalendarYearID();
 
 $current_url = explode('/',$_SERVER['REQUEST_URI']);
 $url = $current_url[1];
 if($current_url[1] == 'contracts_landing' || $current_url[1] == 'contracts_revenue_landing' || $current_url[1] == 'contracts' ||
-   $current_url[1] == 'contracts_pending_exp_landing' || $current_url[1] == 'contracts_pending_rev_landing'){
+    $current_url[1] == 'contracts_pending_exp_landing' || $current_url[1] == 'contracts_pending_rev_landing'){
 
-   $all_agency_url = $url = 'contracts_landing/status/A/yeartype/B/year/'.$current_fy_year;
+    $all_agency_url = $url = 'contracts_landing/status/A/yeartype/B/year/'.$current_fy_year;
 }else if($current_url[1] == 'payroll'){
     $all_agency_url = $url = 'payroll/yeartype/B/year/'.$current_fy_year;
 }else if($current_url[1] == 'budget'){
@@ -41,16 +55,16 @@ if($current_url[1] == 'contracts_landing' || $current_url[1] == 'contracts_reven
 
 $selected_text = 'Citywide Agencies';
 
-foreach($node->data as $key => $value){
-	if($value['agency_id'] == $agency_id_value){
-		$selected_text = $value['agency_name'];
-	}
+foreach($city_agencies as $key => $value){
+    if($value['agency_id'] == $agency_id_value){
+        $selected_text = $value['agency_name'];
+    }
 }
 
-$agencies = array_chunk($node->data, 10);
+$agencies = array_chunk($city_agencies, 10);
 
 $agency_list = "<div id='agency-list' class='agency-nav-dropdowns'>";
-$agency_list .= "<div class='agency-list-open'><span id='all-agency-list-open'>$selected_text</span></div>";
+$agency_list .= "<div class='agency-list-open'><span id='all-agency-list-open' class='".$city_filter_highlight."'>$selected_text</span></div>";
 $agency_list .= "<div class='agency-list-content all-agency-list-content'>";
 $agency_list .= "<div class='listContainer1' id='allAgenciesList'>";
 
@@ -60,8 +74,8 @@ foreach($agencies as $key => $agencies_chunck){
     foreach($agencies_chunck as $a => $agency){
         $agency_url ="";
         $agency_url = ($current_url[1] == 'payroll')?'payroll/agency/'.$agency['agency_id'].'/yeartype/B/year/'.$current_fy_year
-                                              : $url.'/agency/'.$agency['agency_id'];
-        
+            : $url.'/agency/'.$agency['agency_id'];
+
         $agency_list .= "<li id=agency-list-id-".$agency['agency_id'].">
                             <a href='/".$agency_url. "'>".$agency['agency_name']."</a>
                         </li>";
@@ -77,19 +91,26 @@ $agency_list .= "<a href='/".$all_agency_url."' id='citywide_all_agencies'>CITYW
 $agency_list .= "<div class='agency-list-close'><a href='#'>x Close</a></div>";
 $agency_list .= "</div></div>";
 
-$agency_list_other = 
-"<div id='agency-list' class='agency-nav-dropdowns'>
-  <div class='agency-list-open'><span id='other-agency-list-open'>Featured</span></div>
+//$edc_agencies
+if($current_url[1] == 'contracts_landing')
+    $edc_url = "contracts_landing/status/A";
+else
+    $edc_url = "spending_landing";
+
+$agency_list_other = "<div id='agency-list-other' class='agency-nav-dropdowns'>
+  <div class='agency-list-open'><span id='other-agency-list-open' class='".$oge_filter_highlight."'>Other Government Entities</span></div>
   <div class='agency-list-content other-agency-list-content'>
     <div class='listContainer1' id='otherAgenciesList'>
         <div class='agency-slide'>
-          <ul class='listCol'>
-            <li><a href='/".RequestUtil::getEDCURL(). "'>New York City Economic Development Corporation</a></li>
-          </ul>
+          <ul class='listCol'>";
+foreach($edc_agencies as $key => $edc_agency){
+    $agency_list_other .= "<li><a href='/". $edc_url .'/yeartype/B/year/'.$current_fy_year."/datasource/checkbook_oge/agency/".$edc_agency['agency_id']. "'>". $edc_agency['agency_name'] ."</a></li>";
+}
+$agency_list_other .= "</ul>
         </div>
-    </div>        
+    </div>
         <div class='agency-list-nav'><a href='#' id='prev2'>Prev</a><a href='#' id='next2'>Next</a>
-        <a href='/".RequestUtil::getSpendingEDCURL()."' id='citywide_all_agencies'>FEATURED</a>
+        <a href='/spending_landing" . '/yeartype/B/year/'.$current_fy_year ."/datasource/checkbook_oge/agency/9000"."' id='citywide_all_agencies'>OTHER GOVERNMENT ENTITIES</a>
         </div>
     <div class='agency-list-close'><a href='#'>x Close</a></div>
   </div>
