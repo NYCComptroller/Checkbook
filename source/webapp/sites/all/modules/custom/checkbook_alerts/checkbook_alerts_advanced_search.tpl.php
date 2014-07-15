f6fdca1208da6faf795e1de50922cec7885191aa
Alicia Torkelson <atorkelson@reisys.com>
2014-07-15 16:24:23 -0400

    NYCCHKBK-3881 - Fixed conflict with old create alerts functionality
    Former-commit-id: 52ff2882cdfd348641d23aef8f7442f24e71afce [formerly 52ff2882cdfd348641d23aef8f7442f24e71afce [formerly 52ff2882cdfd348641d23aef8f7442f24e71afce [formerly a37495c855b6ebb15782f4f3a3a50e5827e92840]]]
    Former-commit-id: 88446de828fa92090b9756d8e61b8236ac85f5e6
    Former-commit-id: 405dc39679751bd7508d063a0d4fcdd3efabeaa4
    Former-commit-id: 27c4f3844c792081460c7a29a7b4e16cc1ea1e13

--- source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_advanced_search.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_alerts/checkbook_alerts_advanced_search.tpl.php
@@ -1,3 +1,48 @@
-<div id="dialog" style="text-align: center">
-    <p><strong>Thank you for Scheduling your Alert!<br/>You will receive a confirmation email soon.</strong></p>
+<div id="dialog">
+    <div id='errorMessages'></div>
+    <table>
+        <tr>
+            <th>
+                <span class="bold">Alert Settings</span>
+            </th>
+        </tr>
+        <tr>
+            <td><span class='alert-required-field' style='color:red;'>*</span>Description:</td>
+            <td>
+                <input type='text' name='alert_label' size="25" />
+                <div class="description">This is how the alert will be described in the email text.</div>
+            </td>
+        </tr>
+        <tr>
+            <td><span class='alert-required-field' style='color:red;'>*</span></span>Email:</td>
+            <td>
+                <input type='text' name='alert_email' size="50" />
+            </td>
+        </tr>
+        <tr>
+            <td>Minimum Additional Results:</td>
+            <td>
+                <input type='text' name='alert_minimum_results' value='10' size="5" maxlength="5" />
+                <div class="description">Checkbook will not notify you until this many new results are returned.</div>
+            </td>
+        </tr>
+        <tr>
+            <td>Alert Frequency:</td>
+            <td>
+                <select name='alert_minimum_days'>
+                    <option value="1">Daily</option>
+                    <option value="7">Weekly</option>
+                    <option value="30">Monthy</option>
+                    <option value="92" default>Quarterly</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Expiration Date:</td>
+            <td>
+                <input type='text' name='alert_end[date]' size="30" maxlength="30">
+                <div class="description">This is the date the alert will expire.  The default is one year.</div>
+            </td>
+        </tr>
+    </table>
 </div>
