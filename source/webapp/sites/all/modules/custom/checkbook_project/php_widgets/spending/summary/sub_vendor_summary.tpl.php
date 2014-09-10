853bd1d6c9c10656fcc6bdd3a928ca6fce827c16
Alicia Torkelson <atorkelson@reisys.com>
2014-09-10 18:21:19 -0400

    NYCCHKBK-4163 - Sub Vendors Spending - Top 5 Prime vendors - YTD Link - Properly populated the prime vendor name.
    Former-commit-id: c48649a88a82806bc0432d84c3543d63bdd13ac7 [formerly c48649a88a82806bc0432d84c3543d63bdd13ac7 [formerly c48649a88a82806bc0432d84c3543d63bdd13ac7 [formerly 6beac504c766fe7630f4667c2e501cdce7926c1a]]]
    Former-commit-id: 09f70c993668777662715ecabe13c3fa7b119162
    Former-commit-id: 9b4675502257876b6dc39d53558245b84ea10361
    Former-commit-id: 6a65e1540370e864e6baefb42066587e0b11ef11

---
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/spending/summary/sub_vendor_summary.tpl.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* This file is part of the Checkbook NYC financial transparency software.
+* 
+* Copyright (C) 2012, 2013 New York City
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
+<?php
+
+$records = $node->data;
+if(is_array($records)){
+    $row = $records[0];
+    $title = eval($node->widgetConfig->summaryView->templateTitleEval);
+    $vendor_name = WidgetUtil::getLabel("sub_vendor_name");
+    $ytdspending = WidgetUtil::getLabel("ytd_spending");
+    $totcontamnt = WidgetUtil::getLabel("total_contract_amount");
+    
+$summaryContent =  <<<EOD
+<div class="contract-details-heading">
+	<div class="contract-id">
+		<h2 class="contract-title">{$title}</h2>
+		<div class="spending-tx-subtitle">{$vendor_name}: {$row['sub_vendor_sub_vendor_legal_name']}</div>
+	</div>
+	<div class="dollar-amounts">
+        <div class="total-spending-contract-amount">
+            {$row['formatted_total_contract_amount_sum']}
+            <div class="amount-title">{$totcontamnt}</div>
+        </div>
+        <div class="ytd-spending-amount">
+            {$row['formatted_check_amount_sum']}
+            <div class="amount-title">{$ytdspending}</div>
+        </div>
+    </div>
+</div>
+EOD;
+
+    print $summaryContent;
+}
