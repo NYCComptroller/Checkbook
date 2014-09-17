d94ada2bfa26a23f56b4b91874c6b4b27be30f76
Ramaraju Kalidindi <rkalidindi@reisystems.com>
2014-09-16 20:16:30 -0400

    NYCCHKBK-4136.. updates for agency grading page from mwbe current site. mwbe changes for calculating mwbe share based on sub vendor data
    Former-commit-id: f4517f0e019ba53c7cbdcb6d7890e31edeabd313 [formerly f4517f0e019ba53c7cbdcb6d7890e31edeabd313 [formerly f4517f0e019ba53c7cbdcb6d7890e31edeabd313 [formerly 166024bcf9639cf7a95d370efc6f1b9813b44999]]]
    Former-commit-id: 9d3f3ba36a07a1f5342f196612b5d907076acd33
    Former-commit-id: 74b4666fe7a1f229b94a8d17ea0958edb9971bd1
    Former-commit-id: 6eef6b3437e0e40f7f8cb8907facf6cbaf3905e2

--- source/webapp/sites/all/modules/custom/checkbook_mwbe_agency_grading/templates/mwbe_agency_grading_left_table.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_mwbe_agency_grading/templates/mwbe_agency_grading_left_table.tpl.php
@@ -4,30 +4,28 @@
 
 ?>
 <div class="checkbook-grading-left">
-<table>
-	<th>
+<table id="grading_table">
+	<thead>
 		<tr>
-			<td class="sortable"><a href="">Agency</a></td>
-			<td>Spending Chart</td>
-			<td class="sortable"><a href="">Spending</a></td>
+			<th><div><span>Agency</span></div></th>
+			<th><div><span>Spending<br/> Chart</span></div></th>
+			<th><div><span>Spending</span></div></th>
 		</tr>
-	</th>
-	
+	</thead>
 	<tbody>
 		<?php 
 			$id = 0;
 			foreach($left_agencies_data as $row){
 				$agency = $row['agency_name'];
 				$chart = theme('mwbe_agency_grading_row_chart',array('id'=>$id, 'data_row'=>$row['data_row']));
-				$spending = $row['agency'];
-				$spending = '0';
-				
-				echo "<tr>
-					<td>" . $agency . " </td>
-					<td>" . $chart . "  </td>
-					<td>" . custom_number_formatter_format($row['spending_amount'],2,'$') . "  </td>
-					</tr>	
-				"	;
+				if( $row['spending_amount'] > 0){				
+					echo "<tr>
+						<td><div>" . $agency . " </div></td>
+						<td>" . $chart . "  </td>
+						<td>" . $row['spending_amount'] . "  </td>
+						</tr>	
+					"	;
+				}
 				
 				$id +=1;
 			}		
@@ -37,3 +35,60 @@
 </table>
 
 </div>
+
+
+
+<script>
+
+    var oTable;
+    jQuery(document).ready(function() {
+        oTable = jQuery('#grading_table').dataTable(
+        		{
+        			"bFilter": false,
+        	        "bPaginate": true,
+        	        "iDisplayLength":25,
+        	        "sPaginationType":"full_numbers",
+        	        "bLengthChange": false,
+        	        "sDom":"<pr><t><ip>",
+        	        "oLanguage": {
+        	                "sInfo": "Displaying transactions _START_ - _END_ of _TOTAL_",
+        	                "sProcessing":"<img src='/sites/all/themes/checkbook/images/loading_large.gif' title='Processing...'/>"
+        	        },
+        	        "bInfo": true,
+        	        "aaSorting":[[2,"desc"]],
+        	        "aoColumnDefs": [
+        	                         {
+     		                        	"aTargets": [0],
+     		                        	"asSorting": [  ],
+     		                        	"sClass":"text"
+     		                         },
+        	                         {
+      		                        	"aTargets": [1],
+      		                        	"asSorting": [  ],
+     		                        	"sClass":"text"
+      		                         },
+      		                         {
+      		                        	"aTargets": [2],
+     		                        	"sClass":"number",
+	      		      					"aExportFn":"function",
+	      		      					"mDataProp": function ( source, type, val ) {
+	      		      							if (type == "set") {
+	      		      							source.total_contracts = val;
+	      		      							source.total_contracts_display =  "<div>" + custom_number_format(val) + "</div>";
+	      		      							return;
+	      		      					}else if (type == "display") {
+	      		      						return source.total_contracts_display;
+	      		      					}
+	      		      					return source.total_contracts;
+	      		      					}	      		      						      		      					
+      		                         }
+        	                        ]
+        		}
+                );
+	} );
+</script>
+
+
+
+
+
