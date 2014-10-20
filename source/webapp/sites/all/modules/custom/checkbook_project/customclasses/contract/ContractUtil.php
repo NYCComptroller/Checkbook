81fe86cce64218faeb2716a1121f0d2c463a9f18
Prafull Shirodkar <pshirodkar@C02M11HFFD58.local>
2014-10-20 18:40:07 -0400

    NYCCHKBK-4543 smart search vendor link fix
    Former-commit-id: d77b170ed1ed812a38510f9f737880340623132a [formerly d77b170ed1ed812a38510f9f737880340623132a [formerly d77b170ed1ed812a38510f9f737880340623132a [formerly bd0e96ead5f0caa607ff15232e7da5ee0ef581a9]]]
    Former-commit-id: 27faf8b7264912e74b03c4f8b6927da89169d15d
    Former-commit-id: 4e42dfe0078f264b9f97ba8f9f8bba9b7211ebcc
    Former-commit-id: b7dd2163564349a17a7085f0233e81501a283305

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/contract/ContractUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/contract/ContractUtil.php
@@ -197,6 +197,34 @@ namespace { //global
         	return $latest_minority_type_id;
         }
 
+
+        static public function getLatestMwbeCategoryByVendorByTransactionYear($vendor_id, $year_id = null, $year_type = null){
+
+            if($year_id == null){
+                $year_id =  _getRequestParamValue('year');
+            }
+
+            if($year_type == null){
+                $year_type =  _getRequestParamValue('yeartype');
+            }
+
+                $query = "SELECT vendor_id, agency_id, year_id, type_of_year, minority_type_id, is_prime_or_sub
+                      FROM contract_vendor_latest_mwbe_category
+                      WHERE minority_type_id IN (2,3,4,5,9)
+                      AND vendor_id =".$vendor_id."
+                      AND year_id =".$year_id."
+                      AND type_of_year ='".$year_type."'
+                      GROUP BY vendor_id, agency_id, year_id, type_of_year, minority_type_id, is_prime_or_sub LIMIT 1";
+
+                $results = _checkbook_project_execute_sql_by_data_source($query,'checkbook');
+
+                if($results[0]['minority_type_id'] != ''){
+                   return true;
+                }
+                else{
+                    return false;
+                }
+        }
         /* Returns M/WBE category for the given vendor id in the given year and year type for contracts Advanced Serach results*/
 
         static public function get_contract_vendor_link($vendor_id, $is_prime_or_sub, $minority_type_id){
