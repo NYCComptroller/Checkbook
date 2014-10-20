274e3c4e62d7897d450151176f811c72665d8adf
sgade <sgade@reisystems.com>
2014-10-20 15:15:44 -0400

    Changes for contracts transactions page.  NYCCHKBK-3939

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/contract/ContractUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/contract/ContractUtil.php
@@ -155,8 +155,11 @@ namespace { //global
         	if(!isset($contract_vendor_latest_mwbe_category)){
         		$query = "SELECT vendor_id, agency_id, year_id, type_of_year, minority_type_id, is_prime_or_sub
                       FROM contract_vendor_latest_mwbe_category
-                      WHERE minority_type_id IN (2,3,4,5,9) AND year_id = '".$year_id."' AND type_of_year = '".$year_type."' AND agency_id = " . $agency_id ."
-                      GROUP BY vendor_id, agency_id, year_id, type_of_year, minority_type_id, is_prime_or_sub";
+                      WHERE minority_type_id IN (2,3,4,5,9) AND year_id = '".$year_id
+                            ."' AND type_of_year = '".$year_type
+                            ."' AND agency_id = " . $agency_id
+                            ."' AND is_prime_or_sub = " . $is_prime_or_sub
+                      ." GROUP BY vendor_id, agency_id, year_id, type_of_year, minority_type_id, is_prime_or_sub";
         
         		$results = _checkbook_project_execute_sql_by_data_source($query,'checkbook');
         		foreach($results as $row){
