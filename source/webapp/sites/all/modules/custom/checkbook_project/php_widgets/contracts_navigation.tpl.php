21a13b22d1b78468ea7ad0e6cfed74fa45ca61d8
Prafull Shirodkar <pshirodkar@C02M11HFFD58.local>
2014-11-11 11:45:06 -0500

    NYCCHKBK-4621 "Featured Dashboard" added the tooltip
    Former-commit-id: be387d47ec28d16fc89050d8cf81e99b85e9786d [formerly be387d47ec28d16fc89050d8cf81e99b85e9786d [formerly be387d47ec28d16fc89050d8cf81e99b85e9786d [formerly e0bd3c80d861bfdeebc65403ed22ae441a1ca251]]]
    Former-commit-id: 81ec189db970ae548a10ef39737bc93fc233e9f8
    Former-commit-id: fb60ad7c4516ccc69e7ec247e4f855d027ec5fa3
    Former-commit-id: e6b8a70ade6d38ed5741d9faa73a8ac1ee32adbf

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
@@ -247,10 +247,6 @@ if(preg_match('/mwbe/',$_GET['q']) && preg_match('/subvendor/',$_GET['q']) ){
 */
 
 
-
-
-
-
 //TODO: remove placeholder &nbsp; when numbers under each domain are active
 
 ?>
@@ -271,8 +267,9 @@ if(preg_match('/mwbe/',$_GET['q']) && preg_match('/subvendor/',$_GET['q']) ){
 
 
 <div class="top-navigation-right">
-<div class="featured-dashboard-title"><span title="The amounts represented in the featured dashboards are subset amounts of either the Spending or Contract Domains">
-	<?php echo (preg_match('/contract/',$_GET['q']))?"Contracts ":"Spending " ;?>Featured Dashboard</span>
+<div class="featured-dashboard-title"><a href="#" alt="The amounts represented in the featured dashboards are subset amounts of either the Spending or Contract Domains">
+
+	<?php echo (preg_match('/contract/',$_GET['q']))?"Contracts ":"Spending " ;?>Featured Dashboard</a>
 </div>
 <div class="featured-dashboard-table">
 <table class="expense">
