940d09c88c01f871dea9e4cb3a4c32e9873555a7
Alicia Torkelson <atorkelson@reisys.com>
2015-01-20 17:28:12 -0500

    NYCCHKBK-4893 - Updated logic for transaction page titles to omit widget title if the dashboard and the widget title contain Sub Vendors

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/spending/summary/sub_vendor_summary.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/spending/summary/sub_vendor_summary.tpl.php
@@ -25,9 +25,6 @@ $records = $node->data;
 if(is_array($records)){
     $row = $records[0];
     $title = eval($node->widgetConfig->summaryView->templateTitleEval);
-    if(_getRequestParamValue('dtsmnid') == 719){
-        $title = 'Sub Vendors Total Spending Transactions';
-    }
     $totcontamnt = WidgetUtil::getLabel("total_contract_amount");
     $vendor_name = WidgetUtil::getLabel("sub_vendor_name");
     $ytdspending = WidgetUtil::getLabel("ytd_spending");
@@ -40,7 +37,6 @@ if(is_array($records)){
         $percent_spending = WidgetUtil::getLabel("percent_spending");
     }
     if(_getRequestParamValue('smnid') == 719){
-        $title = 'Sub Vendor Total Spending Transactions';
         $percent_spending_value = '';
         $percent_spending = '';
         $associated_prime_vendor_value = $row['prime_vendor_prime_vendor_legal_name'];
