255afb2e0fcf65443671ec48fc7f53d3655dfa17
Prafull Shirodkar <pshirodkar@C02M11HFFD58.local>
2015-05-14 12:54:00 -0400

    NYCCHKBK-5258: smart search- search button is missing in Firefox while results are loading.
    Former-commit-id: dc912c42bd077ea28ab0c0abce1b7a58a256250d [formerly dc912c42bd077ea28ab0c0abce1b7a58a256250d [formerly dc912c42bd077ea28ab0c0abce1b7a58a256250d [formerly aa8adb14d8b257aa738d65bc033da6b7f7fa46a2]]]
    Former-commit-id: a413cf0190398e7ddfcc3708e349565e11efd9d8
    Former-commit-id: 660ffc0c50a29be391e938be365e6b4ce96a14f0
    Former-commit-id: f90c3fd51f01e9bf42cfe9ec0bdaa732bbe236f1

--- source/webapp/sites/all/modules/custom/checkbook_smart_search/js/smart_search.js
+++ source/webapp/sites/all/modules/custom/checkbook_smart_search/js/smart_search.js
@@ -12,19 +12,17 @@
                                 return false;
                         },
                         select: function (event, ui) {
-                                $(event.target).val(ui.item.label);
-                                window.location = ui.item.url;
-                                $("#edit-search-box").addClass('loadingtext');
+                                $("#edit-submit").addClass('disable_button');
                                 $("#edit-search-box").addClass('loadinggif');
                                 $("#edit-search-box").attr("readonly", "readonly");
-                                var protocol = location.protocol;
-                                var url = location.host;
-                                $("#edit-submit").attr('src', protocol+'//'+url+'/sites/all/themes/checkbook/images/search_disabled.png');
                                 // This is to fix the issue with chrome when trying to disable the search button
                                 setTimeout(function(){
-                                    $("#edit-submit").attr("disabled", 'disabled');
+                                    $('input[type=submit]').attr("disabled", "disabled");
+                                    $("#edit-submit").css('cursor', 'default');
                                     $("#edit-search-box").attr("disabled", "disabled");
                                 }, 10);
+                                $(event.target).val(ui.item.label);
+                                window.location = ui.item.url;
                                 return false;
                         }
             })
@@ -52,17 +50,14 @@
 			});
 		}
         $("#edit-submit").click(function(e) {
-            var protocol = location.protocol;
-            var url = location.host;
-            $("#edit-search-box").addClass('loadingtext');
+            $("#edit-submit").addClass('disable_button');
             $("#edit-search-box").addClass('loadinggif');
-            $("#edit-search-box").attr("readonly", "readonly");
-            $("#edit-submit").attr('src', protocol+'//'+url+'/sites/all/themes/checkbook/images/search_disabled.png');
             // This is to fix the issue with chrome when trying to disable the search button
             setTimeout(function(){
-                $("#edit-submit").attr("disabled", 'disabled');
+                $('input[type=submit]').attr("disabled", "disabled");
+                $("#edit-submit").css('cursor', 'default');
                 $("#edit-search-box").attr("disabled", "disabled");
-            }, 10);
+            }, 1);
         });
     });
     Drupal.behaviors.exportSmartSearchTransactions = {
