be78d6ea3837b20c412c5ed27feb862e5ffaafa9
Alicia Torkelson <atorkelson@reisys.com>
2015-05-27 11:50:27 -0400

    NYCCHKBK-5176 - Fixed some dependencies for CheckbookAPI/DataFeeds
    Former-commit-id: 38d3508df1df191c3bc69f847806c8c03135f224 [formerly 38d3508df1df191c3bc69f847806c8c03135f224 [formerly 38d3508df1df191c3bc69f847806c8c03135f224 [formerly 6b3f66587d076ce57ba6f070b3127461ec4f02b7]]]
    Former-commit-id: 7102ce0d3ea5d6180810f789d0047e00733d4e0c
    Former-commit-id: a5b66d1eccfcd5589da3a088997e99640f25bd9d
    Former-commit-id: 98f37dd4282437c0daa70da5d8442c3b4f10a3a8

--- source/webapp/sites/all/modules/custom/checkbook_api/api/CheckbookAPIRepository.php
+++ source/webapp/sites/all/modules/custom/checkbook_api/api/CheckbookAPIRepository.php
@@ -83,39 +83,3 @@ class CheckbookAPIRepository {
         return $rows_affected;
     }
 }
-
-/**
- * Class CheckbookAPIEntity holds the structure of the CheckbookAPI entity
- *
- * @package checkbook_api
- */
-class CheckbookAPIEntity {
-    public $api_id;
-    public $domain;
-    public $request_xml;
-    public $response_status;
-    public $response_log;
-    public $client_ip;
-    public $city;
-    public $state;
-    public $country;
-    public $country_code;
-    public $continent;
-    public $continent_code;
-    public $created_date;
-
-    function __construct(Array $properties=array())
-    {
-        foreach($properties as $key => $value){
-            $this->{$key} = $value;
-        }
-    }
-
-    function getPropertyNames() {
-        $properties = array();
-        foreach ($this as $name => $value) {
-            $properties[] = $name;
-        }
-        return $properties;
-    }
-}
