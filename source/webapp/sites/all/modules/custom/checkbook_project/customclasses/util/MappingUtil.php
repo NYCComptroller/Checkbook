262c2323c9a30a109cd1eb5158c35af8d841750f
Prafull Shirodkar <pshirodkar@C02M11HFFD58.local>
2015-09-23 15:06:22 -0400

    NYCCHKBK-5534: Smart search > subvendor Vendor link is not giving the MWBE category
    Former-commit-id: a747fe2c7a4d74c143c90cb2f75114ceaa5b7f88 [formerly a747fe2c7a4d74c143c90cb2f75114ceaa5b7f88 [formerly a747fe2c7a4d74c143c90cb2f75114ceaa5b7f88 [formerly 2bf19a6354e35606c0181709a26d5b223183158c]]]
    Former-commit-id: 26b7666e0ed961fe36f755c72d3bdd1546337104
    Former-commit-id: 3cf4097f62262c88b288142d8853805354a44730
    Former-commit-id: e29d16a39dfe39818c0c8d72c2d1799a8fe9c210

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/MappingUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/MappingUtil.php
@@ -426,6 +426,17 @@ class MappingUtil {
     				$title = " <br/><span class=\"second-line\">M/WBE category: " . $current_ethnicity_from_filter . "</span>";
     			}else{
     				$ethnicity_id = ContractUtil::getLatestMwbeCategoryByVendor($vendor_id, null, null, null, $is_prime_or_sub);
+                    if(!$ethnicity_id){
+                        $query = "SELECT year_id, minority_type_id
+                      FROM contract_vendor_latest_mwbe_category
+                      WHERE  vendor_id = ".$vendor_id
+                            ." AND is_prime_or_sub = '" . $is_prime_or_sub . "'"
+                            ." ORDER BY year_id DESC "
+                            ." LIMIT 1 ";
+                        $results = _checkbook_project_execute_sql_by_data_source($query,'checkbook');
+                        if($results)
+                            $ethnicity_id = $results[0]['minority_type_id'];
+                    }
     				if($ethnicity_id > 0){
     					$title = " <br/><span class=\"second-line\">M/WBE Category: " . MappingUtil::getMinorityCategoryById($ethnicity_id) . "</span>";
     				}
