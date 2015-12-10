c03c0d4fdd1c813d363656b1820da71dafe748d0
Alicia Torkelson <atorkelson@reisys.com>
2015-12-10 17:34:09 -0500

    NYCCHKBK-5677 - Advanced Search/Alerts Updates - Added payroll Type Facet for both the FY and CY Advanced Search transaction pages.  Handle custom mapping of the amount_basis_id from the dataset to the actual Payroll Type for these facet.  Updated the payroll meta model to handle this as well.
    Former-commit-id: 4b3e0b4215eb4e34d48328adc7625579741e6116 [formerly 4b3e0b4215eb4e34d48328adc7625579741e6116 [formerly 4b3e0b4215eb4e34d48328adc7625579741e6116 [formerly c80e778e349ade0c4b287ef2ababd1f11bd5c36e]]]
    Former-commit-id: b0eea0c4ae92f56faa6ba1fd6089a288724aded3
    Former-commit-id: c121d2a284d2e1d73ed4523f49326882657b50b8
    Former-commit-id: bd16d0a47d5498514cfc66f5f95f2d1ea8fdc1cb

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payroll/PayrollUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payroll/PayrollUtil.php
@@ -13,8 +13,41 @@ class PayrollUtil {
 
     static function getEmploymentTypeByAmountBasisId($amount_basis_id = null){
         $amount_basis_id = !isset($amount_basis_id) ? _getRequestParamValue("salamttype") : $amount_basis_id;
-        $type_of_employment = $amount_basis_id == 1 ? EmploymentType::$SALARIED : EmploymentType::$NON_SALARIED;
+        $type_of_employment = $amount_basis_id == 1 ? PayrollType::$SALARIED : PayrollType::$NON_SALARIED;
         return $type_of_employment;
     }
 
+    /**
+     * Function updates the current payroll facet data, maps amount_basis_id from the database to the Payroll Type
+     *
+     * @param $node
+     * @return array
+     */
+    static function updatePayrollTypeFacetData($node) {
+
+        $data = array();
+        $count = 0;
+        foreach($node->data as $row) {
+            if($row['payroll_type'] == PayrollType::$SALARIED) {
+                $new_row = array(
+                    'amount_basis_id_amount_basis_id' => '1',
+                    'payroll_type' => $row['payroll_type'],
+                    'txcount' => $row['txcount']
+                );
+                array_push($data, $new_row);
+            }
+            else {
+                $count = $count+$row['txcount'];
+            }
+        }
+
+        if($count > 0){
+            array_push($data, array('amount_basis_id_amount_basis_id' => '2~3',
+                'payroll_type' => PayrollType::$NON_SALARIED,
+                'txcount' => $count
+            ));
+        }
+
+        return $data;
+    }
 } 
