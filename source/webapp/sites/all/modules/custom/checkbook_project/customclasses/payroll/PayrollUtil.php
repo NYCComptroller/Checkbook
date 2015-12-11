1aa05c97eec67f43d29cde4038d311371accf632
Alicia Torkelson <atorkelson@reisys.com>
2015-12-11 16:36:19 -0500

    NYCCHKBK-5677 - Fix the employee transaction page logic to only include payroll type parameter if salamttype request parameter is there.

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payroll/PayrollUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payroll/PayrollUtil.php
@@ -11,8 +11,7 @@ require_once(realpath(drupal_get_path('module', 'checkbook_project')) .'/customc
 
 class PayrollUtil {
 
-    static function getEmploymentTypeByAmountBasisId($amount_basis_id = null){
-        $amount_basis_id = !isset($amount_basis_id) ? _getRequestParamValue("salamttype") : $amount_basis_id;
+    static function getEmploymentTypeByAmountBasisId($amount_basis_id){
         $type_of_employment = $amount_basis_id == 1 ? PayrollType::$SALARIED : PayrollType::$NON_SALARIED;
         return $type_of_employment;
     }
