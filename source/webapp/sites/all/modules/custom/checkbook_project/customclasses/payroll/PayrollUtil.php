3b7a8f01a35a85d3c16e618b58b6ca8ff988a364
Alicia Torkelson <atorkelson@reisystems.com>
2016-01-06 12:15:45 -0500

    NYCCHKBK-5742 - Added 3 new functions to be used in getting counts for salaried/non-salaried employees.
    Former-commit-id: a0f237585c8e00a1ca1d270a64fa64ca50800a0f [formerly a0f237585c8e00a1ca1d270a64fa64ca50800a0f [formerly a0f237585c8e00a1ca1d270a64fa64ca50800a0f [formerly c0f9b3e6b7f5685aeed70d2634dc8aaa78c12f0d]]]
    Former-commit-id: 93117533e0daa6b4e35ca30d484a1b7dd5eaef33
    Former-commit-id: 2110814d17ff72f2fd05aec102e83105b582824f
    Former-commit-id: 3d1fce823df46268b2b1dc5e928146f31ea83c36

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payroll/PayrollUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payroll/PayrollUtil.php
@@ -49,4 +49,75 @@ class PayrollUtil {
 
         return $data;
     }
+
+    /**
+     * Returns the count of salaried employees
+     * @param $year
+     * @param $year_type
+     * @param null $agency_id
+     * @param null $employee_numb
+     * @return int|null
+     */
+    static function getSalariedEmployeeCount($year, $year_type, $agency_id = null, $employee_number = null) {
+
+        $employee_count = self::getEmployeeCount(PayrollType::$SALARIED, $year, $year_type, $agency_id, $employee_number);
+        return isset($employee_count) ? $employee_count : 0;
+    }
+
+    /**
+     * Returns the count of non-salaried employees
+     * @param $year
+     * @param $year_type
+     * @param null $agency_id
+     * @param null $employee_number
+     * @return int|null
+     */
+    static function getNonSalariedEmployeeCount($year, $year_type, $agency_id = null, $employee_number = null) {
+
+        $employee_count = self::getEmployeeCount(PayrollType::$NON_SALARIED, $year, $year_type, $agency_id, $employee_number);
+        return isset($employee_count) ? $employee_count : 0;
+    }
+
+    /**
+     * Returns the count of employees based on type
+     * @param PayrollType $payrollType
+     * @param $year
+     * @param $year_type
+     * @param $agency_id
+     * @param $employee_number
+     * @return null
+     */
+    static function getEmployeeCount($payrollType, $year, $year_type, $agency_id, $employee_number) {
+
+        $employee_count = null;
+
+        try {
+            $data_set = isset($agency_id) ? 'aggregateon_payroll_employment_type' : 'aggregateon_payroll_employment_type_nyc';
+            $parameters = array(
+                'fiscal_year_id' => $year,
+                'type_of_year' => $year_type,
+                'type_of_employment' => $payrollType
+            );
+            if(isset($agency_id)) $parameters['agency_id'] = $agency_id;
+            if(isset($employee_number)) $parameters['employee_number'] = $employee_number;
+
+            $where_filter = '';
+            $where_filters = array();
+            foreach($parameters as $param => $value) {
+                $where_filters[] = _widget_build_sql_condition($param, $value);
+            }
+            if(count($where_filters) > 0){
+                $where_filter = ' WHERE ' . implode(' AND ', $where_filters);
+            }
+            $sql = "SELECT COUNT(count_employment) AS record_count
+            FROM {$data_set}".$where_filter;
+
+            $result = _checkbook_project_execute_sql_by_data_source($sql,_get_default_datasource());
+            $employee_count= (int)$result[0]['record_count'];
+        }
+        catch (Exception $e) {
+            log_error("Error in function getEmployeeCount() \nError getting data from controller: \n" . $e->getMessage());
+        }
+        return $employee_count;
+    }
 } 
