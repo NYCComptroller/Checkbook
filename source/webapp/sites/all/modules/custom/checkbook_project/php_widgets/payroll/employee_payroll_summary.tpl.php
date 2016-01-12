43dcd72222edd1db5e5f728fcee3b4101dab5935
Alicia Torkelson <atorkelson@reisystems.com>
2016-01-12 17:24:13 -0500

    NYCCHKBK-5685 - Updated Employees Payroll Transactions page (Title from annual salaries widget), integrated new data set into static content.
    Former-commit-id: af86882ed2f547cb3e0c3ef6cdccd2537f45ccc8 [formerly af86882ed2f547cb3e0c3ef6cdccd2537f45ccc8 [formerly af86882ed2f547cb3e0c3ef6cdccd2537f45ccc8 [formerly 2a421ecf6e3a222b6332a95922d0b8e21493e95e]]]
    Former-commit-id: 77959d5856877927fa05e9366a620e4049768762
    Former-commit-id: 47dc5f047e7354daa298c4a0714fac09cc5d5e75
    Former-commit-id: 41d5e990b8b2300d7a4b735c4d590cf8665fca9a

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/payroll/employee_payroll_summary.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/payroll/employee_payroll_summary.tpl.php
@@ -28,7 +28,7 @@ $non_salaried_count = 0;
 foreach($node->data as $data){
 
     $record = array();
-    $employment_type = $data['employment_type_employment_type'];
+    $employment_type = $data['type_of_employment'];
 
     if($employment_type == PayrollType::$SALARIED) $salaried_count++;
     else $non_salaried_count++;
@@ -71,93 +71,78 @@ else{
 }
 
 $employeeData = '<div class="payroll-emp-wrapper">';
-foreach($node->data as $results) {
-    $employment_type = $results['employment_type_employment_type'];
-    $class = strtolower($employment_type);
-    $original_title = $results['civil_service_title_civil_service_title'];
+foreach($node->data as $data) {
+
+    $employment_type = $data['type_of_employment'];
+    $year = $data['fiscal_year_id'];
+    $year_type = $data['type_of_year'];
+    $title = $data['civil_service_title'];
+    $original_title = $data['civil_service_title'];
     $title = strtolower($original_title);
-    $title = mb_convert_case($title, MB_CASE_TITLE, "UTF-8");
-    $titleUrl  = "<a href='/payroll'"._checkbook_project_get_year_url_param_string() ."/title/" . urlencode($results['civil_service_title_civil_service_title']) . ">".$title."</a>";
-    $total_annual_salary = custom_number_formatter_format($results['combined_max_annual_salary'],2,'$');
-    $total_gross_pay = custom_number_formatter_format($results['combined_total_gross_pay'],2,'$');
-    $total_base_pay = custom_number_formatter_format($results['combined_total_base_salary'],2,'$');
-    $total_other_payments = custom_number_formatter_format($results['combined_total_other_payments'],2,'$');
-    $total_overtime_pay = custom_number_formatter_format($results['combined_total_overtime_amount'],2,'$');
+    $class = strtolower($employment_type);
+    $total_annual_salary = custom_number_formatter_format($data['total_annual_salary'],2,'$');
+    $total_gross_pay = custom_number_formatter_format($data['total_gross_pay'],2,'$');
+    $total_base_pay = custom_number_formatter_format($data['total_base_pay'],2,'$');
+    $total_other_payments = custom_number_formatter_format($data['total_other_payments'],2,'$');
+    $total_overtime_pay = custom_number_formatter_format($data['total_overtime_pay'],2,'$');
+    $number_employees = number_format($data['number_employees']);
+    $total_employees =  number_format($node->total_employees);
+
+    $title_url = "<a href='/payroll/title_landing/yeartype/$year_type/year/$year/title/$original_title'>{$original_title}</a>";
+
     $lbl_total_number_employees =
         $employment_type == PayrollType::$SALARIED
             ? WidgetUtil::getLabel('total_no_of_sal_employees')
             : WidgetUtil::getLabel('total_no_of_non_sal_employees');
-
-    if(isset($agencyId)) {
-        $total_number_employees =
-            $employment_type == PayrollType::$SALARIED
-                ? number_format($results['total_salaried_employees@checkbook:payroll_agency_employment_type'])
-                : number_format($results['total_non_salaried_employees@checkbook:payroll_agency_employment_type']);
-    }
-    else {
-        $total_number_employees =
-            $employment_type == PayrollType::$SALARIED
-                ? number_format($results['total_salaried_employees@checkbook:payroll_employment_type'])
-                : number_format($results['total_non_salaried_employees@checkbook:payroll_employment_type']);
-    }
-
-    $year = $results['year_year'];
-    $yearType = $results['year_type_year_type'];
-    $agencyId = _getRequestParamValue('agency');
-    if(isset($agencyId)) {
-        $agency =  _checkbook_project_get_name_for_argument('agency_id', $agencyId);
-        $agencyUrl  = "<a href='/payroll/agency_landing/yeartype/$yearType/year/$year/agency/$agencyId'>{$agency}</a>";
-    }
-    $original_title = urlencode($original_title);
-    $title_url =  "<a href='/payroll/title_landing/yeartype/$yearType/year/$year/title/$original_title'>{$title}</a>";
-
     $employeeData .= "<div id='payroll-emp-trans-name' class='emp-record-$class'>
                         <span class='payroll-label'>Title: </span>
                         <span class='payroll-value'>{$title_url}</span>
                     </div>";
 
     $employeeData .= "<div id='payroll-tx-static-content' class='emp-record-$class'><table id='payroll-tx-static-content-table'>";
-    if(isset($agencyId)){
-        $employeeData .= "<tr>
-            <td width='60%'><strong>". WidgetUtil::getLabel('agency_name') ."</strong>: {$agencyUrl}</td>
-            <td><strong>". WidgetUtil::getLabel('payroll_type') ."</strong>: ". strtoupper($employment_type)."</td>
-        </tr>
-        <tr>
-            <td><strong>". WidgetUtil::getLabel('combined_gross_pay_ytd') ."</strong>: {$total_gross_pay}</td>
-            <td><strong>". $lbl_total_number_employees ."</strong>: {$total_number_employees}</td>
-        </tr>
-        <tr>
-            <td><strong>". WidgetUtil::getLabel('combined_base_pay_ytd') ."</strong>: {$total_base_pay}</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td><strong>". WidgetUtil::getLabel('combined_other_pay_ytd') ."</strong>: {$total_other_payments}</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td><strong>". WidgetUtil::getLabel('combined_overtime_pay_ytd') ."</strong>: {$total_overtime_pay}</td>
-            <td></td>
-        </tr>";
+
+    if($employment_type == PayrollType::$SALARIED) {
+        $employeeData .=
+            "<tr>
+                <td><strong>". WidgetUtil::getLabel('combined_annual_salary') ."</strong>: {$total_annual_salary}</td>
+                <td><strong>". WidgetUtil::getLabel('payroll_type') ."</strong>: ". strtoupper($employment_type)."</td>
+            </tr>
+            <tr>
+                <td><strong>". WidgetUtil::getLabel('combined_gross_pay_ytd') ."</strong>: {$total_gross_pay}</td>
+                <td><strong>". $lbl_total_number_employees ."</strong>: {$number_employees}</td>
+            </tr>
+            <tr>
+                <td><strong>". WidgetUtil::getLabel('combined_base_pay_ytd') ."</strong>: {$total_base_pay}</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>". WidgetUtil::getLabel('combined_other_pay_ytd') ."</strong>: {$total_other_payments}</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>". WidgetUtil::getLabel('combined_overtime_pay_ytd') ."</strong>: {$total_overtime_pay}</td>
+                <td></td>
+            </tr>";
     }
     else {
-        $employeeData .= "<tr>
-            <td><strong>". WidgetUtil::getLabel('combined_gross_pay_ytd') ."</strong>: {$total_gross_pay}</td>
-            <td><strong>". WidgetUtil::getLabel('payroll_type') ."</strong>: ". strtoupper($employment_type)."</td>
-        </tr>
-        <tr>
-            <td><strong>". WidgetUtil::getLabel('combined_base_pay_ytd') ."</strong>: {$total_base_pay}</td>
-            <td><strong>". $lbl_total_number_employees ."</strong>: {$total_number_employees}</td>
-        </tr>
-        <tr>
-            <td><strong>". WidgetUtil::getLabel('combined_other_pay_ytd') ."</strong>: {$total_other_payments}</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td><strong>". WidgetUtil::getLabel('combined_overtime_pay_ytd') ."</strong>: {$total_overtime_pay}</td>
-            <td></td>
-        </tr>";
+        $employeeData .=
+            "<tr>
+                <td><strong>". WidgetUtil::getLabel('combined_gross_pay_ytd') ."</strong>: {$total_gross_pay}</td>
+                <td><strong>". WidgetUtil::getLabel('payroll_type') ."</strong>: ". strtoupper($employment_type)."</td>
+            </tr>
+            <tr>
+                <td><strong>". WidgetUtil::getLabel('combined_base_pay_ytd') ."</strong>: {$total_base_pay}</td>
+                    <td><strong>". $lbl_total_number_employees ."</strong>: {$number_employees}</td>
+            </tr>
+            <tr>
+                <td><strong>". WidgetUtil::getLabel('combined_other_pay_ytd') ."</strong>: {$total_other_payments}</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><strong>". WidgetUtil::getLabel('combined_overtime_pay_ytd') ."</strong>: {$total_overtime_pay}</td>
+                <td></td>
+            </tr>";
     }
-
 
     $employeeData .= "</table></div>";
 }
