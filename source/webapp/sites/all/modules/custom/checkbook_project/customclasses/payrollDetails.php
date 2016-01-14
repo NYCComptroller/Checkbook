4dcdfdd7c81b1e20917bf7ae492f67c210d36472
Alicia Torkelson <atorkelson@reisystems.com>
2016-01-13 20:07:02 -0500

    NYCCHKBK-5742 - Removed the aggregate tables used in the payroll static content for employee payroll type and wrote a custom join with aggregate tables to derive the value, tested this for NYC, agency and title level static content.

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payrollDetails.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/payrollDetails.php
@@ -26,18 +26,17 @@ class payrollDetails {
         if(isset($title)) {
             $where .= $where == "" ? "WHERE civil_service_title = '$title'" : " AND civil_service_title = '$title'";
         }
-        $dataset = 'aggregateon_payroll_employment_type_nyc';
+        $dataset = 'aggregateon_payroll_employee_agency';
         if(isset($agency)) {
-            $dataset = 'aggregateon_payroll_employment_type';
+            $dataset = 'aggregateon_payroll_employee_agency';
 
             $where .= $where == "" ? "WHERE agency.agency_id = '$agency'" : " AND agency.agency_id = '$agency'";
-            $sub_query_where .= $sub_query_where == "" ? "WHERE agency_id = '$agency'" : " AND agency_id = '$agency'";
-
             $ref_agency_join = "JOIN ref_agency agency ON agency.agency_id = emp.agency_id";
             $agency_select = ",agency_id, agency_name";
             $agency_sub_select = ",agency.agency_id, agency.agency_name";
             $agency_id_sub_select = ",agency_id AS agency_id_1";
             $agency_join = ' AND emp_type.agency_id_1 = emp.agency_id';
+            $agency_sub_query_group_by = ',agency_id';
             $agency_sub_group_by = ',agency.agency_id,agency.agency_name';
             $agency_group_by = ',agency_id,agency_name';
         }
@@ -73,24 +72,36 @@ class payrollDetails {
                     SUM(emp.overtime_pay) AS total_overtime_pay,
                     MAX(emp.annual_salary) AS total_annual_salary,
                     COUNT(DISTINCT (CASE WHEN COALESCE(overtime_pay,0) > 0 THEN employee_number END)) AS total_overtime_employees,
-                    COUNT(DISTINCT (CASE WHEN emp_type.type_of_employment = 'Salaried' THEN emp_type.employee_number_1 END)) AS total_salaried_employees,
-                    COUNT(DISTINCT (CASE WHEN emp_type.type_of_employment = 'Non-Salaried' THEN emp_type.employee_number_1 END)) AS total_non_salaried_employees,
+                    COUNT(DISTINCT (CASE WHEN COALESCE(emp_type.type_of_employment,'Non-Salaried') = 'Salaried' THEN emp_type.employee_number_1 END)) AS total_salaried_employees,
+                    COUNT(DISTINCT (CASE WHEN COALESCE(emp_type.type_of_employment,'Non-Salaried') = 'Non-Salaried' THEN emp.employee_number END)) AS total_non_salaried_employees,
                     emp.employee_number,
-                    emp_type.type_of_employment
+                    COALESCE(emp_type.type_of_employment,'Non-Salaried') AS type_of_employment
                     {$agency_sub_select}
                     {$title_sub_select}
                     FROM aggregateon_payroll_employee_agency emp
                     {$ref_agency_join}
-                    JOIN
+                    LEFT JOIN
                     (
-                        SELECT
-                        type_of_employment AS type_of_employment,
-                        employee_number AS employee_number_1,
-                        type_of_year AS type_of_year_1,
-                        fiscal_year_id AS fiscal_year_id_1
+                        SELECT DISTINCT
+                        emp.type_of_employment AS type_of_employment,
+                        emp.employee_number AS employee_number_1,
+                        emp.fiscal_year_id AS fiscal_year_id_1,
+                        emp.type_of_year AS type_of_year_1
                         {$agency_id_sub_select}
-                        FROM {$dataset}
-                        {$sub_query_where}
+                        FROM {$dataset} emp
+                        JOIN
+                        (
+                            SELECT max(pay_date) as pay_date,
+                            employee_number,fiscal_year_id,type_of_year
+                            FROM {$dataset}
+                            {$sub_query_where}
+                            GROUP BY employee_number,fiscal_year_id,type_of_year
+                            {$agency_sub_query_group_by}
+                        ) latest_emp ON latest_emp.pay_date = emp.pay_date
+                        AND latest_emp.employee_number = emp.employee_number
+                        AND latest_emp.fiscal_year_id = emp.fiscal_year_id
+                        AND latest_emp.type_of_year = emp.type_of_year
+                        AND type_of_employment = 'Salaried'
                     ) emp_type ON emp_type.employee_number_1 = emp.employee_number
                     AND emp_type.type_of_year_1 = emp.type_of_year
                     AND emp_type.fiscal_year_id_1 = emp.fiscal_year_id
@@ -105,7 +116,7 @@ class payrollDetails {
                 {$title_group_by}
     ";
 
-//        log_error('QUERY:' .$query);
+        log_error('QUERY:' .$query);
         $results = _checkbook_project_execute_sql_by_data_source($query,"checkbook");
         $total_employees = 0;
         foreach($results as $result){
