352243b01cfadf6a4d0abe5d40ddda30fd2b4677
PShirodkar-REISys <pshirodkar@reisys.com>
2016-03-01 15:15:45 -0500

    NYCCHKBK-5900: Trends- Addition of only 2015 Data

--- source/webapp/sites/all/modules/custom/checkbook_trends/templates/revenue_capacity_trends/uncollected_parking_violation_fee.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_trends/templates/revenue_capacity_trends/uncollected_parking_violation_fee.tpl.php
@@ -89,9 +89,9 @@ rsort($years);
                   $row['category'] = 'Write offs, Adjustments<br>and Dispositions (b)';
                 if($row['category'] == 'Allowance for Uncollectible Amounts (c)')
                   $row['category'] = 'Allowance for<br>Uncollectible Amounts (c)';
-                $row['category'] = str_replace('(a)','<sup>(a)</sup>', $row['category']);
-                $row['category'] = str_replace('(b)','<sup>(b)</sup>', $row['category']);
-                $row['category'] = str_replace('(c)','<sup>(c)</sup>', $row['category']);
+                $row['category'] = str_replace('(a)','<sup style="text-transform: lowercase;">(a)</sup>', $row['category']);
+                $row['category'] = str_replace('(b)','<sup style="text-transform: lowercase;">(b)</sup>', $row['category']);
+                $row['category'] = str_replace('(c)','<sup style="text-transform: lowercase;">(c)</sup>', $row['category']);
 
 
                 $conditionCategory = ($row['category']?$row['category']:'&nbsp;');
@@ -109,7 +109,7 @@ rsort($years);
                 		$conditionCategory = "<div class='" . $cat_class . "' >" . $row['category'] . "</div>";
                 		break;
                 }
-                
+
                 
 			    echo "<tr><td>" . $conditionCategory . "</td>";
 			    foreach ($years as $year){
@@ -125,9 +125,9 @@ rsort($years);
     </tbody>
 </table>
 <div class="footnote">
-     <p>(A) The summonses issued by various City agencies for parking violations are adjudicated and collected by the Parking Violations Bureau (PVB) of the City’s Department of Finance.</p>
-     <p>(B) Proposed “write-offs” are in accordance with a write-off policy implemented by PVB for summonses determined to be legally uncollectible/unprocessable or for which all prescribed collection efforts are unsuccessful.</p>
-     <p>(C) The Allowance for Uncollectible Amounts is calculated as follows: summonses which are over three years old are fully (100%) reserved and 35% of summonses less than three years old are reserved.</p>
+     <p>(a) The summonses issued by various City agencies for parking violations are adjudicated and collected by the Parking Violations Bureau (PVB) of the City’s Department of Finance.</p>
+     <p>(b) Proposed “write-offs” are in accordance with a write-off policy implemented by PVB for summonses determined to be legally uncollectible/unprocessable or for which all prescribed collection efforts are unsuccessful.</p>
+     <p>(c) The Allowance for Uncollectible Amounts is calculated as follows: summonses which are over three years old are fully (100%) reserved and 35% of summonses less than three years old are reserved.</p>
      <p>Note: Data does not include interest reflected on the books of PVB.</p>
      <p>Source: The City of New York, Department of Finance, Parking Violations Bureau.</p>
 </div>
