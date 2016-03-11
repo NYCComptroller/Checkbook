4a5c411455890b12b79b4054ac16029b6c0bcec2
Alicia Torkelson <atorkelson@reisystems.com>
2016-03-11 18:08:54 -0500

    Updated domain layer to read SQL from config and added M/WBE repo. and factory
    Former-commit-id: 46a9cc500432ae8db726bf4b754641bcc92e4209 [formerly 46a9cc500432ae8db726bf4b754641bcc92e4209 [formerly 46a9cc500432ae8db726bf4b754641bcc92e4209 [formerly 5e6c799c4a806c4c60cd6a10cee16173bc1ac71d]]]
    Former-commit-id: 3410c6e3c07767cf884172378e98e0d48887d605
    Former-commit-id: 4d5365cd9f45194982f1620c7232e404cf115c44
    Former-commit-id: 6481cb1f50c78487ade68e825ebd4d40c90ab33b

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/spending/SpendingService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/spending/SpendingService.php
@@ -16,7 +16,29 @@ class SpendingService {
         $limit = 5;
         $order_by = "check_amount DESC";
 
-        $checks = $repo->GetChecks($parameters, $limit, $order_by);
+        $checks = $repo->GetCitywideChecks($parameters, $limit, $order_by);
         return $checks;
     }
+
+    function GetMwbeChecksData() {
+
+        $repo = new CheckRepository();
+
+        $parameters = array("check_eft_issued_nyc_year_id" => 117);
+        $limit = 5;
+        $order_by = "check_amount DESC";
+
+        $checks = $repo->GetMwbeChecks($parameters, $limit, $order_by);
+        return $checks;
+    }
+
+//    function TestSql() {
+//
+//        $repo = new CheckRepository();
+//
+//        $domain = "checks";
+//        $statement = "GetCityWideChecks";
+//        $sql = $repo->GetSqlConfig($domain, $statement);
+//        return $sql;
+//    }
 } 
