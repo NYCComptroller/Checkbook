5e6c799c4a806c4c60cd6a10cee16173bc1ac71d
Alicia Torkelson <atorkelson@reisystems.com>
2016-03-11 18:08:54 -0500

    Updated domain layer to read SQL from config and added M/WBE repo. and factory

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_domain/checks/CheckRepository.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_domain/checks/CheckRepository.php
@@ -8,16 +8,42 @@
 
 class CheckRepository {
 
-    function GetChecks($parameters, $limit, $order_by)
+    function GetCityWideChecks($parameters, $limit, $order_by)
     {
-        $data = self::GetChecksData($parameters, $limit, $order_by);
+        $data = self::GetCityWideChecksData($parameters, $limit, $order_by);
         $factory = new CheckFactory();
         $entities = $factory->create(new Check(),$data);
 
         return $entities;
     }
 
-    private function GetChecksData($parameters, $limit, $order_by)
+    function GetMwbeChecks($parameters, $limit, $order_by)
+    {
+        $data = self::GetMwbeChecksData($parameters, $limit, $order_by);
+        $factory = new CheckFactory();
+        $entities = $factory->create(new MwbeCheck(),$data);
+
+        return $entities;
+    }
+    private function GetCityWideChecksData($parameters, $limit, $order_by)
+    {
+        try {
+            $domain = "checks";
+            $statement = "GetCityWideChecks";
+
+//            $sql_config = self::LoadSqlConfig($domain, $statement);
+
+            $query = self::BuildSqlQuery($domain, $statement, $parameters, $limit, $order_by);
+            $results = self::_execute_sql_fetch_assoc($query);
+        }
+        catch (Exception $e) {
+            log_error("Error in function GetCitywideChecksData() \nError getting data from controller: \n" . $e->getMessage());
+            throw $e;
+        }
+        return $results;
+    }
+
+    private function GetMwbeChecksData($parameters, $limit, $order_by)
     {
         //make DB call
         $results = null;
@@ -47,7 +73,35 @@ class CheckRepository {
         return $results;
     }
 
-    private function _execute_sql_fetch_assoc($query, $db_name = "main", $data_source = 'checkbook')
+    private function BuildSqlQuery($domain, $statement, $parameters, $limit, $order_by) {
+
+        $node = self::LoadSqlConfig($domain, $statement);
+
+//        $statement = (string)$node->attributes()->name;
+//        $datasource = (string)$node->attributes()->datasource;
+        $sql = trim((string)$node->sql);
+
+        $where = self::_build_where_clause($parameters);
+        $where = isset($where) ? "\nWHERE {$where}" : "";
+        $order_by = isset($order_by) ? "\nORDER BY {$order_by}" : "";
+        $limit = isset($limit) ? "\nLIMIT {$limit}" : "";
+        $query = "{$sql}{$where}{$order_by}{$limit}";
+
+        return $query;
+    }
+
+    private function GetDataSourceByConfig($config) {
+        return (string)$config->attributes()->datasource;
+    }
+
+    private function LoadSqlConfig($domain, $statement) {
+        $file = realpath(drupal_get_path('module', 'checkbook_domain')) . '/sql/'.$domain.'.xml';
+        $xml = simplexml_load_file($file);
+        $node = $xml->xpath('/statements/statement[@name="'.$statement.'"]')[0];
+        return $node;
+    }
+
+    private function _execute_sql_fetch_assoc($query, $data_source = 'checkbook', $db_name = "main")
     {
         $connection = $results = null;
 
@@ -63,7 +117,6 @@ class CheckRepository {
         //execute SQL
         try {
             $results = $connection->query($query);
-//            $results = $connection->query($query)->fetchAssoc();
         }
         catch (Exception $e) {
             LogHelper::log_error("Error executing DB query for generating: " . $query . ". Exception is: " . $e);
@@ -81,3 +134,33 @@ class CheckRepository {
         return $where;
     }
 }
+
+//class SqlConfiguration {
+//
+//    private $config_xml;
+//
+//    function __construct($domain)
+//    {
+//        $file = realpath(drupal_get_path('module', 'checkbook_domain')) . '/sql/'.$domain.'.xml';
+//        $this->config_xml = simplexml_load_file($file);
+//    }
+//
+//    function get_config() {
+//        if(isset($this->config_xml)) {
+//            return $this->config_xml;
+//        }
+//    }
+//
+//    function get_statement_config($name) {
+//
+//        $node = $this->config_xml->xpath('/statements/statement[@name="'.$name.'"]')[0];
+//        return $node;
+//    }
+//
+//    function get_statement($name) {
+//        $sql = trim((string)$this->config_xml->sql);
+//        return $sql;
+//    }
+//
+//
+//}
