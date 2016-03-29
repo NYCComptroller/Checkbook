0a4dc04205ae61048d47d57d95494fa1ee85ba1f
Alicia Torkelson <atorkelson@reisystems.com>
2016-03-29 11:28:49 -0400

    Added Model and ViewModel

---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_presentation_layer/checkbook_models/NodeViewModel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: atorkelson
+ * Date: 3/29/16
+ * Time: 11:08 AM
+ */
+
+abstract class NodeViewModel {
+    public $widgetConfig;
+    public $data;
+}
+
---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_presentation_layer/checkbook_models/WidgetConfigModel.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: atorkelson
+ * Date: 3/29/16
+ * Time: 11:08 AM
+ */
+
+abstract class WidgetConfigModel {
+    public $keepOriginalDatasource;
+}
