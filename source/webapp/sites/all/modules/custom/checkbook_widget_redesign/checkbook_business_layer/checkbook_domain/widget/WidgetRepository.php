c4fa7a0d5fe0a6f8198b5ec0325d9ed8fd0ec5ee
Alicia Torkelson <atorkelson@reisystems.com>
2016-04-11 11:05:17 -0400

    Additional files for previous checkins

---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_domain/widget/WidgetRepository.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: atorkelson
+ * Date: 4/8/16
+ * Time: 2:50 PM
+ */
+
+class WidgetRepository extends AbstractWidgetRepository {
+
+}
+
+
---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/spending/ChecksWidgetService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: atorkelson
+ * Date: 4/8/16
+ * Time: 3:58 PM
+ */
+
+class ChecksWidgetService extends AbstractWidgetService {
+
+    public function implDerivedColumns($data) {
+        // TODO: Implement implDerivedColumns() method.
+        foreach($data as $row) {
+            foreach($row as $column) {
+
+            }
+        }
+        return $data;
+    }
+
+}
