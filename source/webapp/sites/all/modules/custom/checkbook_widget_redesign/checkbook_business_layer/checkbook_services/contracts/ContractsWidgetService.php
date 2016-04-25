51e89e3a54c5b77a8ef3f3860e19cf9f3927974b
Alicia Torkelson <atorkelson@reisystems.com>
2016-04-25 13:33:25 -0400

    NYCCHKBK-6033 - Updated the new contracts widget to have correct link urls and counts, including the parameters for mwbe

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetService.php
@@ -12,112 +12,29 @@ class ContractsWidgetService extends AbstractWidgetService {
         $value = null;
         switch($column_name) {
             case "contract_id_link":
-
                 $column = $row['contract_number'];
                 $class = "bottomContainerReload";
-                $url = "/panel_html/contract_transactions/contract_details"
-                    . _checkbook_project_get_url_param_string('status')
-                    ."/magid/".$row['original_agreement_id']
-                    ."/doctype/".$row['document_code']
-                    ._checkbook_append_url_params();
-
+                $url = ContractsUrlService::contractIdUrl($row['original_agreement_id'],$row['document_code']);
                 $value = "<a class='{$class}' href='{$url}'>{$column}</a>";
-
                 break;
+
             case "agency_name_link":
-
                 $column = $row['agency_name'];
-                $url = "/contracts_landing"
-                    ."/magid/".$row['original_agreement_id']
-                    . _checkbook_append_url_params()
-                    ._checkbook_project_get_url_param_string('vendor')
-                    ._checkbook_project_get_url_param_string('cindustry')
-                    ._checkbook_project_get_url_param_string('csize')
-                    ._checkbook_project_get_url_param_string('awdmethod')
-                    ._checkbook_project_get_url_param_string('status')
-                    ._checkbook_project_get_year_url_param_string()
-                    ."/agency/".$row['agency_id']
-                    ."?expandBottomCont=true";
-
+                $url = ContractsUrlService::agencyUrl($row['original_agreement_id'],$row['agency_id']);
                 $value = "<a href='{$url}'>{$column}</a>";
-
                 break;
+
             case "vendor_name_link":
-
                 $column = $row['vendor_name'];
-                $url = "/contracts_landing"
-//                    .ContractUtil::get_contracts_vendor_link_by_mwbe_category($row)
-                    ."?expandBottomCont=true";
-
+                $url = ContractsUrlService::vendorUrl($row['vendor_id'], $row['minority_type_id'], $row['is_prime_or_sub']);
                 $value = "<a href='{$url}'>{$column}</a>";
-
                 break;
-            case "spent_to_date_link":
 
-                $nid = "";
+            case "spent_to_date_link":
                 $column = $row['spending_amount_sum'];
                 $class = "new_window";
-                $url = "/spending/transactions"
-                    ."/magid/".$row['original_agreement_id']
-                    ._checkbook_append_url_params()
-                    .(new ContractURLHelper())->_checkbook_project_spending_get_year_url_param_string()
-                    .ContractUtil::getSpentToDateParams()
-                    ."/smnid/".$nid
-                    ."/newwindow";
-
+                $url = ContractsUrlService::spentToDateUrl($row['original_agreement_id'],$row['vendor_id'],$row['contract_number']);
                 $value = "<a class='{$class}' href='{$url}'>{$column}</a>";
-
-                break;
-            case "award_method_name_link":
-
-                $column = $row['award_method_name'];
-                $url = "/contracts_landing"
-                    ._checkbook_project_get_url_param_string('agency')
-                    . _checkbook_append_url_params()
-                    ._checkbook_project_get_url_param_string('vendor')
-                    ._checkbook_project_get_url_param_string('status')
-                    ._checkbook_project_get_url_param_string('cindustry')
-                    ._checkbook_project_get_url_param_string('csize')
-                    ."/awdmethod/".$row['award_method_id']
-                    ._checkbook_project_get_year_url_param_string()
-                    ."?expandBottomCont=true";
-
-                $value = "<a href='{$url}'>{$column}</a>";
-
-                break;
-            case "award_size_name_link":
-
-                $column = $row['award_size_name'];
-                $url = "/contracts_landing"
-                    ._checkbook_project_get_url_param_string('agency')
-                    . _checkbook_append_url_params()
-                    ._checkbook_project_get_url_param_string('vendor')
-                    ._checkbook_project_get_url_param_string('awdmethod')
-                    ._checkbook_project_get_url_param_string('status')
-                    ._checkbook_project_get_url_param_string('cindustry')
-                    ._checkbook_project_get_url_param_string('csize')
-                    ."/csize/".$row['award_size_id']
-                    ._checkbook_project_get_year_url_param_string()
-                    ."?expandBottomCont=true";
-
-                $value = "<a href='{$url}'>{$column}</a>";
-                break;
-            case "industry_type_name_link":
-
-                $column = $row['industry_type_name'];
-                $url = "/contracts_landing"
-                    ._checkbook_project_get_url_param_string('agency')
-                    . _checkbook_append_url_params()
-                    ._checkbook_project_get_url_param_string('vendor')
-                    ._checkbook_project_get_url_param_string('status')
-                    ._checkbook_project_get_url_param_string('csize')
-                    ._checkbook_project_get_url_param_string('awdmethod')
-                    ."/cindustry/".$row['industry_type_id']
-                    ._checkbook_project_get_year_url_param_string()
-                    ."?expandBottomCont=true";
-
-                $value = "<a href='{$url}'>{$column}</a>";
-
                 break;
         }
 
