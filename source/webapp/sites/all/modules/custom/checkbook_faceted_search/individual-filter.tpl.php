3ec6ea68009dcbf435c14a7d7d17b003cad0cd23
Alicia Torkelson <atorkelson@reisystems.com>
2016-04-29 15:59:04 -0400

    NYCCHKBK-6041 - Addition of the contract id facet to the contracts transaction page with related theming changes

--- source/webapp/sites/all/modules/custom/checkbook_faceted_search/individual-filter.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_faceted_search/individual-filter.tpl.php
@@ -207,7 +207,12 @@ $id_filter_name = str_replace(" ", "_", strtolower($filter_name));
         $id = $id_filter_name."_checked_".$ct;
         echo '<div class="row">';
         echo '<div class="checkbox"><input class="styled" id="'.$id.'" name="' . $autocomplete_id . '" type="checkbox" ' . $disableFacet . 'checked="checked" value="' . urlencode(html_entity_decode($row[0],ENT_QUOTES)) . '" onClick="return applyTableListFilters();"><label for="'.$id.'"></label></div>';
-        echo '<div class="name">' . _break_text_custom2($row[1],15) . '</div>';
+        if($node->widgetConfig->filterName == 'Contract ID') {
+            echo '<div class="name">' . $row[1],15 . '</div>';
+        }
+        else {
+            echo '<div class="name">' . _break_text_custom2($row[1],15) . '</div>';
+        }
         echo '<div class="number"><span class="active">' . number_format($row[2]) . '</span></div>';
         echo '</div>';
         $ct++;
@@ -224,7 +229,12 @@ $id_filter_name = str_replace(" ", "_", strtolower($filter_name));
         $id = $id_filter_name."_unchecked_".$ct;
         echo '<div class="row">';
         echo '<div class="checkbox"><input class="styled" id="'.$id.'" name="' . $autocomplete_id . '" type="checkbox" '  .  $disabled .  'value="' . urlencode(html_entity_decode($row[0],ENT_QUOTES)) . '" onClick="return applyTableListFilters();"><label for="'.$id.'"></label></div>';
-        echo '<div class="name">' . _break_text_custom2($row[1],15) . '</div>';
+        if($node->widgetConfig->filterName == 'Contract ID') {
+            echo '<div class="name">' . $row[1],15 . '</div>';
+        }
+        else {
+            echo '<div class="name">' . _break_text_custom2($row[1],15) . '</div>';
+        }
         echo '<div class="number"><span>' . number_format($row[2]) . '</span></div>';
         echo '</div>';
         $ct++;
