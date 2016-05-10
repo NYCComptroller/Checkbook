23bb4bebe87e77bf59a0859f8734d731b5d4a88e
Alicia Torkelson <atorkelson@reisystems.com>
2016-05-10 13:46:52 -0400

    Updates to widget files for demo

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/spending/SpendingWidgetService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/spending/SpendingWidgetService.php
@@ -11,30 +11,28 @@ class SpendingWidgetService extends AbstractWidgetService {
     public function implDerivedColumn($column_name,$row) {
         $value = null;
         switch($column_name) {
-            case "contract_id_link":
-                $value = "test";
-
-                //"expression": "'<a class=\"bottomContainerReload\" href=\"/panel_html/contract_transactions/contract_details/magid/' .$row[original_agreement_original_agreement] . _checkbook_append_url_params()  .  '/doctype/'   . $row[document_code_document_code_document_code]    . '\">' . $row['contract_number_contract_number'] . '</a>'"
-//                            '<a href="/spending_landing'
-//                            .  _checkbook_project_get_url_param_string('vendor')
-//                            . _checkbook_project_get_url_param_string('category')
-//                            . _checkbook_project_get_year_url_param_string()
-//                            . _checkbook_append_url_params()
-//                            . '/agency/' . $row['agency_id'] . '">' . _get_tooltip_markup($row['agency_name']) . '</a>';
+            case "agency_name_link":
+                $column = $row['agency_name'];
+                $url = "";
+                $value = "<a href='{$url}'>{$column}</a>";
                 break;
-            case "vendor_name":
-                $value = "test";
-//                            $row['expenditure_object_name'] == 'Payroll Summary'
-//                                ? _get_tooltip_markup($row['vendor_name'],36)
-//                                : '<a href=\"' . SpendingUtil::getPrimeVendorNameLinkUrl(null,$row) . '\">' . $row['vendor_name_formatted'] . '</a>';
+            case "vendor_name_link":
+                $datasource = _getRequestParamValue("datasource");
+                $dashboard = _getRequestParamValue("dashboard");
+                $column = $row['vendor_name'];
+                if(!isset($datasource) && !isset($dashboard)) return $column;
+                $url = "";
+                $value = "<a href='{$url}'>{$column}</a>";
                 break;
-            case "check_amount":
-                $value = "custom_number_formatter_format($row[check_amount],2,'$')";
+            case "sub_vendor_name_link":
+                $column = $row['sub_vendor_name'];
+                $url = "";
+                $value = "<a href='{$url}'>{$column}</a>";
                 break;
         }
 
         if(isset($value)) {
-            return $value; //do something
+            return $value;
         }
         return $value;
     }
