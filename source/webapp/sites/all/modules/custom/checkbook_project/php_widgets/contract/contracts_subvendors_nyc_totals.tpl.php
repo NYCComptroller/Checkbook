32664be1a4fe1157047804636f6eb1358b31d01e
PShirodkar-REISys <pshirodkar@reisys.com>
2016-06-21 16:35:06 -0400

    NYCCHKBK-6195: Create and Add the New Bottom Navigation Menu
    Former-commit-id: e15f6a45c22f1d98ac6d30a916cf0ce4ba3b6e37 [formerly e15f6a45c22f1d98ac6d30a916cf0ce4ba3b6e37 [formerly e15f6a45c22f1d98ac6d30a916cf0ce4ba3b6e37 [formerly 3bfd9cb66fa1d46556b5d7951732f9971252e49e]]]
    Former-commit-id: 67261e73aaa8dd1561517ced6af3a6270de7eee5
    Former-commit-id: 8815004468cacf55c63e9377eaa768a5b683f1ab
    Former-commit-id: f7a30793aae4725e76a21a63a421bc2067ed9311

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contract/contracts_subvendors_nyc_totals.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contract/contracts_subvendors_nyc_totals.tpl.php
@@ -19,7 +19,7 @@
 */
 ?>
 <div class="activeExpenseContractNote toolTip">Includes all multiyear contracts whose end date is greater than today’s date or completed in the current fiscal year</div>
-<div class="nyc_totals_links">
+<div class="nyc_subvendors_totals_links">
   <table>
     <tbody>
     <tr>
@@ -45,7 +45,7 @@
           ?>
         <div<?php echo $class; ?>>
       <?php if($node->data[0]['total_contracts'] > 0 ){?>
-          <a href="/<?php echo $active_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>Active<br>Expense Contracts<br><?php echo $dollars; ?></a>
+          <a href="/<?php echo $active_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>Total Active<br>Sub Vendor Contracts<br><?php echo $dollars; ?></a>
         <?php }else{?>
         <?php echo $count; ?><br>Active<br>Expense Contracts<br><?php echo $dollars; ?>
         <?php }?>           
@@ -64,91 +64,25 @@
       <td<?php echo $class; ?>>
         <div class="positioning">
       <?php if($node->data[1]['total_contracts'] > 0 ){?>                
-        <a href="/<?php echo $reg_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>Registered<br>Expense Contracts<br><?php echo $dollars; ?></a>
+        <a href="/<?php echo $reg_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>New Sub Vendor Contracts<br>by Fiscal Year<br><?php echo $dollars; ?></a>
         <?php }else{?>
-        <?php echo $count; ?><br>Registered<br>Expense Contracts<br><?php echo $dollars; ?>
+        <?php echo $count; ?><br>New Sub Vendor Contracts<br>by Fiscal Year<br><?php echo $dollars; ?>
         <?php }?>         
         </div>
         <div class="indicator"></div>
       </td>
       <?php
       $class = "";
-      if (preg_match("/^contracts_revenue_landing/", $_GET['q']) & _getRequestParamValue("status") == "A") {
+      if (preg_match("/^contracts_landing/", $_GET['q']) & _getRequestParamValue("status") == "") {
         $class = ' class="active"';
       }
-      $active_link = ContractURLHelper::prepareActRegContractsSliderFilter('contracts_revenue_landing', 'A');
-      $count = "<span class='count'>" . number_format($node->data[2]['total_contracts']) . "</span>";
-      $dollars = "<span class='dollars'>" . custom_number_formatter_format($node->data[2]['current_amount_sum'],1,'$') . "</span>";
-      
+      $subvendor_link = ContractURLHelper::prepareSubvendorContractsSliderFilter('contracts_landing');
       ?>
       <td<?php echo $class; ?>>
         <div class="positioning">
-        <?php if($node->data[2]['total_contracts'] > 0 ){?>
-          <a href="/<?php echo $active_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>Active<br>Revenue Contracts<br><?php echo $dollars; ?></a>
-        <?php }else{?>
-        <?php echo $count; ?><br>Active<br>Revenue Contracts<br><?php echo $dollars; ?>
-        <?php }?>            
+          <a href="/<?php echo $subvendor_link; ?>?expandBottomCont=true"><br>Status of Sub Vendor<br>Contracts by Prime Vendor<br></a>
         </div>
-        <div class="indicator"></div>
-      </td>
-      <?php
-      $class = "";
-      if (preg_match("/^contracts_revenue_landing/", $_GET['q']) & _getRequestParamValue("status") == "R") {
-        $class = ' class="active"';
-      }
-      $reg_link = ContractURLHelper::prepareActRegContractsSliderFilter('contracts_revenue_landing', 'R');
-      $count = "<span class='count'>" . number_format($node->data[3]['total_contracts']) . "</span>";
-      $dollars = "<span class='dollars'>" . custom_number_formatter_format($node->data[3]['current_amount_sum'],1,'$') . "</span>";
-      
-      ?>
-      <td<?php echo $class; ?>>
-        <div class="positioning">
-      <?php if($node->data[3]['total_contracts'] > 0 ){?>                
-          <a href="/<?php echo $reg_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>Registered<br>Revenue Contracts<br><?php echo $dollars; ?></a>
-        <?php }else{?>
-        <?php echo $count; ?><br>Registered<br>Revenue Contracts<br><?php echo $dollars; ?>
-        <?php }?>           
-        </div>
-        <div class="indicator"></div>
-      </td>
-      <?php
-      $class = "";
-      if (preg_match("/^contracts_pending_exp_landing/", $_GET['q'])) {
-        $class = ' class="active"';
-      }
-      $pending_exp_link = ContractURLHelper::preparePendingContractsSliderFilter('contracts_pending_exp_landing');
-      $count = "<span class='count'>" . number_format($node->data[5]['total_contracts']) . "</span>";
-      $dollars = "<span class='dollars'>" . custom_number_formatter_format($node->data[4]['total_contract_amount'],1,'$') . "</span>";
-      ?>
-      <td<?php echo $class;?>>
-        <div class="positioning">
-      <?php if($node->data[5]['total_contracts'] > 0 ){?>
-          <a href="/<?php echo $pending_exp_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>Pending<br>Expense Contracts<br><?php echo $dollars; ?></a>
-        <?php }else{?>
-        <?php echo $count; ?><br>Pending<br>Expense Contracts<br><?php echo $dollars; ?>
-        <?php }?>           
-        </div>
-        <div class="indicator"></div>
-      </td>
-      <?php
-      $class = "";
-      if (preg_match("/^contracts_pending_rev_landing/", $_GET['q'])) {
-        $class = ' active';
-      }
-      $pending_rev_link = ContractURLHelper::preparePendingContractsSliderFilter('contracts_pending_rev_landing');
-      $count = "<span class='count'>" . number_format($node->data[7]['total_contracts']) . "</span>";
-      $dollars = "<span class='dollars'>" . custom_number_formatter_format($node->data[6]['total_contract_amount'],1,'$') . "</span>";
-      
-      ?>
-      <td class="last<?php echo $class;?>">
-        <div class="positioning">
-        <?php if($node->data[7]['total_contracts'] > 0 ){?>
-          <a href="/<?php echo $pending_rev_link; ?>?expandBottomCont=true"><?php echo $count; ?><br>Pending<br>Revenue Contracts<br><?php echo $dollars; ?></a>
-        <?php }else{?>
-        <?php echo $count; ?><br>Pending<br>Revenue Contracts<br><?php echo $dollars; ?>
-        <?php }?>  
-        </div>
-        <div class="indicator"></div>
+        <div class="indicator" style="top:23px;"></div>
       </td>
     </tr>
     </tbody>
