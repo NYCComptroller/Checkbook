5949e51b6dc177e42ad0723192e462ca0ac0e69d
Alicia Torkelson <atorkelson@reisystems.com>
2016-06-29 14:21:27 -0400

    NYCCHKBK-6209 - Updated label title for the Sub M/WBE Category column
    Former-commit-id: 1629752aa829818d1531503eca6ae328cde880ef [formerly 1629752aa829818d1531503eca6ae328cde880ef [formerly 1629752aa829818d1531503eca6ae328cde880ef [formerly bc1cb38867e4329e4e86d1fda1a2175aa4216e7c]]]
    Former-commit-id: bc670b2dfcd7663736dbde356861ee3025d37470
    Former-commit-id: 91c4500beb88bac7a836917842487eed35319780
    Former-commit-id: 10bb4b941431f81cf470207068108424d08ef193

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/WidgetUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/WidgetUtil.php
@@ -208,7 +208,7 @@ class WidgetUtil
         "sub_contract_reference_id"=>"Sub Contract<br/>Reference ID",
         "mwbe_category"=>"M/WBE<br/>Category",
         "prime_mwbe_category"=>"Prime M/WBE<br/>Category",
-        "sub_mwbe_category"=>"M/WBE<br/>Category",
+        "sub_mwbe_category"=>"Sub M/WBE<br/>Category",
         "num_sub_vendors"=>"Number of<br/>Sub Vendors",
         "num_sub_contracts"=>"Number of<br/>Sub Contracts",
         "ytd_spending_sub_vendors"=>"YTD Spending<br/>Sub Vendors",
