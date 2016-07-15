766b40576f4f23dc0422773da40fc176d19750b2
PShirodkar-REISys <pshirodkar@reisys.com>
2016-07-15 13:17:53 -0400

    NYCCHKBK-6156: Contracts Transaction Page titles - visualization link title pages
    Former-commit-id: caedf58f1cf141f17643496edc47d6c9db5ae168 [formerly caedf58f1cf141f17643496edc47d6c9db5ae168 [formerly caedf58f1cf141f17643496edc47d6c9db5ae168 [formerly bc95a3a6c3814b4a1c38faebe61b8fe1eb9fc1ef]]]
    Former-commit-id: c3aea9b9ac9d3067e523c375d79e96a1bcf2676d
    Former-commit-id: e41f992e255b6da813deea3f521c9edd1dc35fb4
    Former-commit-id: 6a18f1a3e19c21ab5251d11692569d40e3e3fc14

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/spending/SpendingUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/spending/SpendingUtil.php
@@ -1211,23 +1211,29 @@ class SpendingUtil{
             $dashboard = MappingUtil::getCurrenEhtnicityName();
         }
         $bottomNavigation = '';
-        if($dashboard_param == 'ss'){
-            if($status == 'A')
-                $bottomNavigation = "Total Active Sub Vendor Contracts";
-            else
-                $bottomNavigation = "New Sub Vendor Contracts by Fiscal Year";
-        }
-        if($dashboard_param == 'ms' || $dashboard_param == 'sp'){
-            if($status == 'A')
-                $bottomNavigation = "Total Active M/WBE Sub Vendor Contracts";
-            else
-                $bottomNavigation = "New M/WBE Sub Vendor Contracts by Fiscal Year";
-        }
+        if($status == 'A')
+            $bottomNavigation = "Total Active Sub Vendor Contracts";
+        else
+            $bottomNavigation = "New Sub Vendor Contracts by Fiscal Year";
+
         if($smnid == 721 || $smnid == 720 || $smnid == 781 || $smnid == 784){
             $widgetTitle = 'Spending';
         }
+        if($smnid =='subvendor_contracts_visual_1' && $dashboard_param == 'sp'){
+            return(MappingUtil::getCurrenEhtnicityName() . " Sub Vendors Spending by <br />" .$bottomNavigation);
+        }
+        if($smnid =='subven_mwbe_contracts_visual_2' && $dashboard_param == 'ms'){
+
+            return(MappingUtil::getCurrenEhtnicityName() . " Sub Spending by <br />" .$bottomNavigation);
+        }
 
         if($dashboard_param == 'ss' || $dashboard_param == 'ms' || $dashboard_param == 'sp') {
+            if($dashboard_param == 'ms' || $dashboard_param == 'sp'){
+                if($status == 'A')
+                    $bottomNavigation = "Total Active M/WBE Sub Vendor Contracts";
+                else
+                    $bottomNavigation = "New M/WBE Sub Vendor Contracts by Fiscal Year";
+            }
            return($widgetTitle . " by  " . $bottomNavigation . " " . "Transactions");
         }
 
