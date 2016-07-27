56248c51cbe029dfb1f3976eaf6b1c6cff8c5c83
Alicia Torkelson <atorkelson@reisystems.com>
2016-07-27 16:42:52 -0400

    NYCCHKBK-6297 - Added pre-selection for prime mwbe in the sub contract transaction page

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/contract/ContractUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/contract/ContractUtil.php
@@ -975,9 +975,9 @@ namespace { //global
                 . _checkbook_project_get_url_param_string('csize')
                 . _checkbook_project_get_url_param_string('cindustry')
                 . _checkbook_project_get_url_param_string('dashboard')
+                . $mwbe_param
                 . ((!_checkbook_check_isEDCPage())? $subvendorURLString . $vendorURLString : _checkbook_project_get_url_param_string('vendor'))
-                . _checkbook_project_get_year_url_param_string()
-                . $mwbe_param;
+                . _checkbook_project_get_year_url_param_string();
             return $url;
         }
         static function getSubVendorCustomerCode($subVendorId){
