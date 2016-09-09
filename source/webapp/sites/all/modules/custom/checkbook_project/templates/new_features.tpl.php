bbf537cb39505c23256ed0d0a1d4893f615cee00
sgade-reisys <sgade@reisystems.com>
2016-09-09 19:04:39 -0400

    NYCCHKBK-6420 :Addition of 'New Features' window

--- source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
@@ -1,7 +1,7 @@
-<h3>NEW FEATURES</h3>
 <?php
 foreach($new_features as $new_feature=>$value){
+    echo '<br/>';
     $release_date = date("d/m/Y",strtotime($value->field_release_date['und'][0]['value']));
-    print '<b>' . $release_date . ': '.$value->title .'</b>';
+    print '<div><strong>' . $release_date . ': '.$value->title .'</strong></div>';
     print $value->body['und'][0]['safe_value'];
 }
