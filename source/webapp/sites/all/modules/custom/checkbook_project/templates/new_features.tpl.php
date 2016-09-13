ead61a402393c22abe5f26d9f9d1abdef5a146d2
sgade-reisys <sgade@reisystems.com>
2016-09-13 15:40:53 -0400

    NYCCHKBK-6420: Addition of 'New Features' window Former-commit-id: 44cad6d0a168d20b05b72e611cb4e2f12bd11f54 [formerly 44cad6d0a168d20b05b72e611cb4e2f12bd11f54 [formerly 44cad6d0a168d20b05b72e611cb4e2f12bd11f54 [formerly 0ad35c15f614ebdf416125d02dceaff3482197ac]]] Former-commit-id: 426f12270b48a17e84334a75e586ddbddfbf2e53 Former-commit-id: d7d8b5b4fc10e8dd58e145df668649cb7c0b5f0f Former-commit-id: 136420b37cac54b9eaabe75fa2b6744bc55d5747

--- source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
@@ -1,7 +1,8 @@
 <?php
 foreach($new_features as $new_feature=>$value){
-    echo '<br/>';
-    $release_date = date("d/m/Y",strtotime($value->field_release_date['und'][0]['value']));
-    print '<div><strong>' . $release_date . ': '.$value->title .'</strong></div>';
-    print $value->body['und'][0]['safe_value'];
+    if($value->field_display['und'][0]['value']){
+        $release_date = date("d/m/Y",strtotime($value->field_release_date['und'][0]['value']));
+        print '<div><strong>' . $release_date . ': '.$value->title .'</strong></div>';
+        print $value->body['und'][0]['safe_value'];
+    }
 }
