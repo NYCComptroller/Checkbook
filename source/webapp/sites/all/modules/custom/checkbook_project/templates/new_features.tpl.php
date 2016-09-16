5de5c43db0b0d4efb68896dac2417d1fd24c7267
sgade-reisys <sgade@reisystems.com>
2016-09-16 17:15:29 -0400

    NYCCHKBK-6420: Theming for 'New Features'button and the popup Former-commit-id: a98afd8c02f5f35ed529edf7f53b90c38064226f [formerly a98afd8c02f5f35ed529edf7f53b90c38064226f [formerly a98afd8c02f5f35ed529edf7f53b90c38064226f [formerly d42ba92d9d4bac526a638f855b55ebc7f531f15d]]] Former-commit-id: 6681c660b37e6b07f81487e7d67eb9e5747defbd Former-commit-id: b2c9442c4245c1155f705c3d8894f521b6350526 Former-commit-id: 7d7f9858fc4c4bab76f25ad6b71c787a432f1eaa

--- source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
@@ -1,7 +1,7 @@
 <?php
 foreach($new_features as $new_feature=>$value){
     if($value->field_display['und'][0]['value']){
-        $release_date = date("d/m/Y",strtotime($value->field_release_date['und'][0]['value']));
+        $release_date = date("m/d/Y",strtotime($value->field_release_date['und'][0]['value']));
         print "<div class='new-features-title'>" . $release_date . ": ".$value->title ."</div>";
         print "<div class='new-features-description'>".$value->body['und'][0]['safe_value'] . "</div>";
     }
