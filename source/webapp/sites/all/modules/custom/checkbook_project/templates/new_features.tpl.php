a74c81bfb69256d9434e437c59c9953be9f0d219
sgade-reisys <sgade@reisystems.com>
2016-09-23 16:19:39 -0400

    NYCCHKBK-6420: Added height to 'New Features' results container. Former-commit-id: 3ebc07c79dcb8925d19356a7d9fdd7ed47801671 [formerly 3ebc07c79dcb8925d19356a7d9fdd7ed47801671 [formerly 3ebc07c79dcb8925d19356a7d9fdd7ed47801671 [formerly 73463499f182b309d1ffd2ac837dde4b5fabfbc6]]] Former-commit-id: dd184705089b6df66d24eebf5b9c4ab63857f5a3 Former-commit-id: a012e66b51a5afcc866efb28ba1c6dbafc37ae6d Former-commit-id: 975f7027bd0a7a9b97fb0430560fe3535fc8b3c5

--- source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/templates/new_features.tpl.php
@@ -1,8 +1,12 @@
 <?php
-foreach($new_features as $new_feature=>$value){
-    if($value->field_display['und'][0]['value']){
-        $release_date = date("m/d/Y",strtotime($value->field_release_date['und'][0]['value']));
-        print "<div class='new-features-title'>" . $release_date . ": ".$value->title ."</div>";
-        print "<div class='new-features-description'>".$value->body['und'][0]['safe_value'] . "</div>";
-    }
+if(count($new_features) > 0)
+    foreach($new_features as $new_feature=>$value){
+        if($value->field_display['und'][0]['value']){
+            $release_date = date("m/d/Y",strtotime($value->field_release_date['und'][0]['value']));
+            print "<div class='new-features-title'>" . $release_date . ": ".$value->title ."</div>";
+            print "<div class='new-features-description'>".$value->body['und'][0]['safe_value'] . "</div>";
+        }
+}else{
+    print "<div id='no-records' class='clearfix'><span>There are no new features available now.</span></div>";
 }
+    
