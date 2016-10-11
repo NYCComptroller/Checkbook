6eb0b74d36424686bfe723b4c9ab6b7221cf857d
sgade-reisys <sgade@reisystems.com>
2016-10-11 15:08:59 -0400

    NYCCHKBK-6515: Create a re-usable & configurable function to display widgets Added new utility functions to get parameter value and contracts category. Former-commit-id: 024df468a692616404ffa8de11878a073ea69283 [formerly 024df468a692616404ffa8de11878a073ea69283 [formerly 024df468a692616404ffa8de11878a073ea69283 [formerly 0d29361e15a178caca43e7194bdb1346de453ea2]]] Former-commit-id: d76e2781094a495c5cb1be18fdbb94335e1d117f Former-commit-id: bcfb8532e48974647af9d5b2e602366509e66a5b Former-commit-id: 0c2d51c29b5b4f035de578eeae07ab5cbd7a21f3

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetVisibilityService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetVisibilityService.php
@@ -7,10 +7,32 @@
  */
 
 class ContractsWidgetVisibilityService {
+    /**
+     * returns the view to be displayed
+     * @param string $widget
+     * @return view name to be displayed
+     */
     static function getWidgetVisibility($widget) {
+        $dashboard = self::getRequestParamValue('dashboard');
+        $datasouce = self::getRequestParamValue('datasouce');
+        $category = self::contractCategory();
+
         switch($widget){
             case 'departments':
-                $view = 'contracts_departments_view';
+                if(self::getRequestParamValue('agency')){
+                    if($category['category'] === 'expense' && $category['status'] != 'pending'){
+                        if($datasouce === 'checkbook_oge'){
+                            if(self::getRequestParamValue('vendor'))
+                                $view = 'contracts_departments_view';
+                            else
+                               $view = 'oge_contracts_departments_view'; 
+                        }else{
+                           if(!$dashboard || $dashboard == 'mp'){
+                                $view = 'contracts_departments_view';
+                           }
+                        }
+                    }
+                }
                 break;
             default : 
                 //handle the exception when there is no match
@@ -18,4 +40,69 @@ class ContractsWidgetVisibilityService {
         }
         return $view;
     }
+    
+    /**
+     * returns request parameter value from URL($_REQUEST['q'])
+     * @param string $paramName
+     * @return request parameter value
+     */
+    function getRequestParamValue($paramName, $fromRequestPath = TRUE){
+        if(empty($paramName)){
+            return NULL;
+        }
+        $value = NULL;
+        if($fromRequestPath){
+          $urlPath = drupal_get_path_alias($_GET['q']);
+          $pathParams = explode('/', $urlPath);
+          $index = array_search($paramName,$pathParams);
+          if($index !== FALSE){
+              $value =  filter_xss($pathParams[($index+1)]);
+          }
+          if(trim($value) == ""){
+            return NULL;
+          }
+          if(isset($value) || $fromRequestPath){
+              return htmlspecialchars_decode($value,ENT_QUOTES);
+          }
+        }else{
+          return filter_xss(htmlspecialchars_decode($_GET[$paramName],ENT_QUOTES));
+        }
+    }
+    
+    function contractCategory(){
+        $urlPath = drupal_get_path_alias($_GET['q']);
+        $pathParams = explode('/', $urlPath);
+        $status = getRequestParamValue('status');
+        $category = array();
+        
+        switch($pathParams[0]){
+            case 'contracts_landing':
+                $category['category'] = 'expense';
+                if($status == 'A'){
+                    $category['status'] = 'active';
+                }else{
+                    $category['status'] = 'registered';
+                }
+                break;
+            case 'cpntracts_revenue_landing':
+               $category['category'] = 'revenue';
+                if($status == 'A'){
+                    $category['status'] = 'active';
+                }else{
+                    $category['status'] = 'registered';
+                }
+               break; 
+            case 'cpntracts_pending_exp_landing':
+                $category['category'] = 'expense';
+                $category['status'] = 'pending';
+               break;  
+            case 'cpntracts_pending_rev_landing':
+               $category['category'] = 'revenue';
+                $category['status'] = 'pending';
+               break; 
+        }
+        
+        return $category;
+    }
+    
 }
