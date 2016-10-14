9db13c499c0e2dc24b44eb2cc317cd41b703282f
PShirodkar-REISys <pshirodkar@reisys.com>
2016-10-14 16:39:25 -0400

    NYCCHKBK-6491: Implement - Top 5 Award Method Widget visibility fix

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetVisibilityService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetVisibilityService.php
@@ -61,31 +61,29 @@ class ContractsWidgetVisibilityService {
                     }
                 }
                 break;
-            case 'industries':
-                if(!self::getRequestParamValue('cindustry')){
+            case 'award_methods':
+                if(!self::getRequestParamValue('awdmethod')){
                     switch($category) {
                         case "expense":
                             switch($dashboard) {
                                 case "ss":
                                 case "sp":
-                                    $view = 'sub_contracts_by_industries_view';
-                                    break;
                                 case "ms":
-                                    $view = 'mwbe_sub_contracts_by_industries_view';
+                                    $view = 'subvendor_award_methods_view';
                                     break;
                                 default:
-                                    $view = self::isEDCPage() ? 'oge_contracts_by_industries_view' : 'contracts_by_industries_view';
+                                    $view = self::isEDCPage() ? 'oge_award_methods_view' : 'expense_award_methods_view';
                                     break;
                             }
                             break;
 
                         case "revenue":
-                            $view = 'revenue_contracts_by_industries_view';
+                            $view = 'revenue_award_methods_view';
                             break;
 
                         case "pending expense":
                         case "pending revenue":
-                            $view = 'pending_contracts_by_industries_view';
+                            $view = 'pending_award_methods_view';
                             break;
                     }
                 }
