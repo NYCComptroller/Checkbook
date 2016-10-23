659b302402cf1ca2aa7b4028da1063fc0ae0badb
sgade-reisys <sgade@reisystems.com>
2016-10-23 18:40:32 -0400

    NYCCHKBK-6497: Implement - "Contract Amount Modifications" Widget Former-commit-id: 5e52eadeed5516b7fce58c94076bc124eae45060 [formerly 5e52eadeed5516b7fce58c94076bc124eae45060 [formerly 5e52eadeed5516b7fce58c94076bc124eae45060 [formerly 42d6a84923a8f0fb1da8aba38692b65c0aa32f15]]] Former-commit-id: 781fff3ba7c210cbd16b0c2cfb0d20e40e93c88a Former-commit-id: 817cbd99d67238de3fefbac0c52df02fc8c2f9a2 Former-commit-id: 9b4925abfd2efbe76ab064bc133b3408da59b0a9

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsUrlService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsUrlService.php
@@ -93,12 +93,6 @@ class ContractsUrlService {
         return $url;
     }
     
-    //TODO: move to a separate re-usable class (ie ContractUrlParameters) to parse the Url and return parameters
-    static private  function _getCurrentPage() {
-        $currentUrl = explode('/',$_SERVER['HTTP_REFERER']);
-        return '/'.$currentUrl[3];
-    }
-    
     /**
      * Gets the Minoritype Name link for the given minority type id
      * @param $minorityTypeId
@@ -107,7 +101,7 @@ class ContractsUrlService {
     static function minorityTypeUrl($minorityTypeId){
         $url = NULL;
         if(MinorityTypeURLService::isMWBECertified($minorityTypeId)){
-            $currentUrl = self::_getCurrentPage();
+            $currentUrl = RequestUtilities::_getCurrentPage();
             $minorityTypeId = ($minorityTypeId == 4 || $minorityTypeId == 5) ? '4~5': $minorityTypeId;
             $dashboard = "mp";
             $url =  '/'. $currentUrl
@@ -140,42 +134,8 @@ class ContractsUrlService {
         return 'expense';
     }
 
-    static function vendorUrl($vendor_id,$agency_id,$year_id,$year_type,$minority_type_id,$is_prime_or_sub) {
-
-        $url = _checkbook_project_get_url_param_string("agency")
-            . _checkbook_project_get_url_param_string("contstatus","status")
-            . _checkbook_project_get_url_param_string("cindustry")
-            . _checkbook_project_get_url_param_string("csize")
-            . _checkbook_project_get_url_param_string("awdmethod")
-            . _checkbook_project_get_year_url_param_string();
-
-        $latest_minority_id = MinorityTypeURLService::getLatestMwbeCategoryByVendor($vendor_id, $agency_id, $year_id, $year_type, $is_prime_or_sub);
-        $latest_minority_id = isset($latest_minority_id) ? $latest_minority_id : $minority_type_id;
-        $is_mwbe_certified = MinorityTypeURLService::isMWBECertified(array($latest_minority_id));
-        $dashboard= RequestUtilities::getRequestParamValue("dashboard");
-
-        $urlPath = drupal_get_path_alias($_GET['q']);
-        if(!preg_match('/pending/',$urlPath)){
-            if(!RequestUtilities::getRequestParamValue('status')){
-                $url .= "/status/A";
-            }
-        }
-
-        if($is_mwbe_certified && $dashboard == 'mp') {
-            $url .= "/dashboard/mp/mwbe/2~3~4~5~9/vendor/".$vendor_id;
-        }
-        else if($is_mwbe_certified && $dashboard != 'mp') {
-            $url .= "/dashboard/mp/mwbe/2~3~4~5~9/vendor/".$vendor_id;
-        }
-        else {
-            $url .= _checkbook_project_get_url_param_string("datasource")."/vendor/".$vendor_id;
-        }
-        $currentUrl = self::_getCurrentPage();
-        return $currentUrl . $url . "?expandBottomCont=true";
-    }
-
     static function agencyUrl($agency_id, $original_agreement_id = null) {
-        $currentUrl = self::_getCurrentPage();
+        $currentUrl = RequestUtilities::_getCurrentPage();
         $url = $currentUrl
             .(isset($original_agreement_id) ? ("/magid/".$original_agreement_id):'')
             . _checkbook_append_url_params()
@@ -191,7 +151,7 @@ class ContractsUrlService {
     }
 
     static function awardmethodUrl($award_method_id) {
-        $currentUrl = self::_getCurrentPage();
+        $currentUrl = RequestUtilities::_getCurrentPage();
         $url = $currentUrl
             . _checkbook_append_url_params()
             ._checkbook_project_get_url_param_string('vendor')
@@ -206,7 +166,7 @@ class ContractsUrlService {
     }
 
     static function industryUrl($industry_type_id) {
-        $currentUrl = self::_getCurrentPage();
+        $currentUrl = RequestUtilities::_getCurrentPage();
         $url = $currentUrl
             . _checkbook_append_url_params()
             ._checkbook_project_get_url_param_string('vendor')
@@ -221,7 +181,7 @@ class ContractsUrlService {
     }
 
     static function contractSizeUrl($award_size_id) {
-        $currentUrl = self::_getCurrentPage();
+        $currentUrl = RequestUtilities::_getCurrentPage();
         $url = $currentUrl
             . _checkbook_append_url_params()
             ._checkbook_project_get_url_param_string('vendor')
@@ -292,106 +252,5 @@ class ContractsUrlService {
             . $subvendorURLString . $vendorURLString
             . _checkbook_project_get_year_url_param_string();
         return $url;
-    }
-    
-    static function getSubVendorCustomerCode($subVendorId){
-        $result = NULL;
-        $query = "SELECT v.vendor_customer_code 
-                FROM subvendor v
-                JOIN (SELECT vendor_id, MAX(vendor_history_id) AS vendor_history_id FROM subvendor_history GROUP BY 1) vh 
-                  ON v.vendor_id = vh.vendor_id
-                WHERE v.vendor_id = ".$subVendorId;
-
-        $subVendorCustomerCode = _checkbook_project_execute_sql_by_data_source($query,'checkbook');
-        if($subVendorCustomerCode) {
-            return $subVendorCustomerCode[0]['vendor_customer_code'];
-        }
-        else {
-            return null;
-        }
-    }
-
-    static function getVendorCustomerCode($vendorId){
-        $vendor = _checkbook_project_querydataset("checkbook:vendor","vendor_customer_code",array("vendor_id"=>$vendorId));
-        if($vendor[0]) {
-            return $vendor[0]['vendor_customer_code'];
-        }
-        else {
-            return null;
-        }
-    }
-    
-    /**
-    * Returns M/WBE category for the given vendor id in the given year and year type for
-    * Active/Registered Contracts Landing Pages
-    * @param $row
-    * @return string
-    */
-    static public function getContractsVendorLinkByMWBECategory($row){
-
-        $vendor_id = $row["vendor_id"] != null ? $row["vendor_id"] : $row["vendor_id"];
-        if($vendor_id == null)
-            $vendor_id = $row["prime_vendor_id"];
-
-        $year_id = RequestUtilities::getRequestParamValue("year");
-        $year_type = $row["type_of_year,"];
-        $is_prime_or_sub = $row["is_prime_or_sub"] != null ? $row["is_prime_or_sub"] : "P";
-        $agency_id = null;
-
-        if($row["current_prime_minority_type_id"])
-            $minority_type_id = $row["current_prime_minority_type_id"];
-        if($row["minority_type_id"])
-            $minority_type_id = $row["minority_type_id"];
-        if($row["prime_minority_type_id"])
-            $minority_type_id = $row["prime_minority_type_id"];
-
-        $smnid = RequestUtilities::getRequestParamValue("smnid");
-        if($smnid == 720 || $smnid == 784) return self::getSubContractsVendorLink($vendor_id, $year_id, $year_type,$agency_id);
-
-        $latest_minority_id = MinorityTypeURLService::getLatestMwbeCategoryByVendor($vendor_id, $agency_id = null, $year_id, $year_type, $is_prime_or_sub);
-        $latest_minority_id = isset($latest_minority_id) ? $latest_minority_id : $minority_type_id;
-        $is_mwbe_certified = MinorityTypeURLService::isMWBECertified($latest_minority_id);
-
-        $status = _checkbook_project_get_url_param_string("contstatus","status");
-        $status = isset($status) && $status != "" ? $status : "/status/A";
-        $url = _checkbook_project_get_url_param_string("agency") . $status . _checkbook_project_get_year_url_param_string();
-
-        if($is_mwbe_certified && RequestUtilities::getRequestParamValue('dashboard') == 'mp') {
-            $url .= _checkbook_project_get_url_param_string("cindustry")
-                . _checkbook_project_get_url_param_string("csize")
-                . _checkbook_project_get_url_param_string("awdmethod")
-                . "/dashboard/mp/mwbe/2~3~4~5~9/vendor/".$vendor_id;
-        }
-        else if($is_mwbe_certified && RequestUtilities::getRequestParamValue('dashboard') != 'mp') {
-            $url .= "/dashboard/mp/mwbe/2~3~4~5~9/vendor/".$vendor_id;
-        }
-        else {
-            $url .= _checkbook_project_get_url_param_string("datasource")."/vendor/".$vendor_id;
-        }
-        return $url;
-    }
-    
-    static public function getSubContractsVendorLink($vendor_id, $year_id = null, $year_type = null,$agency_id = null, $mwbe_cat = null){
-
-        $latest_minority_id = isset($mwbe_cat) ? $mwbe_cat : self::getLatestMwbeCategoryByVendor($vendor_id, $agency_id = null, $year_id, $year_type, "S");
-        $url = _checkbook_project_get_url_param_string("agency") .  _checkbook_project_get_url_param_string("contstatus","status") . _checkbook_project_get_year_url_param_string();
-
-        $current_dashboard = RequestUtilities::getRequestParamValue("dashboard");
-        $is_mwbe_certified = in_array($latest_minority_id, array(2, 3, 4, 5, 9));
-
-        //if M/WBE certified, go to M/WBE (Sub Vendor) else if NOT M/WBE certified, go to Sub Vendor dashboard
-        $new_dashboard = $is_mwbe_certified ? "ms" : "ss";
-        $status = strlen(_checkbook_project_get_url_param_string("contstatus","status"))== 0 ? "/status/A" : "";
-
-        if($current_dashboard != $new_dashboard ){
-                return $url . $status . "/dashboard/" . $new_dashboard . ($is_mwbe_certified ? "/mwbe/2~3~4~5~9" : "" ) . "/subvendor/".$vendor_id;
-        }else{
-                $url .= $status._checkbook_project_get_url_param_string("cindustry"). _checkbook_project_get_url_param_string("csize")
-                . _checkbook_project_get_url_param_string("awdmethod") ."/dashboard/" . $new_dashboard .
-                ($is_mwbe_certified ? "/mwbe/2~3~4~5~9" : "" ) . "/subvendor/".$vendor_id;
-                return $url;
-        }
-
-        return '';
-    }
+    }   
 }
--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsWidgetService.php
@@ -54,13 +54,13 @@ class ContractsWidgetService extends AbstractWidgetService {
                 $column = $row['vendor_name'];
                 $year_id = _getRequestParamValue("year");
                 $year_type = _getRequestParamValue("yeartype");
-                $url = ContractsUrlService::vendorUrl($row['vendor_id'], $row['agency_id'], $year_id, $year_type, $row['minority_type_id'], $row['is_prime_or_sub']);
+                $url = ContractsVendorUrlService::vendorUrl($row['vendor_id'], $row['agency_id'], $year_id, $year_type, $row['minority_type_id'], $row['is_prime_or_sub']);
                 $value = "<a href='{$url}'>{$column}</a>";
                 break;
             
             case "vendor_name_link_by_mwbe":
                 $column = $row['vendor_name'];
-                $url = ContractsUrlService::getContractsVendorLinkByMWBECategory($row);
+                $url = ContractsVendorUrlService::getContractsVendorLinkByMWBECategory($row);
                 $value = "<a href='{$url}'>{$column}</a>";
                 break;
 
@@ -82,7 +82,7 @@ class ContractsWidgetService extends AbstractWidgetService {
                 }
                 $year_id = _getRequestParamValue("year");
                 $year_type = _getRequestParamValue("yeartype");
-                $url = ContractsUrlService::vendorUrl($row['vendor_id'], $row['agency_id'], $year_id, $year_type, $row['minority_type_id'], $row['is_prime_or_sub']);
+                $url = ContractsVendorUrlService::vendorUrl($row['vendor_id'], $row['agency_id'], $year_id, $year_type, $row['minority_type_id'], $row['is_prime_or_sub']);
                 $value = "<a href='{$url}'>{$column}</a>";
                 break;
 
