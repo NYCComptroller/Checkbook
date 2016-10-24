5db56bd40185595d8def37c29f166ab268417b2d
Alicia Torkelson <atorkelson@reisystems.com>
2016-10-24 12:22:59 -0400

    New Common functions
    Former-commit-id: c7090c0771789c32680b834137593c21f931dce0 [formerly c7090c0771789c32680b834137593c21f931dce0 [formerly c7090c0771789c32680b834137593c21f931dce0 [formerly ad9e2328dc53542249f20b87f7fce17211fc1e96]]]
    Former-commit-id: 071c168291bc42d1b88271251b86e742572b5c09
    Former-commit-id: 806194de5f1f40be4aee1aa31f0053cb5206f1f4
    Former-commit-id: 4ecec1ede4c91e6407a67227e5c2799909ad3a37

---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/ContractConstants.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: atorkelson
+ * Date: 10/18/16
+ * Time: 5:43 PM
+ */
+
+abstract class ContractCategory {
+
+    const EXPENSE = "expense";
+    const REVENUE = "revenue";
+
+    static public function getCurrent() {
+        $urlPath = $_GET['q'];
+        $ajaxPath = $_SERVER['HTTP_REFERER'];
+        $category = self::EXPENSE;
+
+        if(preg_match('/revenue/',$urlPath) || preg_match('/revenue/',$ajaxPath)
+        || preg_match('/pending_rev/',$urlPath) || preg_match('/pending_rev/',$ajaxPath)) {
+            $category = self::REVENUE;
+        }
+
+        return $category;
+    }
+}
+
+abstract class ContractStatus {
+
+    const ACTIVE = "active";
+    const REGISTERED = "registered";
+    const PENDING = "pending";
+
+    static public function getCurrent() {
+        $parameter = ContractStatusParameter::getCurrent();
+        switch($parameter) {
+            case ContractStatusParameter::ACTIVE: return self::ACTIVE;
+            case ContractStatusParameter::REGISTERED: return self::REGISTERED;
+            default: return self::PENDING;
+        }
+    }
+}
+
+abstract class ContractStatusParameter {
+    const ACTIVE = "A";
+    const REGISTERED = "R";
+
+    static public function getCurrent() {
+        return RequestUtilities::getRequestParamValue(UrlParameter::CONTRACT_STATUS);
+    }
+}
+
+abstract class ContractType {
+
+    const ACTIVE_EXPENSE = "active_expense";
+    const REGISTERED_EXPENSE = "registered_expense";
+    const ACTIVE_REVENUE = "active_revenue";
+    const REGISTERED_REVENUE = "registered_revenue";
+    const PENDING_EXPENSE = "pending_expense";
+    const PENDING_REVENUE = "pending_revenue";
+
+    static public function getCurrent() {
+
+        $status = ContractStatus::getCurrent();
+        $category = ContractCategory::getCurrent();
+        return "{$status}_{$category}";
+    }
+}
+
