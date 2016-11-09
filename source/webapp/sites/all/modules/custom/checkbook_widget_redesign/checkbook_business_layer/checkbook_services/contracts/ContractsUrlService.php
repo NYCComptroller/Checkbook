c0b9475d7c7282c7d89e758c07906b0969c0f115
Alicia Torkelson <atorkelson@reisystems.com>
2016-11-09 17:42:37 -0500

    NYCCHKBK-6574 - Fixed details link on sub vendors widget & corrected static amount filter with vendor mapping NYCCHKBK-6577 - Fixed industry parameter in details link NYCCHKBK-6559 - Fixed the doc type parameter in the case with no default

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsUrlService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/contracts/ContractsUrlService.php
@@ -268,6 +268,7 @@ class ContractsUrlService {
         $subvendor = RequestUtilities::getRequestParamValue('subvendor');
         $vendor = RequestUtilities::getRequestParamValue('vendor');
         $mwbe = RequestUtilities::getRequestParamValue('mwbe');
+        $industry = RequestUtilities::getRequestParamValue('cindustry');
         $category = ContractCategory::getCurrent();
 
         $subvendor_code = $subvendor ? VendorService::getSubVendorCode($subvendor) : null;
@@ -276,12 +277,13 @@ class ContractsUrlService {
         $subvendor_param = isset($subvendor_code) ? '/vendorcode/'.$subvendor_code : '';
         $vendor_param = isset($vendor_code) ? '/vendorcode/'.$vendor_code : '';
         $mwbe_param = isset($mwbe) ? (Dashboard::isSubDashboard() ? '/smwbe/'.$mwbe : '/pmwbe/'.$mwbe) : '';
+        $industry_param = isset($industry) ? (Dashboard::isSubDashboard() ? '/scindustry/'.$industry : '/pcindustry/'.$industry) : '';
         $category_param = '/contcat/'.(isset($category) ? $category : ContractCategory::EXPENSE);
         $smnid_param = isset($legacy_node_id) ? '/smnid/'.$legacy_node_id : '';
         $contract_status = _checkbook_project_get_url_param_string('status','contstatus');
         $contract_status = isset($contract_status) && $contract_status != '' ? $contract_status : "/contstatus/P";
 
-        $path = Dashboard::isSubDashboard()
+        $path = Dashboard::isSubDashboard() && ContractStatus::getCurrent() == ContractStatus::PENDING
             ? '/panel_html/sub_contracts_transactions/subcontract/transactions'
             : '/panel_html/contract_details/contract/transactions';
 
@@ -290,11 +292,11 @@ class ContractsUrlService {
             . _checkbook_append_url_params()
             . _checkbook_project_get_url_param_string('agency')
             . _checkbook_project_get_url_param_string('vendor')
+            . _checkbook_project_get_url_param_string('subvendor')
             . _checkbook_project_get_url_param_string("vendor","fvendor")
             . _checkbook_project_get_url_param_string('awdmethod')
             . _checkbook_project_get_url_param_string('csize')
-            . _checkbook_project_get_url_param_string('cindustry')
-            . $mwbe_param . $subvendor_param . $vendor_param
+            . $mwbe_param . $subvendor_param . $vendor_param . $industry_param
             . _checkbook_project_get_year_url_param_string()
             . self::getDocumentCodeUrlString($parameters)
             . $smnid_param;
@@ -302,10 +304,45 @@ class ContractsUrlService {
     }
 
     static function getDocumentCodeUrlString($parameters) {
-        $doc_types = explode(",",$parameters['doctype']);
-        $doc_types_url =  implode("~",str_replace("'", "", $doc_types));
-        $doc_types_url =  str_replace("(", "", str_replace(")", "", $doc_types_url));
-        return isset($doc_types_url) ? '/doctype/'. $doc_types_url : '';
+
+        $doc_type = $parameters['doctype'];
+
+        if(isset($doc_type)) {
+            $doc_type = explode(",",$doc_type);
+            $doc_type =  implode("~",str_replace("'", "", $doc_type));
+            $doc_type =  str_replace("(", "", str_replace(")", "", $doc_type));
+        }
+        else {
+            //contract category or doc type is derived from the page path
+            $status = ContractStatus::getCurrent();
+            $category = ContractCategory::getCurrent();
+
+            switch($status){
+                case ContractStatus::PENDING:
+                    switch($category) {
+                        case ContractCategory::REVENUE:
+                            $doc_type = "RCT1";
+                            break;
+                        default:
+                            $doc_type = "MMA1~MA1~MAR~CT1~CTA1~CTR";
+                            break;
+                    }
+                    break;
+
+                default:
+                    switch($category) {
+                        case ContractCategory::REVENUE:
+                            $doc_type = "RCT1";
+                            break;
+                        default:
+                            $doc_type = "MA1~CTA1~CT1";
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        return isset($doc_type) ? '/doctype/'. $doc_type : '';
     }
 
     /**
