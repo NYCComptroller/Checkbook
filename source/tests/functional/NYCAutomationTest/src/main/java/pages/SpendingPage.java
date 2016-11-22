18e4bb098a7ca4f0ee686764608ee02f5a2b41ca
Alekhya Myanani <amyanani@reisystems.com>
2016-11-22 15:59:17 -0500

    NYCCHKBK-6596	Spending Page Graph Titles verification NYCCHKBK-6597 Spending Page Widget titles verification
    Former-commit-id: fb699a133774e33211a07b931fa92fee5e15e070 [formerly fb699a133774e33211a07b931fa92fee5e15e070 [formerly fb699a133774e33211a07b931fa92fee5e15e070 [formerly f5d4b79895b30f2ca0f3a277990edc79d7d9ce1d]]]
    Former-commit-id: 614aaaa1bda2dd947bf32d61a9ea7e5ba88a4b98
    Former-commit-id: 9c9e2bbe099daa8d1e2174d9afa85644891972e9
    Former-commit-id: c5bd5bdc5377ff1755e38ece8f68dd8e71ff1b02

--- source/tests/functional/NYCAutomationTest/src/main/java/pages/SpendingPage.java
+++ source/tests/functional/NYCAutomationTest/src/main/java/pages/SpendingPage.java
@@ -1,6 +1,11 @@
 package pages;
 
 import navigation.TopNavigation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utility.Driver;
@@ -9,7 +14,6 @@ public class SpendingPage {
 
     public static void GoTo() {
         TopNavigation.Spending.Select();
-
     }
 
     public static String GetSpendingAmount() {
@@ -20,7 +24,43 @@ public class SpendingPage {
     public static boolean isAt() {
         WebElement h2title = Driver.Instance.findElement(By.xpath("//*[@id=\"node-widget-21\"]/div[1]/h2"));
         return h2title.getText().equals("Total Spending");
-
     }
+
+	public static ArrayList<String> VisualizationTitles() {
+		ArrayList<String> titles = new ArrayList<String>();
+		List<WebElement> titleContainers = Driver.Instance.findElements(By.cssSelector("#nyc-spending > .top-chart > .inside > .panel-pane"));
+		for(int i=0; i < titleContainers.size(); i++){
+			selectVisualizationSlider(i);
+			WebElement titleClass = titleContainers.get(i).findElement(By.cssSelector(".pane-content > .node-widget > .content > .chart-title"));
+			if(titleClass.isDisplayed()){
+				String title = titleClass.getText();
+				titles.add(title);
+			}
+		}	
+		return titles;
+	}
+	
+	public static void selectVisualizationSlider(int sliderPosition){
+		List<WebElement> sliderContainer = Driver.Instance.findElements(By.cssSelector("#nyc-spending > .top-chart > .slider-pager > a"));
+		sliderContainer.get(sliderPosition).click();
+		Driver.Instance.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+	}
+
+	public static ArrayList<String> WidgetTitles() {
+		ArrayList<String> titles = new ArrayList<String>();
+		List<WebElement> titleContainers = Driver.Instance.findElements(By.className("tableHeader"));
+		for (WebElement titleContainer : titleContainers) {
+			WebElement titleHeaderContainer = titleContainer.findElement(By.cssSelector("h2"));
+			titles.add(titleHeaderContainer.getText());
+		}	
+		return titles;
+	}
+	
+	public static void ShowWidgetDetails(){
+		WebElement detailsLinkContainer = Driver.Instance.findElement(By.className("bottomContainerToggle"));
+		if(detailsLinkContainer.getText().contains("Show Details")){
+			detailsLinkContainer.click();
+		}
+	}
 
 }
