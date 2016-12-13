a2aa0986c6c3c06d0aed339202d6aaf6efc0ecec
sgade <sgade@10.200.110.74>
2016-12-13 12:37:33 -0500

    NYCCHKBK-6666: Top 5 Departments - Implementation Former-commit-id: 32413e14f59bae38a9214c54d7a8c7398149f506 [formerly 32413e14f59bae38a9214c54d7a8c7398149f506 [formerly 32413e14f59bae38a9214c54d7a8c7398149f506 [formerly 5f3168ca89e7cfcd76bfac59b4d416404989e3d2]]] Former-commit-id: d1d8dcabe90a95cc0d5b2010cd799d5783c3170f Former-commit-id: 54aa28cc9c5703bce6a26b00c071fb2bf0466d18 Former-commit-id: 5d40bac6f49879d8f56560e98a53d6dcd9369749

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/spending/SpendingWidgetService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/spending/SpendingWidgetService.php
@@ -29,6 +29,12 @@ class SpendingWidgetService extends AbstractWidgetService {
                 $url = SpendingUrlService::ytdSpendindUrl('expcategory',$row['expenditure_object_id'], $this->getLegacyNodeId());
                 $value = "<a class='{$class}' href='{$url}'>{$column}</a>";
                 break;
+            case "department_ytd_spending_link":
+                $column = $row['check_amount_sum'];
+                $class = "bottomContainerReload";
+                $url = SpendingUrlService::ytdSpendindUrl('dept',$row['department_id'], $this->getLegacyNodeId());
+                $value = "<a class='{$class}' href='{$url}'>{$column}</a>";
+                break;
             case "vendor_name_link":
                 $datasource = _getRequestParamValue("datasource");
                 $dashboard = _getRequestParamValue("dashboard");
