577f94d72f46578ffd8c99d5128e35d7045bb10b
sgade <sgade@10.200.110.74>
2017-01-10 14:18:15 -0500

    NYCCHKBK-6826: Revenue: File Structure Updates

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/revenue/RevenueDataService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/revenue/RevenueDataService.php
@@ -1,8 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class RevenueDataService extends AbstractDataService {
 
+    const sqlConfigPath = "revenue/revenue";
+
+    function __construct() {
+        parent::__construct(self::sqlConfigPath);
+    }
+
+} 
--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/revenue/RevenueWidgetService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/revenue/RevenueWidgetService.php
@@ -1,8 +1,35 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Created by PhpStorm.
+ * User: sgade
+ * Date: 01/10/16
+ * Time: 2:05 PM
  */
 
+class RevenueWidgetService extends AbstractWidgetService {
+
+    public function implDerivedColumn($column_name,$row) {
+        $value = null;
+        $legacy_node_id = $this->getLegacyNodeId();
+        switch($column_name) {
+           case "agency_name_link":
+                $column = $row['agency_name'];
+                $url = '';
+                $value = "<a href='{$url}'>{$column}</a>";
+                break; 
+        }
+        
+        if(isset($value)) {
+            return $value;
+        }
+        return $value;
+    }
+    
+    public function adjustParameters($parameters, $urlPath) {
+        return $parameters;
+    }
+    
+    public function getWidgetFooterUrl($parameters) {
+        return RevenueUrlService::getFooterUrl($parameters,$this->getLegacyNodeId());
+    }
+}
