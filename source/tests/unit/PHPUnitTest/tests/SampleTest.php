e3784b8d01e77ff9d3df12ab18db8812ec8804d2
Alekhya Myanani <amyanani@reisystems.com>
2017-01-11 17:01:08 -0500

    Sample PHP unit test
    Former-commit-id: 97a4a20d4d9ba04f0797b428b568a5aeaefb5e1c [formerly 97a4a20d4d9ba04f0797b428b568a5aeaefb5e1c [formerly 97a4a20d4d9ba04f0797b428b568a5aeaefb5e1c [formerly 8a2d901cc36aabb94aedea80e88ee98246ae46a7]]]
    Former-commit-id: ef26f38824083bbb6b89ad37853ac09a05755f37
    Former-commit-id: db3eb617423a4cc70967d199d078a6fb49c36ea4
    Former-commit-id: fc70241c5c4b6d8c80e6568ee3bc1d3bce9b94e7

---
+++ source/tests/unit/PHPUnitTest/tests/SampleTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amyanani
+ * Date: 1/3/17
+ * Time: 3:58 PM
+ */
+
+class SampleTest extends PHPUnit_Framework_TestCase {
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
+ 
