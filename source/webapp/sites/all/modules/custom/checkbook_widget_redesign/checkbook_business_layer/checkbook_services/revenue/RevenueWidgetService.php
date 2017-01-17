41345ff4fbef5e5af10e605d0672621f4347a327
Isaac Kim <isaac.kim@reisystems.com>
2017-01-17 10:56:29 -0500

    Merge

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/revenue/RevenueWidgetService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/revenue/RevenueWidgetService.php
@@ -12,28 +12,51 @@ class RevenueWidgetService extends AbstractWidgetService {
         $value = null;
         $legacy_node_id = $this->getLegacyNodeId();
         switch($column_name) {
-           case "agency_name_link":
+            case "agency_name_link":
                 $column = $row['agency_name'];
-                $url = '';
+                $url = '/revenue'.RequestUtilities::_getUrlParamString('year')
+                  .RequestUtilities::_getUrlParamString('yeartype')
+                  .'/agency/'.$row['agency_id'];
                 $value = "<a href='{$url}'>{$column}</a>";
                 break;
+            //url needs to be built for below cases
             case "recognized_amount_link":
-                $column = $row['recognized_amount'];
-                $url = '';
+                $column = $row['revenue_amount_sum'];
+                $url = '/revenue/transactions'.RequestUtilities::_getUrlParamString('year');
+                  //?
                 $value = "<a href='{$url}'>{$column}</a>";
-            break;
+                break;
+            case "current_recognized_link":
+                $column = $row['current_recognized'];
+                $url = '/revenue/transactions'.RequestUtilities::_getUrlParamString('year');
+                  //?
+                $value = "<a href='{$url}'>{$column}</a>";
+                break;
+            case "reognized_1_link":
+                $column = $row['recognized_1'];
+                $url = '/revenue/transactions'.RequestUtilities::_getUrlParamString('year');
+                  //?
+                $value = "<a href='{$url}'>{$column}</a>";
+                break;
+            case "recognized_2_link":
+                $column = $row['recognized_2'];
+                $url = '/revenue/transactions'.RequestUtilities::_getUrlParamString('year');
+                  //?
+                $value = "<a href='{$url}'>{$column}</a>";
+                break;
+
         }
-        
+
         if(isset($value)) {
             return $value;
         }
         return $value;
     }
-    
+
     public function adjustParameters($parameters, $urlPath) {
         return $parameters;
     }
-    
+
     public function getWidgetFooterUrl($parameters) {
         return RevenueUrlService::getFooterUrl($parameters,$this->getLegacyNodeId());
     }
