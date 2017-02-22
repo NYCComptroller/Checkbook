c0d47a7de9914c60e8eb404eb01dd8e40d2c641b
Saroja Gade <sgade@reisystems.com>
2017-02-22 13:59:33 -0500

    NYCCHKBK-6992: Budget: File Structure Updates

---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/budget/BudgetUrlService.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sgade
+ * Date: 02/22/2017
+ * Time: 1:16 PM
+ */
+class BudgetUrlService {
+    /**
+     * @param $parameters
+     * @param null $legacy_node_id
+     * @return string
+     */
+    static function getFooterUrl($parameters,$legacy_node_id = null) {
+        $url = "";
+        return $url;
+    }
+    
+}
