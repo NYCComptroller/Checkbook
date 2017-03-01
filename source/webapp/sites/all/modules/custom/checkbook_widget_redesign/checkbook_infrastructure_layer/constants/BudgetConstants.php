7183faad4fc216541ca344d39364276296f2e373
Saroja Gade <sgade@reisystems.com>
2017-03-01 16:29:00 -0500

    NYCCHKBK-7007: Top 5 Agencies by Percent Difference - Implementation Refactored visibility rules Former-commit-id: 4cbb537ef95ef8fecedd878d0c6c2fd91a3a81e4 [formerly 4cbb537ef95ef8fecedd878d0c6c2fd91a3a81e4 [formerly 4cbb537ef95ef8fecedd878d0c6c2fd91a3a81e4 [formerly 91df71c9a490863336a48517398c65fdc6214c3b]]] Former-commit-id: 4689371c75ec0bce0eccf9abb7527032d9f244a9 Former-commit-id: c779e62a220b4b995fa1f81d392830453a779bf1 Former-commit-id: 754bc0fb92fd5b974307f853c18ab16cc5cccea4

---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/BudgetConstants.php
@@ -0,0 +1,74 @@
+<?php
+
+abstract class BudgetConstants{
+    const BASE_YEAR_ID = 112;
+    static public function getCurrent() {
+        return self::BASE_YEAR_ID;
+    }
+}
+
+abstract class BudgetAgencyPercentWidgetViews {
+
+    const AGENCIES_PERCENT_DIFF = "percent_difference_by_agencies";
+    const AGENCIES_PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_agencies_previous_1";
+    const AGENCIES_PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_agencies_previous_2";
+    
+    static public function getCurrent() {
+        $base_year = BudgetConstants::getCurrent();
+        $year = RequestUtilities::getRequestParamValue(UrlParameter::YEAR);
+
+        if($year == $base_year + 1) {
+            return self::AGENCIES_PERCENT_DIFF_PREVIOUS_1;
+        }
+        else if($year == $base_year + 2) {
+            return self::AGENCIES_PERCENT_DIFF_PREVIOUS_2;
+        }
+        else if($year > $base_year) {
+            return self::AGENCIES_PERCENT_DIFF;
+        }
+    }
+}
+
+abstract class BudgetDepartmentPercentWidgetViews {
+    const DEPT_PERCENT_DIFF = "percent_difference_by_depatments";
+    const DEPT_PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_depatments_previous_1";
+    const DEPT_PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_depatments_previous_2";
+    
+    static public function getCurrent() {
+        $base_year = BudgetConstants::getCurrent();
+        $year = RequestUtilities::getRequestParamValue(UrlParameter::YEAR);
+
+        if($year == $base_year + 1) {
+            return self::DEPT_PERCENT_DIFF_PREVIOUS_1;
+        }
+        else if($year == $base_year + 2) {
+            return self::DEPT_PERCENT_DIFF_PREVIOUS_2;
+        }
+        else if($year > $base_year) {
+            return self::DEPT_PERCENT_DIFF;
+        }
+    }
+}
+
+abstract class BudgetCatExpensePercentWidgetViews {
+
+    const EXPCAT_PERCENT_DIFF = "percent_difference_by_expense_categories";
+    const EXPCAT_PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_expense_categories_previous_1";
+    const EXPCAT_PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_expense_categories_previous_2";
+    
+    static public function getCurrent() {
+        $base_year = BudgetConstants::getCurrent();
+        $year = RequestUtilities::getRequestParamValue(UrlParameter::YEAR);
+
+        if($year == $base_year + 1) {
+            return self::EXPCAT_PERCENT_DIFF_PREVIOUS_1;
+        }
+        else if($year == $base_year + 2) {
+            return self::EXPCAT_PERCENT_DIFF_PREVIOUS_2;
+        }
+        else if($year > $base_year) {
+            return self::EXPCAT_PERCENT_DIFF;
+        }
+    }
+}
+
