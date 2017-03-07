45fdb58a59ff8ca0622735b7dccb40c38187d6f7
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2017-03-07 15:43:25 -0600

    Hack to include javascript and show charts
    Manually include javascript libraries so that charts show up on pages
    like localhost/spending_landing/yeartype/B/year/113#.  This is not how
    this should be done, and continues to have problems where certain
    javascript is not loaded.  The next step is to figure out why Drupal
    isn't loading the js libraries as it should be, and to fix that -- and
    then probably to revert this commit.
    
    Note that some of the elements of $node that were being checked don't
    seem to exist (e.g. $node->widgetConfig->chartConfig->title->text was
    always empty, even though $node->widgetConfig->chartTitle had a value).
    I'm not sure if that's a problem with my local copy or if they've been
    deprecated.
    
    I set `var chart` equal to null before setting it to a new Highcharts
    Chart object just to avoid having any problem with initializing
    Highcharts twice (if it somehow was initialized before being loaded in
    this file).
    
    Plenty of things are still broken in this state, though my local
    deployment *mostly* works now.  The Filter dropdown doesn't work, the
    citywide agencies and other government entities lists are empty, and the
    search doesn't run.

--- source/webapp/sites/all/modules/custom/widget_framework/widget_highcharts/highcharts.tpl.php
+++ source/webapp/sites/all/modules/custom/widget_framework/widget_highcharts/highcharts.tpl.php
@@ -20,16 +20,19 @@
 ?>
 <?php print eval($node->widgetConfig->header);  ?>
 
-<?php if(($node->totalDataCount == 0 && isset($node->totalDataCount) ) || count($node->data) == 0 ) {  ?>
+<?php
+if(count($node->data) == 0 ) {
+?>
 <div id="node-chart-<?php print widget_unique_identifier($node)?>" class="highcharts-wrapper">
-  <?php if(isset($node->widgetConfig->chartConfig->title->text ))  {?>
-  	<h2  class="text-center"><?php echo $node->widgetConfig->chartConfig->title->text ?></h2>
+  <?php if(isset($node->widgetConfig->chartTitle ))  {?>
+  	<h2  class="text-center"><?php echo $node->widgetConfig->chartTitle ?></h2>
   <?php } ?>
   <div class="clearfix" id="no-records"><span>There is no data for this visualization.</span></div>
  
 </div>
 
-<?php } else{ 
+<?php } else {
+    
 
   
   ?>
@@ -37,14 +40,22 @@
 <div id="node-chart-<?php print widget_unique_identifier($node)?>" class="highcharts-wrapper <?php echo $node->widgetConfig->html_class ?>">
 &nbsp
 </div>
-<?php print eval($node->widgetConfig->footer );  ?>
+<?php
+print eval($node->widgetConfig->footer );  ?>
 </div>
   <?php if ($node->widgetConfig->deferredRendering == TRUE){
     widget_highcharts_add_js_setting($node);
-  } else {?>
+  } else {
+?>
+<script src="http://localhost/sites/all/modules/custom/checkbook_datafeeds/js/multi-select/js/jquery.js"></script>
+    <script src="http://localhost/sites/all/modules/custom/widget_framework/widget_highcharts/highstock/1.2.4/js/highstock.js"></script>
+        <script src="http://localhost/sites/all/modules/custom/widget_framework/widget_highcharts/highstock/1.2.4/js/highstock.js"></script>
+    <script src="http://localhost/sites/all/modules/custom/widget_framework/widget_highcharts/highcharts-globals.js"></script>
 <script type="text/javascript">
 jQuery(document).ready(function() {
-  var chart = new Highcharts.Chart(<?php print widget_mergeJSFunctions($node, $node->widgetConfig->chartConfig); ?><?php if ($node->widgetConfig->callback){ print ',function(chart){'.$node->widgetConfig->callback.'}';}?>);
+    console.log("DEBUG: inside the jquery ready function");
+    var chart = null;
+    chart = new Highcharts.Chart(<?php print widget_mergeJSFunctions($node, $node->widgetConfig->chartConfig); ?><?php if ($node->widgetConfig->callback){ print ',function(chart){'.$node->widgetConfig->callback.'}';}?>);
         Highcharts.chartarray.push(chart); //array defined in highcharts-globals.js
 
     //Display grid view link
@@ -56,7 +67,6 @@ jQuery(document).ready(function() {
 
 <script type="text/javascript">
 jQuery(document).ready(function() {
-
     jQuery('.chart-grid-view').show();
 });
 </script>
