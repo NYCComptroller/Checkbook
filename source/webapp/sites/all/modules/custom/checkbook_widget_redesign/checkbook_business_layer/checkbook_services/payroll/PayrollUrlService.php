52b6727e726b84c07830cdc4ac0f51b82cacaf79
Isaac Kim <isaac.kim@reisystems.com>
2017-03-16 15:28:34 -0400

    merging

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/payroll/PayrollUrlService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/payroll/PayrollUrlService.php
@@ -1,17 +1,45 @@
 <?php
 
-class PayrollUrlService {
+class PayrollUrlService
+{
 
-  /**
-   * @param $parameters
-   * @param null $legacy_node_id
-   * @return string
-   */
-  static function getFooterUrl($legacy_node_id = null) {
-    $url = '/panel_html/payroll_nyc_transactions'.'/smnid/'.$legacy_node_id;
-    $url .= RequestUtilities::_getUrlParamString('yeartype');
-    $url .= RequestUtilities::_getUrlParamString('year');
-    return $url;
-  }
+    /**
+     * @param $parameters
+     * @param null $legacy_node_id
+     * @return string
+     */
+    static function getFooterUrl($legacy_node_id = null)
+    {
+        $legacy_node_id = isset($legacy_node_id) ? '/smnid/' . $legacy_node_id : '';
+
+        $url = '/panel_html/payroll_nyc_transactions/payroll/transactions'
+            . RequestUtilities::_getUrlParamString('yeartype')
+            . RequestUtilities::_getUrlParamString('year')
+            . $legacy_node_id;
+        return $url;
+    }
+
+    static function agencyNameUrl($agency_id)
+    {
+        $url = "/payroll/agency_landing"
+            . RequestUtilities::_getUrlParamString("yeartype")
+            . RequestUtilities::_getUrlParamString("year")
+            . RequestUtilities::_getUrlParamString("agency")
+            . "/agency/" . $agency_id;
+        return $url;
+    }
+
+    static function payUrl($dynamic_parameter, $legacy_node_id = null)
+    {
+        $legacy_node_id = isset($legacy_node_id) ? '/smnid/' . $legacy_node_id : '';
+        $dynamic_parameter = isset($dynamic_parameter) ? $dynamic_parameter : '';
+
+        $url = "/panel_html/payroll_agencytransactions/payroll/agencywide/transactions"
+            . RequestUtilities::_getUrlParamString("yeartype")
+            . RequestUtilities::_getUrlParamString("year")
+            . $dynamic_parameter
+            . $legacy_node_id;
+        return $url;
+    }
 
 }
