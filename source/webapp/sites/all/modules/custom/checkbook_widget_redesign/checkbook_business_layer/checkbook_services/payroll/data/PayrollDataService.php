eb4ec0159df7616cbef9dcdb164d61b5f3058184
Alicia Torkelson <atorkelson@reisystems.com>
2017-03-16 13:04:55 -0400

    NYCCHKBK-7277 - Updated the Payroll by Agencies widget/Sql to handle the query for salaried/non-salaried employees.  Added a separate count Sql for Agencies count to improve performance. Added number formatting for salaried fields.
    Former-commit-id: a227f50275d47a136cf861bc65e387a8a7941c2a [formerly a227f50275d47a136cf861bc65e387a8a7941c2a [formerly a227f50275d47a136cf861bc65e387a8a7941c2a [formerly d4aa5e44da9b14bc8421255c1410f84a39d4df2a]]]
    Former-commit-id: 5870f55cc25810c8030829447402d6bccdd45bf7
    Former-commit-id: 5d85b503817b4978f55630272e024ee33ccfcd38
    Former-commit-id: 2521d186035fd51c5316627fcac9c2cf7850b736

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/payroll/data/PayrollDataService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/payroll/data/PayrollDataService.php
@@ -2,35 +2,39 @@
 
 class PayrollDataService extends DataService implements IPayrollDataService {
 
-  function GetAgenciesByOvertime($parameters, $limit = null, $orderBy = null) {
-    return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
-  }
+    function GetAgenciesByOvertime($parameters, $limit = null, $orderBy = null) {
+        return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
+    }
 
-  function GetAgenciesByPayroll($parameters, $limit = null, $orderBy = null) {
-    return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
-  }
+    function GetAgenciesByPayroll($parameters, $limit = null, $orderBy = null) {
+        return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
+    }
 
-  function GetAnnualSalariesPerAgency($parameters, $limit = null, $orderBy = null) {
-    return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
-  }
+    function GetAnnualSalariesPerAgency($parameters, $limit = null, $orderBy = null) {
+        return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
+    }
 
-  function GetAnnualSalaries($parameters, $limit = null, $orderBy = null) {
-    return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
-  }
+    function GetAnnualSalaries($parameters, $limit = null, $orderBy = null) {
+        return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
+    }
 
-  function GetTitlesByNumberOfEmployees($parameters, $limit = null, $orderBy = null) {
-    return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
-  }
+    function GetTitlesByNumberOfEmployees($parameters, $limit = null, $orderBy = null) {
+        return $this->configureCitywide(__FUNCTION__, $parameters, $limit, $orderBy);
+    }
 
-  /**
-   * Common function that automatically configures the Citywide Budget sql
-   * @param $dataFunction
-   * @param $parameters
-   * @param null $limit
-   * @param null $orderBy
-   * @return DataService
-   */
-  private function configureCitywide($dataFunction, $parameters, $limit = null, $orderBy = null) {
-    return $this->configure($dataFunction,$parameters,$limit,$orderBy,SqlConfigPath::CitywidePayroll);
-  }
+    function GetCountAgencies($parameters) {
+        return $this->configureCitywide(__FUNCTION__, $parameters);
+    }
+
+    /**
+     * Common function that automatically configures the Citywide Budget sql
+     * @param $dataFunction
+     * @param $parameters
+     * @param null $limit
+     * @param null $orderBy
+     * @return DataService
+     */
+    private function configureCitywide($dataFunction, $parameters, $limit = null, $orderBy = null) {
+        return $this->configure($dataFunction, $parameters, $limit, $orderBy, SqlConfigPath::CitywidePayroll);
+    }
 }
