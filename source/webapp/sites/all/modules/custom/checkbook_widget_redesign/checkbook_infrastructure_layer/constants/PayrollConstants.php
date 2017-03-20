469a14debe91b43ba7a37c8be75ee782da689d0f
Alicia Torkelson <atorkelson@reisystems.com>
2017-03-20 17:58:52 -0400

    NYCCHKBK-7272 - Updated the agency url to persist title NYCCHKBK-7272 - Fixed the Visibility rules to include the landing page as a dimension of payroll

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/PayrollConstants.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/PayrollConstants.php
@@ -1,8 +1,28 @@
 <?php
 
+abstract class PayrollLandingPage {
 
-abstract class PayrollDimension {
+    const NYC_LEVEL = "nyc_landing";
+    const AGENCY_LEVEL = "agency_landing";
+    const TITLE_LEVEL = "title_landing";
 
-  const TITLE = "title";
+    static public function getCurrent() {
 
+        $urlPath = $_GET['q'];
+        $ajaxPath = $_SERVER['HTTP_REFERER'];
+        $page = null;
+
+        if(preg_match('/payroll/',$urlPath) || preg_match('/payroll/', $ajaxPath)) {
+            if(preg_match('/agency_landing/',$urlPath) || preg_match('/agency_landing/', $ajaxPath)) {
+                $page = self::AGENCY_LEVEL;
+            }
+            else if(preg_match('/title_landing/',$urlPath) || preg_match('/title_landing/', $ajaxPath)) {
+                $page = self::TITLE_LEVEL;
+            }
+            else {
+                $page = self::NYC_LEVEL;
+            }
+        }
+        return $page;
+    }
 }
