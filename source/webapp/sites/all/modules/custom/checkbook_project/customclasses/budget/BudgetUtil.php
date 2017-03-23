991b05cdfcbe03aea65500f0c5802b15cd8f8e16
Saroja Gade <sgade@reisystems.com>
2017-03-23 16:57:30 -0400

    NYCCHKBK-7379: Fixed issue with apostrophe in DB query

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/budget/BudgetUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/budget/BudgetUtil.php
@@ -33,7 +33,7 @@ class BudgetUtil{
     static public function getBudgetCodeId($budget_code_name, $budget_code, $year){
         $query = "SELECT DISTINCT budget_code_id FROM budget
                   WHERE budget_code = '". trim($budget_code). "'"
-                ." AND budget_code_name ILIKE '". $budget_code_name . "'"
+                ." AND budget_code_name ILIKE '". addslashes(trim($budget_code_name)) . "'"
                 ." AND budget_fiscal_year_id = ".$year;
 
         $results = _checkbook_project_execute_sql_by_data_source($query,'checkbook');
