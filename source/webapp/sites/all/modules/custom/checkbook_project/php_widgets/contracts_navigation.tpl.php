451f8cda5cfddfe56dda4fe647244a1dfe1f10e2
Saroja Gade <sgade@reisystems.com>
2017-06-19 14:36:34 -0400

    NYCCHKBK-7658: Fixed Contracts-M/WBE(Subvendors) navigation issue when amount is zero

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
@@ -210,7 +210,7 @@ if($mwbe_amount  == 0 && $mwbe_amount_active_inc == 0){
 }else{
     //Contracts-M/WBE(Subvendors) should be navigated to third bottom slider when amount is zero
     if((!isset($mwbe_amount) || $mwbe_amount == 0) && preg_match('/contract/',$_GET['q']) && !_checkbook_check_isEDCPage() && RequestUtil::getDashboardTopNavTitle("mwbe") == 'M/WBE (Sub Vendors)'){
-        $mwbe_active_domain_link = ContractURLHelper::prepareSubvendorContractsSliderFilter($mwbe_active_domain_link, $dashboard, TRUE);
+        $mwbe_active_domain_link = ContractURLHelper::prepareSubvendorContractsSliderFilter($mwbe_active_domain_link, 'ms', TRUE);
     }
     $mwbe_link = l('<div><div class="top-navigation-amount"><span class="nav-title">' . RequestUtil::getDashboardTopNavTitle("mwbe") . '</span><br>&nbsp;'. custom_number_formatter_format($mwbe_amount ,1,'$') . '</div></div>',$mwbe_active_domain_link,$options);	
 }
