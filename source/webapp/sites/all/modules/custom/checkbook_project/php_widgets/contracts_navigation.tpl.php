fe03b21db61debf8cdf2fc1f5f90685f7f9af6ef
Saroja Gade <sgade@reisystems.com>
2017-06-19 14:29:25 -0400

    NYCCHKBK-7658: Fixed Contracts-M/WBE(Subvendors) navigation issue when amount is zero Former-commit-id: 55b87a5a9397e6245839b368f75271504f6a63f0 [formerly 55b87a5a9397e6245839b368f75271504f6a63f0 [formerly 55b87a5a9397e6245839b368f75271504f6a63f0 [formerly 13d588dfcf49ec51cc944e89ed0f0f93521830de]]] Former-commit-id: 84934cc3b63a7f26f734d94c5e25a5416f9beda6 Former-commit-id: 5af6255eb9162c22992ba4d871835322d658fc10 Former-commit-id: a420fdc98e149aa5a4ef1605b3f5cedceb43f6dd

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
@@ -203,14 +203,19 @@ if(!preg_match('/smnid/',$_GET['q']) && (preg_match('/spending\/transactions/',$
 	$mwbeclass = ' ';
 }
 
+$featured_dashboard = _getRequestParamValue("dashboard");
+
 if($mwbe_amount  == 0 && $mwbe_amount_active_inc == 0){
     $mwbe_link = l('<div><div class="top-navigation-amount"><span class="nav-title">' . RequestUtil::getDashboardTopNavTitle("mwbe") . '</span><br>&nbsp;'. custom_number_formatter_format(0 ,1,'$') . '</div></div>','',$options_disabled);	
-}else{	
+}else{
+    //Contracts-M/WBE(Subvendors) should be navigated to third bottom slider when amount is zero
+    if((!isset($mwbe_amount) || $mwbe_amount == 0) && preg_match('/contract/',$_GET['q']) && !_checkbook_check_isEDCPage() && RequestUtil::getDashboardTopNavTitle("mwbe") == 'M/WBE (Sub Vendors)'){
+        $mwbe_active_domain_link = ContractURLHelper::prepareSubvendorContractsSliderFilter($mwbe_active_domain_link, $dashboard, TRUE);
+    }
     $mwbe_link = l('<div><div class="top-navigation-amount"><span class="nav-title">' . RequestUtil::getDashboardTopNavTitle("mwbe") . '</span><br>&nbsp;'. custom_number_formatter_format($mwbe_amount ,1,'$') . '</div></div>',$mwbe_active_domain_link,$options);	
 }
 
 $indicator_left = true;
-$featured_dashboard = _getRequestParamValue("dashboard");
 if($featured_dashboard != null){
     $indicator_left = false;
 }else{
@@ -219,9 +224,7 @@ if($featured_dashboard != null){
 
 if($svendor_amount  == 0 && $svendor_amount_active_inc == 0){
     if($svendor_amount_active_inc == 0 && preg_match('/contract/',$_GET['q']) && !_checkbook_check_isEDCPage()){
-        
         $dashboard = (isset($featured_dashboard) && $featured_dashboard == 'mp')? 'sp': 'ss';
-        
         $svendor_active_domain_link = ContractURLHelper::prepareSubvendorContractsSliderFilter('contracts_landing', $dashboard, TRUE);
         $subvendors_link = l('<div><div class="top-navigation-amount"><span class="nav-title">' .RequestUtil::getDashboardTopNavTitle("subvendor")  .'</span><br>&nbsp;'. custom_number_formatter_format($svendor_amount ,1,'$') . '</div></div>',$svendor_active_domain_link ,$options);	 
     }else{
