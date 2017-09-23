8356ee383d4cc17878cfb98a8486fa3fdb4f4f54
Saroja Gade <sgade@reisystems.com>
2017-09-23 10:58:33 -0400

    NYCCHKBK-7727 Data Feeds - Budget Code Search Enhancement: NYCCHKBK-7828 changes to results dataset
    Former-commit-id: 5de6a6fc2e6ad7dee5d138b13756f3f549891446 [formerly 5f5a930825201a19fa65f52046e7349caff47d2a]
    Former-commit-id: f89e500f0f7bc43f61a20ba5d371218e84b7882c
    Former-commit-id: e2aa8299c95b224179d5c44a17f9732d0ba09068
    Former-commit-id: e84ec89940b035204a558f3c0fdf7f7ca7368cc3

--- source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/budget.js
+++ source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/budget.js
@@ -1,13 +1,5 @@
 (function ($) {
-    $.fn.disableStuff = function(){
-        if ($('#edit-agency').val() === 'Citywide (All Agencies)'){
-            $('select[name="dept"]').val('');
-            $('select[name="dept"]').attr('disabled','disabled');
-            $('select[name="expense_category"]').val('');
-            $('select[name="expense_category"]').attr('disabled','disabled');
-        }
-    }
-    
+
     $.fn.reloadDepartment = function(){
        var agency = encodeURIComponent($('#edit-agency').val());
        var year = ($('#edit-fiscal-year').val() === 'All Years') ? 0 : $('#edit-fiscal-year').val();
@@ -26,7 +18,7 @@
                 }
             }); 
         }else{
-            $('select[name="dept"]').val('');
+            $('select[name="dept"]').append('<option value="" selected="selected">Select Department</option>');
             $('select[name="dept"]').attr('disabled','disabled');
         }
     }
@@ -50,7 +42,7 @@
                 }
             }); 
         }else{
-            $('select[name="expense_category"]').val('');
+            $('select[name="expense_category"]').append('<option value="" selected="selected">Select Expense Category</option>');
             $('select[name="expense_category"]').attr('disabled','disabled');
         }
     }
@@ -59,14 +51,14 @@
        var agency = encodeURIComponent($('#edit-agency').val());
        var dept = encodeURIComponent($('#edit-dept').val());
        var expCategory = encodeURIComponent($('#edit-expense-category').val());
-       var budgetName = encodeURIComponent(($('#edit-budget-name').val() === "") ? 0 : $('#edit-budget-name').val());
-       var budgetCode = ($('#edit-budget-code').val() === "") ? 0 : $('#edit-budget-code').val();
+       var budgetName = ($('#edit-budget-name').val()) ? encodeURIComponent($('#edit-budget-name').val()) : 0;
+       var budgetCode = $('#edit-budget-code').val();
        var year = ($('#edit-fiscal-year').val() === 'All Years') ? 0 : $('#edit-fiscal-year').val();
        
        $.ajax({
             url: '/datafeeds/budget/budgetcode/' + year + '/' + agency + '/' + dept + '/' + expCategory + '/' + budgetName,
             success: function(data) {
-                var html = '<option value="" >Select Budget Code</option>';
+                var html = '<option value="0" >Select Budget Code</option>';
                 if(data[0]){
                     for (i = 0; i < data.length; i++) {
                         html = html + '<option value="' + data[i] + ' ">' + data[i]  + '</option>';
@@ -104,6 +96,8 @@
 
     Drupal.behaviors.budgetDataFeeds = {
         attach:function(context,settings){
+            $.fn.reloadDepartment();
+            $.fn.reloadExpenseCategory();
             $.fn.reloadBudgetCode();
             $.fn.reloadBudgetName();
             $('select[name="budget_code"]').chosen({
@@ -144,7 +138,6 @@
             });
             
             // Sets up multi-select/option transfer
-            //Expense
             $('#edit-column-select-expense',context).multiSelect();
             $('#ms-edit-column-select-expense .ms-selectable .ms-list',context).after('<a class="select">Add All</a>');
             $('#ms-edit-column-select-expense .ms-selection .ms-list',context).after('<a class="deselect">Remove All</a>');
@@ -154,10 +147,7 @@
             $('#ms-edit-column-select-expense a.deselect',context).click(function(){
                 $('#edit-column-select-expense',context).multiSelect('deselect_all');
             });
-            
-            //Sets up jQuery UI autocompletes and autocomplete filtering functionality
-            $.fn.disableStuff();
-            
+
             $(':input[name="budgettype"]',context).change(function(){
                 $('#edit-column-select-expense',context).multiSelect('deselect_all');
             });
