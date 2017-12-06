21083968c1a9c96b69fb9ef6243e03cb270d693d
Alex Perfilov <alexandr.perfilov@reisystems.com>
2017-12-06 15:16:21 -0500

    -Composer
    Former-commit-id: 3cab02c9b893fd4b1d3fcf385b0113a9e7070e05 [formerly 9b38d3fc82edc5f8975cc2220032c960ff68debe]
    Former-commit-id: 57af201dcc13aa7f703f0cb6d70697c6aa363e96
    Former-commit-id: c2d77475a7d6efda0d4c351b81559c38aaeef089
    Former-commit-id: 01feb2aafa9f75c566f570a11e54b73bd15086db

--- source/webapp/index.php
+++ source/webapp/index.php
@@ -16,8 +16,6 @@
  */
 define('DRUPAL_ROOT', getcwd());
 
-require_once DRUPAL_ROOT . '/vendor/autoload.php';
-
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
