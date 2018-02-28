a523e3bbb5b371851032f5d26f59b0f46f3272b9
Snigdha Madhavaram <snigdha.madhavaram@reisystems.com>
2018-02-28 10:43:21 -0500

    fixed duplicate departments
    Former-commit-id: 517d6bc8fd223f46feaa76112ce60309d7b5fe46 [formerly 70cc7d5e2bcdd2a9fed957bc0d3e36a4a28bdccd]
    Former-commit-id: 457c4cf13d07aeb090d9321fc20b2ec9152d5487
    Former-commit-id: 4b8109d0f4e5c25c3cd5a824fe5e77979697afee
    Former-commit-id: f979c8ad20db6398fb652b26e8bb0a96c5f85342

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/spending/summary/dept_summary.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/spending/summary/dept_summary.tpl.php
@@ -1,19 +1,19 @@
 <?php
 /**
 * This file is part of the Checkbook NYC financial transparency software.
-* 
+*
 * Copyright (C) 2012, 2013 New York City
-* 
+*
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
-* 
+*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU Affero General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU Affero General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -24,15 +24,34 @@ if(is_array($records)){
     $row = $records[0];
     $title = eval($node->widgetConfig->summaryView->templateTitleEval);
     $label = $node->widgetConfig->summaryView->templateLabel;
-    $value = eval($node->widgetConfig->summaryView->templateLabelEval);
+    //$value = eval($node->widgetConfig->summaryView->templateLabelEval);
     $ytdspending = WidgetUtil::getLabel("ytd_spending");
     $dept = WidgetUtil::getLabel("dept_name");
     $percent_spending = WidgetUtil::getLabel("percent_spending");
     $percent_spending_value = $row['percent_spending'];
-    if(_getRequestParamValue('smnid') == 29){
+    if(_getRequestParamValue('smnid') == 29) {
         $percent_spending = '';
         $percent_spending_value = '';
+
+        $agency_id = _getRequestParamValue('agency');
+        $type_of_year = _getRequestParamValue('yeartype');
+        $year_id = _getRequestParamValue('year');
+        $deptcode = _getRequestParamValue('dept');
+        $dept = "'".$deptcode."'";
     }
+       $query = "SELECT  j.agency_agency, j.department_department,j1.department_name AS department_department_department_name
+                  FROM (SELECT s0.agency_id AS agency_agency,s0.department_code AS department_department
+                        FROM aggregateon_spending_coa_entities s0
+                        WHERE s0.agency_id = ".$agency_id."
+                        AND s0.year_id = ".$year_id."AND s0.department_code = ".$deptcode."
+                        GROUP BY s0.agency_id, s0.department_code, s0.year_id) j
+                 LEFT OUTER JOIN ref_department j1 ON j1.department_code = j.department_department and j1.agency_id = j.agency_agency
+                  LIMIT 1";
+        $result = _checkbook_project_execute_sql_by_data_source($query,'checkbook');
+
+
+   $value = $result[0]['department_department_department_name'].$result['agency_agency'];
+
 $summaryContent =  <<<EOD
 <div class="contract-details-heading">
 	<div class="contract-id">
