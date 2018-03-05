00220b2d7d88202ed37f7a2c93f8002e8b309d33
Alex Perfilov <alexandr.perfilov@reisystems.com>
2018-03-05 14:23:47 -0500

    Anchors fix
    Former-commit-id: f01f4c34984b487981f7ccee7e275ab4dbf9243d [formerly d554ffd1857c4afe895e2ac8d4ef968b71844f7d]
    Former-commit-id: 570b57a20ab3388eaf810c035d348b471a0e170f
    Former-commit-id: 1700f1cdcf56f91bf1f9b31695880134f6497dc7
    Former-commit-id: 0976e656e8998164fc5d7848e87dc7a410fcf783

--- source/webapp/sites/all/modules/custom/checkbook_project/js/featured-trends.js
+++ source/webapp/sites/all/modules/custom/checkbook_project/js/featured-trends.js
@@ -57,19 +57,19 @@
                 startingSlide: slide,
                 pagerAnchorBuilder:function (i, DOMElement) {
                     switch (i) {
-                        case '0':
+                        case 0:
                             return '<div id="general-fund" class="first navigation">General Fund Revenues and<br>General Fund Expenditures</div>';
                             break;
-                        case '1':
+                        case 1:
                             return '<div id="property-tax" class="navigation">Property Tax Levies<br>and Collections</div>';
                             break;
-                        case '2':
+                        case 2:
                             return '<div id="capital-projects" class="navigation">Capital Projects Fund<br>Aid Revenues</div>';
                             break;
-                        case '3':
+                        case 3:
                             return '<div id="personal-income" class="navigation">Personal Income</div>';
                             break;
-                        case '4':
+                        case 4:
                             return '<div id="debt-ratio" class="last navigation">Ratios of Outstanding Debt<br>by Type</div>';
                             break;
                     }
@@ -78,24 +78,23 @@
         );
     });
     
-    
     $('#featured-trends-thumbnails div.navigation').live("click",function (e) {
-        if(this.id === 'general-fund')
-    		$('#breadcrumb span.last').text("General Fund Revenues and General Fund Expenditures");
-        else if(this.id === 'property-tax')
-        	$('#breadcrumb span.last').text("Property Tax Levies and Collections");
-        else if(this.id === 'capital-projects')
-        	$('#breadcrumb span.last').text("Capital Projects Fund Aid Revenues");
-        else if(this.id === 'personal-income')
-        	$('#breadcrumb span.last').text("Personal Income");
-        else if(this.id === 'debt-ratio')
-        	$('#breadcrumb span.last').text("Ratios of Outstanding Debt by Type");
+        switch(this.id){
+            case 'general-fund':
+                $('#breadcrumb span.last').text("General Fund Revenues and General Fund Expenditures"); break;
+            case 'property-tax':
+                $('#breadcrumb span.last').text("Property Tax Levies and Collections"); break;
+            case 'capital-projects':
+                $('#breadcrumb span.last').text("Capital Projects Fund Aid Revenues"); break;
+            case 'personal-income':
+                $('#breadcrumb span.last').text("Personal Income"); break;
+            case 'debt-ratio':
+            default:
+                $('#breadcrumb span.last').text("Ratios of Outstanding Debt by Type");
+        }
         return true;
+    })
 
-    })    
-    
-    
-    
 }(jQuery));
 
 function isValidYear(n, floor) {
