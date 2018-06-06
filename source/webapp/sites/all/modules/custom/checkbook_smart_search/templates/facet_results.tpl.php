8fe481b13adde2f1f187a72e3dd0adb03d80608f
Alex Perfilov <alexandr.perfilov@reisystems.com>
2018-06-06 19:10:39 -0400

    Fix smart search facet unlimited checkbox ids fix

--- source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/facet_results.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/facet_results.tpl.php
@@ -1,19 +1,19 @@
 <?php
 /**
 * This file is part of the Checkbook NYC financial transparency software.
-* 
+*
 * Copyright (C) 2012, 2013 New York City
-* 
+*
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
-* 
+*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU Affero General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU Affero General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -29,8 +29,9 @@
 <div class="rows">
   <?php
   $ct = 0;
+  $page = isset($page)?$page:rand(7,999);
   foreach ($results as $key => $value){
-      $id = $name."_checked_".$ct;
+      $id = $name.'_checked_'.$page.'_'.$ct++;
       echo '<div class="row">';
       echo '<div class="checkbox"><input type="checkbox" id="'.$id.'" value="'.urlencode($key).'" name="'.$name.'" onClick="javascript:applySearchFilters();"><label for="'.$id.'"></label></div>';
       echo '<div class="name">'.$key.'</div>';
