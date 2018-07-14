3b8fc888e0e80f17ce2252450628c86c3d07a47e
Saroja Gade <sgade@reisystems.com>
2018-07-14 19:10:23 -0400

    NYCCHKBK-8530: NYCHA changes for Agencies list, visualizations, top navigation and dashboard platform
    Former-commit-id: 67fd4b1918bf98eaefd6bc2c8664fcd8d1ceded4 [formerly 0480ff9263b8a5e91fc3b55aeb6ecbd07c134c6f]
    Former-commit-id: 2d5475e8abe4c4b583e7cfcea4a6087a057bdf52
    Former-commit-id: c6b0d7fc7d12fe638a1a699e560ece5758eb5b82
    Former-commit-id: c55ee0a28bee377f0762fcfef4e59adf384c9c0e

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/agencies_list.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/agencies_list.tpl.php
@@ -21,14 +21,17 @@
 
 $city_agencies = array();
 $edc_agencies = array();
+$nycha_agencies= array();
 foreach($node->data as $key=>$value){
-    if(array_key_exists('is_oge_agency', $value)){
+    if($value['is_oge_agency'] == 'Y'){
         $edc_agencies[$key] = $value;
-    }else{
+    }else if($value['is_oge_agency'] == 'N'){
         $city_agencies[$key] = $value;
+    }else{
+        $nycha_agencies[$key] = $value;
     }
 }
-
+//log_error($city_agencies);log_error($nycha_agencies);
 $oge_filter_highlight = (_checkbook_check_isEDCPage()) ? 'agency_filter_highlight' : '';
 $city_filter_highlight = (!_checkbook_check_isEDCPage()) ? 'agency_filter_highlight' : '';
 
@@ -97,6 +100,9 @@ if($current_url[1] == 'contracts_landing')
 else
     $edc_url = "spending_landing";
 
+//NYCHA Agencies: Set NYCHA default URL to Payroll
+$nycha_url = "payroll/agency_landing/yeartype/C/year/".$current_cal_year."/datasource/checkbook_nycha/agency/";
+
 $agency_list_other = "<div id='agency-list-other' class='agency-nav-dropdowns'>
   <div class='agency-list-open'><span id='other-agency-list-open' class='".$oge_filter_highlight."'>Other Government Entities</span></div>
   <div class='agency-list-content other-agency-list-content'>
@@ -105,6 +111,9 @@ $agency_list_other = "<div id='agency-list-other' class='agency-nav-dropdowns'>
           <ul class='listCol'>";
 foreach($edc_agencies as $key => $edc_agency){
     $agency_list_other .= "<li><a href='/". $edc_url .'/yeartype/B/year/'.$current_fy_year."/datasource/checkbook_oge/agency/".$edc_agency['agency_id']. "'>". $edc_agency['agency_name'] ."</a></li>";
+}
+foreach($nycha_agencies as $key => $nycha_agency){
+    $agency_list_other .= "<li><a href='/". $nycha_url.$nycha_agency['agency_id'] ."'>". $nycha_agency['agency_name'] ."</a></li>";
 }
 $agency_list_other .= "</ul>
         </div>
