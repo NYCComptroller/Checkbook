78f274ffafb1fe0fc8c5b2946ab212854b2bc741
Saroja Gade <sgade@reisystems.com>
2018-08-22 13:29:18 -0400

    NYCCHKBK-8590: NYCHA Payroll changes
    Former-commit-id: 40f0ca96d8f2349f9af9cdc79708ef3b903503f0 [formerly fda10e750179ef8faa16b4a61126573b7a04713e]
    Former-commit-id: 267535698a2c4f5bf398d8fa81cc1010e32aa7f3
    Former-commit-id: c91c8988099effd795f13ef0fc8fe501b1ee1d77
    Former-commit-id: 97b1756ca6c1bf5804f69bba1673b622b231f72c

--- source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/payroll.js
+++ source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/payroll.js
@@ -1,6 +1,8 @@
 (function ($) {
     Drupal.behaviors.payrollDataFeeds = {
         attach:function(context,settings){
+
+            //Citywide multi-select
             $('#edit-column-select',context).multiSelect();
             $('#ms-edit-column-select .ms-selectable',context).after('<a class="deselect">Remove All</a>');
             $('#ms-edit-column-select .ms-selectable',context).after('<a class="select">Add All</a>');
@@ -10,6 +12,18 @@
             $('#ms-edit-column-select a.deselect',context).click(function(){
                 $('#edit-column-select',context).multiSelect('deselect_all');
             });
+
+            //OGE multi-select
+            $('#edit-oge-column-select',context).multiSelect();
+            $('#ms-edit-oge-column-select .ms-selectable',context).after('<a class="deselect">Remove All</a>');
+            $('#ms-edit-oge-column-select .ms-selectable',context).after('<a class="select">Add All</a>');
+            $('#ms-edit-oge-column-select a.select',context).click(function(){
+                $('#edit-oge-column-select',context).multiSelect('select_all');
+            });
+            $('#ms-edit-oge-column-select a.deselect',context).click(function(){
+                $('#edit-oge-column-select',context).multiSelect('deselect_all');
+            });
+
             //Sets up jQuery UI datepickers
             var currentYear = new Date().getFullYear();
             $('.datepicker', context).datepicker({dateFormat:"yy-mm-dd",
@@ -34,8 +48,65 @@
                     $("#edit-title").autocomplete("option", "source", '/autocomplete/payroll/title/'+ agency + '/' + payfrequency + '/' + year);
                 });
             });
+
+            var dataSource = $('input:hidden[name="data_source"]', context).val();
+            $.fn.onDataSourceChange(dataSource);
+
+            //Data Source change event
+            $('input:radio[name=datafeeds_payroll_domain_filter]', context).change(function (){
+                //Remove all the validation errors when data source is changed
+                $('div.messages', context).remove();
+                $('.error', context).removeClass('error');
+
+                $('input:hidden[name="data_source"]', context).val($(this, context).val());
+                $.fn.clearInputFields();
+                $.fn.onDataSourceChange($(this, context).val());
+            });
         }
     }
+
+    //On Data Source Change
+    $.fn.onDataSourceChange = function (dataSource) {
+        if(dataSource == 'checkbook_oge'){
+            $('.datafield.agency').hide();
+            $('.datafield.other_government_entities').show();
+            $('.form-item-oge-column-select').show();
+            $('.form-item-column-select').hide();
+        }else{
+            $('.datafield.agency').show();
+            $('.datafield.other_government_entities').hide();
+            $('.form-item-oge-column-select').hide();
+            $('.form-item-column-select').show();
+        }
+    }
+
+     $.fn.clearInputFields = function () {
+        $('.fieldset-wrapper').find(':input').each(function () {
+            switch (this.type) {
+                case 'select-one':
+                    var default_option = $(this).attr('default_selected_value');
+                    if (default_option) {
+                      $(this).find('option[value=' + default_option + ']').attr("selected", "selected");
+                    } else {
+                      $(this).find('option:first').attr("selected", "selected");
+                    }
+                    break;
+                case 'text':
+                    $(this).val('');
+                    break;
+                case 'select-multiple':
+                case 'password':
+                case 'textarea':
+                    $(this).val('');
+                    break;
+                case 'checkbox':
+                case 'radio':
+                    $('#edit-salary-type-all').attr('checked', 'checked');
+                    break;
+            }
+        });
+     }
+
     //Function to retrieve values enclosed in brackets or return zero if none
     function emptyToZero(input) {
         var p = /\[(.*?)\]$/;
@@ -48,4 +119,5 @@
         }
         return output;
     }
+
 }(jQuery));
