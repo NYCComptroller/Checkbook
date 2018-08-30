587a46d9a1dd21b7fe6e48ce3755d85b58bef825
Alex Perfilov <alexandr.perfilov@reisystems.com>
2018-08-29 22:05:38 -0400

    More phpunit
    Former-commit-id: 59cbe7cf2a3042a5249eddf5aaef27190a923132 [formerly fea404bdf41975c15a78313ec53c8a6006309e16]
    Former-commit-id: c7d3379c6750422ddb835e751503d0f542fc2dc1
    Former-commit-id: c205f7922902d89242ab3d1785526be9210c26c7
    Former-commit-id: 052dda6000d48ca1f56e80f6dbc9fffe441b9842

--- source/tests/unit/tests/checkbook_api/CheckbookJsonApiHelperTest.php
+++ source/tests/unit/tests/checkbook_api/CheckbookJsonApiHelperTest.php
@@ -120,9 +120,10 @@ class CheckbookJsonApiHelperTest extends TestCase
             'audit_status_timestamp' => filemtime(
                 $conf['etl-status-path'] . 'audit_status.txt'),
             'match_status' => [
+                'PMSSummary' => 'unknown',
                 'COAExpenditureObject' => 'unknown',
-                'MAG' => '5 days ago',
                 'COARevenueSource' => '7 days ago',
+                'MAG' => '5 days ago',
                 'Some Incredible Name' => 'unknown data source name',
             ],
             'match_status_timestamp' => filemtime(
@@ -131,15 +132,14 @@ class CheckbookJsonApiHelperTest extends TestCase
         $fakeToday = '2222-12-22';
         $this->helper->timeNow = strtotime($fakeToday);
         $this->helper->dataSourceLastSuccess = serialize([
-            'COARevenueSource' => '2222-12-15',
             'MAG' => '2222-12-17',
+            'COARevenueSource' => '2222-12-15',
         ]);
         $this->assertEquals($expected, $this->helper->getProdEtlStatus());
         $expected = [
             'COARevenueSource' => '2222-12-15',
             'MAG' => '2222-12-17',
             'PendingContracts' => $fakeToday,
-            'PMSSummary' => $fakeToday,
             'PMS' => $fakeToday,
         ];
         $this->assertEquals($expected, unserialize($this->helper->dataSourceLastSuccess));
