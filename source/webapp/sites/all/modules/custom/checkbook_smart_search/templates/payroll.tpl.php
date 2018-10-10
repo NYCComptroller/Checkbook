9925a651efcf0cc3ba49ba10347cd9c4cb5c3499
Saroja Gade <sgade@reisystems.com>
2018-10-10 14:17:26 -0400

    NYCCHKBK-8615: NYCHA Payroll smart search changes

--- source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/payroll.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/payroll.tpl.php
@@ -18,7 +18,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-$payroll_parameter_mapping = _checkbook_smart_search_domain_fields('payroll');
+$payroll_parameter_mapping = _checkbook_smart_search_domain_fields('payroll', $IsOge);
 
 $agency_id = $payroll_results['agency_id'];
 $dept_id = $payroll_results['department_id'];
