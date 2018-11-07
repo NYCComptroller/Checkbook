2651a250a7a2df48ecddc9c2b47c1d9df5f471f2
Snigdha Madhavaram <snigdha.madhavaram@reisystems.com>
2018-11-07 15:16:54 -0500

    NYCCHKBK-8725: Removed count function
    Former-commit-id: 11da421db25e0fcce738471331606dd1ca1a7614 [formerly 2550338e4d67ba0206a64971c206a4d33dc46958]
    Former-commit-id: 09e23f8760fc1d053f0baa3ca0cf12a354819521
    Former-commit-id: 8dbf55efd2b057587b540c63edfd3d7b6a724dfb
    Former-commit-id: f0bd46410ee1b61dc32be1eba0ff5f252266415f

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/data/INychaContractsDataService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/data/INychaContractsDataService.php
@@ -7,5 +7,4 @@ interface INychaContractsDataService {
     function GetContractsByVendors($parameters, $limit = null, $orderBy = null);
     function GetCountContractsByVendors($parameters);
     function GetContractsByAwardMethods($parameters, $limit = null, $orderBy = null);
-    function GetCountContractsByAwardMethods($parameters);
 }
