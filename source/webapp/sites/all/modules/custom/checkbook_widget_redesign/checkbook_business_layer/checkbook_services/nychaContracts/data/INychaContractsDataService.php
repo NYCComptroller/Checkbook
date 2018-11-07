00213f11f085d3d59234ed926b24fbf17639215c
Snigdha Madhavaram <snigdha.madhavaram@reisystems.com>
2018-11-07 14:44:03 -0500

    NYCCHKBK-8725: Top 5 Award Methods

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/data/INychaContractsDataService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/data/INychaContractsDataService.php
@@ -6,4 +6,6 @@
 interface INychaContractsDataService {
     function GetContractsByVendors($parameters, $limit = null, $orderBy = null);
     function GetCountContractsByVendors($parameters);
-} 
+    function GetContractsByAwardMethods($parameters, $limit = null, $orderBy = null);
+    function GetCountContractsByAwardMethods($parameters);
+}
