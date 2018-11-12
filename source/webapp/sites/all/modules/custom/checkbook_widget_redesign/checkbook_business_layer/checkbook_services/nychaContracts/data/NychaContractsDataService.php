f7144b2dfde4a9d6598e433335a7ea380f5e26f9
Snigdha Madhavaram <snigdha.madhavaram@reisystems.com>
2018-11-12 11:26:31 -0500

    NYCCHKBK-8734:[NYCHA][Contracts][Widgets] - Top 5 Boroughs

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/data/NychaContractsDataService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/data/NychaContractsDataService.php
@@ -13,6 +13,9 @@ class NychaContractsDataService extends DataService implements INychaContractsDa
     function GetContractsByPurchaseOrders($parameters, $limit = null, $orderBy = null){
         return $this->configureNycha(__FUNCTION__,$parameters,$limit,$orderBy);
     }
+    function GetContractsByBoroughs($parameters, $limit = null, $orderBy = null){
+        return $this->configureNycha(__FUNCTION__,$parameters,$limit,$orderBy);
+    }
     /**
      * Common function that automatically configures the NYCHA Contracts sql
      * @param $dataFunction
