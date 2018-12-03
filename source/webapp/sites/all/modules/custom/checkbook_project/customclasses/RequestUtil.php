51902788097d44170a607eb35611cbfee824ca21
Snigdha Madhavaram <snigdha.madhavaram@reisystems.com>
2018-12-03 14:47:03 -0500

    NYCCHKBK-8693: NYCHA Contracts Page title

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/RequestUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/RequestUtil.php
@@ -634,7 +634,7 @@ class RequestUtil
 
         switch ($domain) {
             case "nycha_contracts":
-                $path = "/nycha_contracts/datasource/". Datasource::NYCHA;
+                $path = "/nycha_contracts/datasource/".Datasource::NYCHA. Datasource::getNYCHAUrl();
                 break;
             case "contracts":
                 //Get 'Contracts Bottom Slider' amounts
