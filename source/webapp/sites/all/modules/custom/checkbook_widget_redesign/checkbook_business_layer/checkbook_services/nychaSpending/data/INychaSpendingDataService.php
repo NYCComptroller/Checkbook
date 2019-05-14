4cf9a6ea19601becc107fa7d29d0e9724b90c5e4
Snigdha Madhavaram <snigdha.madhavaram@reisystems.com>
2019-05-14 14:09:37 -0400

    NYCCHKBK-9063:Top 5 Vendors Widget

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaSpending/data/INychaSpendingDataService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaSpending/data/INychaSpendingDataService.php
@@ -5,4 +5,8 @@
  */
 interface INychaSpendingDataService {
   function GetNychaSpendingByChecks($parameters, $limit = null, $orderBy = null);
+  function  GetNychaSpendingByVendors($parameters, $limit = null, $orderBy = null);
+  function  GetCountVendors($parameters);
+
+
 }
