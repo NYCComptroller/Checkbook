6691664dd812845ea4991f369f7679f9b6ad6c8f
Snigdha Madhavaram <snigdha.madhavaram@reisystems.com>
2019-05-15 18:10:44 -0400

    NYCCHKBK-9069:Spending by industries widget
    Former-commit-id: 7cd784abcea0830a5325b734db9b6ce6a790d094 [formerly c137c265cfe143fbc0c092b3a66a164daa9562e2]
    Former-commit-id: f9265f34d977d3bdece3d7f29b54690dca14a497
    Former-commit-id: 43abb3dfe17ad856641dfba5af5edf74a2cfb783
    Former-commit-id: f9538592e9ea8d5ad4580e699f7341d1b63d29a3

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaSpending/data/INychaSpendingDataService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaSpending/data/INychaSpendingDataService.php
@@ -8,6 +8,7 @@ interface INychaSpendingDataService {
   function  GetNychaSpendingByVendors($parameters, $limit = null, $orderBy = null);
   function GetNychaSpendingByContracts($parameters, $limit = null, $orderBy = null);
   function GetNychaSpendingByExpenseCategories($parameters, $limit = null, $orderBy = null);
+  function GetNychaSpendingByIndustries($parameters, $limit = null, $orderBy = null);
   function  GetCountVendors($parameters);
     function GetCountContracts($parameters);
 
