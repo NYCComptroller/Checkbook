a57d9134c515b964c66d502243f5e5bf5128e3d8
Alex Perfilov <alexandr.perfilov@reisystems.com>
2019-06-07 16:17:05 -0400

    Unit testing
    Former-commit-id: 72364504ff1d0fa0b75fb63a5a2b88a1ecb46bfd [formerly 1cbc020df8b4c553599a5b31fbd47d53db5db6fa]
    Former-commit-id: aa6d4ceffec9e096aafd2e3db0fcdb2ffc5ae1dc
    Former-commit-id: 77a61c5b8120c2667eacd1ccf9834616ccbe1c42
    Former-commit-id: 1d220a51f2117b9814758408f63afe6376376fbe

---
+++ source/tests/unit/tests/checkbook_solr/CheckbookSolrQueryTest.php
@@ -0,0 +1,61 @@
+<?php
+
+include_once CUSTOM_MODULES_DIR . '/checkbook_solr/src/checkbook_solr_query.class.inc';
+include_once CUSTOM_MODULES_DIR . '/checkbook_solr/src/checkbook_solr.class.inc';
+
+/**
+ * Class CheckbookApiModuleTest
+ */
+class CheckbookSolrQueryTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     *
+     */
+    public function test_constructor()
+    {
+        $searchTerms = 'rei';
+        $query = new CheckbookSolrQuery($searchTerms, 999, 7);
+        $start = 999 * 7;
+        $this->assertEquals('q=rei&start=' . $start . '&rows=999&wt=phps', $query->buildQuery());
+
+        $searchTerms = 'rei*|*vendor_names=rei%2Bsystems%252C%2Binc.';
+        $query = new CheckbookSolrQuery($searchTerms, 9);
+        $this->assertEquals('q=rei&fq=vendor_name:"rei%5C%2Bsystems%252c%5C%2Binc."&start=0&rows=9&wt=phps',
+            $query->buildQuery());
+    }
+
+    /**
+     *
+     */
+    public function test_sort()
+    {
+        $query = new CheckbookSolrQuery();
+        $query->setSort('vendor_name asc, original_amount desc');
+        $this->assertEquals('q=*:*&start=0&rows=0&sort=vendor_name+asc%2C+original_amount+desc&wt=phps',
+            $query->buildQuery());
+    }
+
+    /**
+     *
+     */
+    public function testFq()
+    {
+        $query = new CheckbookSolrQuery();
+        $query->setFq('vendor_name', 'vendor_name:rei systems');
+        $this->assertEquals('q=*:*&fq=vendor_name:rei systems&start=0&rows=0&wt=phps', $query->buildQuery());
+    }
+
+    /**
+     *
+     */
+    public function testPagination()
+    {
+        $query = new CheckbookSolrQuery();
+        $query
+            ->setRows(9)
+            ->setPage(7);
+
+        $this->assertEquals('q=*:*&start=63&rows=9&wt=phps', $query->buildQuery());
+    }
+
+}
