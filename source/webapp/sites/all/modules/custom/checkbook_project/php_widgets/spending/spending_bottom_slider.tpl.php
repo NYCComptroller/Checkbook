b18416a99634066646b8ae3051deec65f46cc56b
Saroja Gade <sgade@reisystems.com>
2019-08-13 14:23:27 -0400

    NYCCHKBK-9078: Updated spending url function name
    Former-commit-id: 4526b5cac2c8637bb430cff9d06650f3fab3e37f [formerly 9e7518c60e65d2f1a69559c2592c297608952135]
    Former-commit-id: 49dd0921549f7c2fb76b6f670603b0dcea20c666
    Former-commit-id: d8a31c9fdfd2b726e6f9e74957ffa2ce69629e15
    Former-commit-id: 7eb38648d5db27f9b27623f48f8d51c3eac95784

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/spending/spending_bottom_slider.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/spending/spending_bottom_slider.tpl.php
@@ -32,7 +32,7 @@
       if ( RequestUtilities::get("category") == "") {
         $class = ' class="active"';
       }
-      $link = RequestUtil::preparePayrollBottomNavFilter("spending_landing",null);
+      $link = RequestUtil::prepareSpendingBottomNavFilter("spending_landing",null);
       $dollars = "<span class='dollars'>" . custom_number_formatter_format($total,1,'$') . "</span>";
       ?>
       <td<?php echo $class; ?>>
@@ -50,7 +50,7 @@
       if (RequestUtilities::get("category") == 2) {
         $class = ' class="active"';
       }
-      $link = RequestUtil::preparePayrollBottomNavFilter("spending_landing",2);
+      $link = RequestUtil::prepareSpendingBottomNavFilter("spending_landing",2);
       $dollars = "<span class='dollars'>" . custom_number_formatter_format($dollars_by_cat[2],1,'$') . "</span>";
       ?>
       <td<?php echo $class; ?>>
@@ -68,7 +68,7 @@
       if (RequestUtilities::get("category") == 3) {
         $class = ' class="active"';
       }
-      $link = RequestUtil::preparePayrollBottomNavFilter("spending_landing",3);
+      $link = RequestUtil::prepareSpendingBottomNavFilter("spending_landing",3);
       $dollars = "<span class='dollars'>" . custom_number_formatter_format($dollars_by_cat[3],1,'$') . "</span>";
 
       ?>
@@ -87,7 +87,7 @@
       if (RequestUtilities::get("category") == 1) {
         $class = ' class="active"';
       }
-      $link = RequestUtil::preparePayrollBottomNavFilter("spending_landing",1);
+      $link = RequestUtil::prepareSpendingBottomNavFilter("spending_landing",1);
       $dollars = "<span class='dollars'>" . custom_number_formatter_format($dollars_by_cat[1],1,'$') . "</span>";
 
       ?>
@@ -106,7 +106,7 @@
       if (RequestUtilities::get("category") == 5) {
         $class = ' class="active"';
       }
-      $link = RequestUtil::preparePayrollBottomNavFilter("spending_landing",5);
+      $link = RequestUtil::prepareSpendingBottomNavFilter("spending_landing",5);
       $dollars = "<span class='dollars'>" . custom_number_formatter_format($dollars_by_cat[5],1,'$') . "</span>";
 
       ?>
@@ -125,7 +125,7 @@
       if (RequestUtilities::get("category") == 4) {
         $class = ' active';
       }
-      $link = RequestUtil::preparePayrollBottomNavFilter("spending_landing",4);
+      $link = RequestUtil::prepareSpendingBottomNavFilter("spending_landing",4);
       $dollars = "<span class='dollars'>" . custom_number_formatter_format($dollars_by_cat[4],1,'$') . "</span>";
 
       ?>
