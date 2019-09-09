dd33abfcfc931f5fe48372b8f41411e4d591df77
Kavitha Gopalakrishnan <saidapet.kavitha@gmail.com>
2019-09-09 08:57:46 -0400

    NYCCHKBK-8998 Fixed the widget show/hide button issue and alignment issue.
    Signed-off-by: Kavitha Gopalakrishnan <saidapet.kavitha@gmail.com>

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contract/nycha_contract_details.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contract/nycha_contract_details.tpl.php
@@ -196,9 +196,9 @@ $contract = $node->data;
           </tr>
           <tr id="showHideNychaOrderRevisions<?= $year ?>"
               class="showHide <?= ($yi % 2 ? 'odd' : '') ?>" <?= ($hidden ? 'style="display:none"' : '') ?>>
-            <td colspan="4">
-              <div class="scroll">
-                <table class="dataTable outerTable">
+            <td  colspan="4">
+              <div  class="scroll">
+                <table class= "dataTable outerTable">
                   <thead>
                   <tr>
                     <th class="text th1">
