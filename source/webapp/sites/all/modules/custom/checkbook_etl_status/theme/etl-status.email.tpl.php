32ee587d755001fb9165084df49500118a12ff0a
Alex Perfilov <6089683+vasili4@users.noreply.github.com>
2019-09-20 01:55:27 -0400

    Detecting missing pgp-txt decryption files

--- source/webapp/sites/all/modules/custom/checkbook_etl_status/theme/etl-status.email.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_etl_status/theme/etl-status.email.tpl.php
@@ -1,352 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+  <meta charset="UTF-8">
+  <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 
-    <style type="text/css">
-        body {
-            font-family: Roboto;
-            font-size: 12px;
-        }
+  <style type="text/css">
+    body {
+      font-family: Roboto;
+      font-size: 12px;
+    }
 
-        table {
-            border: #ccc 1px dotted;
-            margin-left: auto;
-            margin-right: auto;
-        }
+    table {
+      border: #ccc 1px dotted;
+      margin-left: auto;
+      margin-right: auto;
+    }
 
-        table.status {
-            font-size: larger;
-            background: #ddd;
-        }
+    table.status {
+      font-size: larger;
+      background: #ddd;
+    }
 
-        table.status th {
-            text-align: right;
-        }
+    table.status th {
+      text-align: right;
+    }
 
-        thead tr.header th, tbody th.env {
-            text-transform: uppercase;
-            background: #3c6e95;
-            color: #fff;
-        }
+    thead tr.header th, tbody th.env {
+      text-transform: uppercase;
+      background: #3c6e95;
+      color: #fff;
+    }
 
-        td.bytesize, tr.bytesize {
-          text-align: right;
-        }
+    td.bytesize, tr.bytesize {
+      text-align: right;
+    }
 
-        table.dbconnections tr.header th, table.dbconnections th.env {
-            background: #8e9eac;
-        }
+    table.dbconnections tr.header th, table.dbconnections th.env {
+      background: #8e9eac;
+    }
 
-        thead tr.filename th {
-            color: darkred;
-            text-align: right;
-        }
+    thead tr.filename th {
+      color: darkred;
+      text-align: right;
+    }
 
-        tbody tr.even {
-            background: #ddd;
-        }
+    tbody tr.even {
+      background: #ddd;
+    }
 
-        tbody tr.odd {
-            background: #eee;
-        }
+    tbody tr.odd {
+      background: #eee;
+    }
 
-        tr.today td{
-          color: darkgreen;
-        }
+    tr.today td {
+      color: darkgreen;
+    }
 
-        footer {
-            border-top: 1px #2d5879 solid;
-            text-align: center;
-        }
+    footer {
+      border-top: 1px #2d5879 solid;
+      text-align: center;
+    }
 
-        a:link, footer a:visited {
-            color: #5b5b5b;
-            text-decoration: none;
-        }
+    a:link, footer a:visited {
+      color: #5b5b5b;
+      text-decoration: none;
+    }
 
-        footer table {
-            border: 0;
-        }
-    </style>
+    footer table {
+      border: 0;
+    }
+  </style>
 
 </head>
 <body text="#5b5b5b">
 
 <header>
-    <table bgcolor="2d5879" width="100%">
-        <tr align="center">
-            <td width="50%"></td>
-            <td width="400">
-                <a href="https://checkbooknyc.com/">
-                    <img alt="CheckbookNYC logo" title="CheckbookNYC"
-                         src="https://www.checkbooknyc.com/sites/all/themes/checkbook3/images/logo.png"/><br/>
-                </a>
-            </td>
-            <td width="50%"></td>
-        </tr>
-    </table>
+  <table bgcolor="2d5879" width="100%">
+    <tr align="center">
+      <td width="50%"></td>
+      <td width="400">
+        <a href="https://checkbooknyc.com/">
+          <img alt="CheckbookNYC logo" title="CheckbookNYC"
+               src="https://www.checkbooknyc.com/sites/all/themes/checkbook3/images/logo.png"/><br/>
+        </a>
+      </td>
+      <td width="50%"></td>
+    </tr>
+  </table>
 </header>
 
 <main>
-    <br/>
-    <br/>
-
-    <table class="status" cellspacing="8">
-        <tbody>
-        <?php foreach ([$prod_status, $uat_status] as $json): ?>
-            <tr>
-                <th><?php echo $json['source'] ?> ETL</th>
-                <td title="<?php echo $json['hint'] ?>">
-                    <?php echo($json['success'] ? '✅' : '❌'); ?>
-                </td>
-                <td>
-                    <?php echo $json['hint'] ?>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-
-    <br/>
-    <br/>
-
-    <?php
-    foreach ([$prod_status, $uat_status] as $json):
-        if ($json['audit_status_timestamp']): ?>
-            <table class="file" cellpadding="5">
-                <?php if (!empty($json['audit_status']) && (['OK'] == $json['audit_status'])): ?>
-                    <tbody>
-                    <tr class="odd">
-                        <td>
-                            <strong>PROD-UAT Match</strong> ✅
-                            <?php echo $json['audit_status_time_diff']; ?>
-                        </td>
-                    </tr>
-                    </tbody>
-                <?php else: ?>
-                    <thead>
-                    <tr class="filename">
-                        <th>
-                            <?php echo $json['source'] ?> ETL `audit_status.txt`
-                            (<?php echo date("Y-m-d g:iA", $json['audit_status_timestamp']); ?>)
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr class="odd">
-                        <td>
-                            <strong>PROD-UAT Match</strong> ❌ <br/>
-                        </td>
-                    </tr>
-                    <tr class="even">
-                        <td>
-                            <?php
-                                if(empty($json['audit_status'])):
-                                    echo '*file is empty*';
-                                else:
-                                    echo json_encode($json['audit_status']);
-                                endif;
-                            ?>
-                        </td>
-                    </tr>
-                    </tbody>
-                <?php endif; ?>
-            </table>
-            <br/>
-            <br/>
-        <?php
-        endif;
-        if ($json['invalid_records_timestamp'] && $json['invalid_records']): ?>
-
-            <table class="file" cellpadding="5">
-                <thead>
-                <tr class="filename">
-                    <th colspan="<?php echo sizeof($json['invalid_records'][0]) ?>">
-                        <?php echo $json['source'] ?> ETL `invalid_records_details.csv`
-                        (<?php echo date("Y-m-d g:iA", $json['invalid_records_timestamp']); ?>)
-                    </th>
-                </tr>
-                <?php $first = true;
-                foreach ($json['invalid_records'] as $key => $row):
-                $class = $key ? ($key % 2 ? 'odd' : 'even') : 'header';
-                $td = $first ? 'th' : 'td'; ?>
-                <tr class="<?php echo $class ?>">
-                    <?php foreach ($row as $item) {
-                        echo "<$td>" . htmlentities($item) . "</$td>";
-                    } ?>
-                </tr>
-                <?php if ($first): ?>
-                </thead>
-                <tbody>
-                <?php endif; ?>
-                <?php $first = false;
-                endforeach; ?>
-                </tbody>
-            </table>
-            <br/>
-            <br/>
-        <?php endif;
-
-        if ($json['match_status_timestamp'] && $json['match_status']): ?>
-
-            <table class="file" cellpadding="5" title="file_data_statistics.csv (<?php
-                    echo date("Y-m-d g:iA", $json['match_status_timestamp']); ?>)">
-                <thead>
-                <tr class="filename">
-                    <th colspan="3">
-                        <?php echo $json['source'] ?> ETL :: Missing data source files
-                    </th>
-                </tr>
-                <tr>
-                    <th>
-                        Data source name
-                    </th>
-                    <th>
-                    </th>
-                    <th>
-                        Last successful run
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $key = 0;
-                foreach ($json['match_status'] as $source => $source_status):
-                    $class = $key++ % 2 ? 'odd' : 'even';
-                    ?>
-                    <tr class="<?php echo $class ?>">
-                        <td><?php echo htmlentities($source) ?></td>
-                        <td>❌</td>
-                        <td><?php echo $source_status; ?></td>
-                    </tr>
-                <?php
-                endforeach; ?>
-                </tbody>
-            </table>
-            <br/>
-            <br/>
-        <?php elseif('PROD' == $json['source']):?>
-            <table class="file" cellpadding="5">
-                <thead>
-                <tr class="filename">
-                    <th>
-                        PROD ETL :: Missing data source files
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr class="odd">
-                    <td><strong>No missing data found</strong> ✅</td>
-                </tr>
-                </tbody>
-                <tbody>
-            </table>
-            <br/>
-            <br/>
-        <?php endif;
-    endforeach; ?>
-
-    <?php if (!empty($fisa_files)): $i=0; $oldPrefix='';?>
-    <table class="file" cellpadding="5">
-      <thead>
-        <tr class="filename">
-          <th colspan="3">FISA contracts files (received by <?= $fisa_files['date'] ?> 10pm EST):</th>
-        </tr>
-        <tr class="header">
-          <th>Bytesize</th>
-          <th class="bytesize">Number of lines</th>
-          <th>Filename</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach($fisa_files['contract_lines'] as $line):
-          list($prefix,) = explode('_'.date('Y'), $line['filename']);
-          $strong = (strpos($line['filename'], $fisa_files['date'])) ? 'today' : '';
-          if ($prefix !== $oldPrefix) {$oldPrefix = $prefix; $i++;}
-          ?>
-        <tr class="<?php echo($i % 2 ? 'even' : 'odd'); ?> <?= $strong ?>">
-          <td><?= $line['bytes'] ?></td>
-          <td class="bytesize"><?= $line['lines'] ?></td>
-          <td><?= $line['filename'] ?></td>
-        </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-    <?php else: ?>
-    <table class="file" cellpadding="5">
-      <thead>
-        <tr class="filename" align="center"><th> FISA files: </th></tr>
-      </thead>
-      <tbody>
-      <tr class="odd"><td> ❌ NO DATA ❌ </td></tr>
-      </tbody>
-    </table>
-    <?php endif; ?>
+  <br/>
+  <br/>
+  <?php include("_main_status.tpl.php"); ?>
+  <br/>
+  <br/>
+  <?php include("_prod_uat_match.tpl.php"); ?>
+  <br/>
+  <br/>
+  <?php include("_fisa_files.tpl.php"); ?>
   <br/>
   <br/>
   <br/>
 
-    <?php if (!empty($connections) && isset($dev_mode) && $dev_mode):
-        $i = 0;
-        ?>
-        <table class="dbconnections" cellpadding="3">
-            <thead>
-            <tr>
-                <th colspan="<?php echo(sizeof($connection_keys) + 1); ?>">
-                    DB CONNECTIONS
-                </th>
-            </tr>
-            <tr class="header">
-                <th></th>
-                <?php foreach ($connection_keys as $key): ?>
-                    <th><?php echo $key; ?></th>
-                <?php endforeach; ?>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($connections as $env => $v): ?>
-                <tr class="<?php echo($i++ % 2 ? 'even' : 'odd'); ?>">
-                    <th class="env"><?php echo $env; ?></th>
-                    <?php foreach ($connection_keys as $key): ?>
-                        <th><?php echo(!empty($v[$key]) ? str_replace('|', '<br />', $v[$key]) : '-'); ?></th>
-                    <?php endforeach; ?>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-        <br/>
-        <br/>
-    <?php endif; ?>
+  <?php include("_dev_debug.tpl.php"); ?>
 </main>
 
 <footer>
-    <table width="100%">
-        <tr align="center">
-            <td>
-                © <?php echo date('Y'); ?>, Checkbook NYC<br/>
-                <?php if(isset($dev_mode) && $dev_mode):?>
-                <small>
-                    <?php
-                    global $conf;
-                    if (!empty($conf['etl-status-footer'])):
-                        $out = '';
-                        $arr = [];
-                        foreach ($conf['etl-status-footer'] as $line) {
-                            foreach ($line as $text => $url):
-                                $arr[] = "<a target=\"_blank\" href=\"$url\">$text</a>";
-                            endforeach;
-                            $out .= join(' | ', $arr) . '<br />';
-                            $arr = [];
-                        }
-                        echo $out;
-                    endif; ?>
-                </small>
-                <?php endif; ?>
-            </td>
-        </tr>
-    </table>
+  <?php include("_footer.tpl.php"); ?>
 </footer>
 
 </body>
