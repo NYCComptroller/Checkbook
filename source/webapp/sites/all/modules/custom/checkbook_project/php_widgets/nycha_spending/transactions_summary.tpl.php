3bf2cb48ed4d2fc57f9e8fa6a2b1a0b3800ce776
Saroja Gade <sgade@reisystems.com>
2019-09-30 12:04:29 -0400

    NYCCHKBK-9213: summary section for NYCHA Spending Transactions page
    Former-commit-id: 5b602a0e2cb8e823fd135da4549f9033e7c07b11 [formerly e630eaaecc5a80870b0f766b2b20e22fdadb66da]
    Former-commit-id: 09d3e63c28ba9ac5fd8d95fb9c17a40ffe6f5440
    Former-commit-id: d9215b0908daa29e26ba65db2b5fa79992e86bf4
    Former-commit-id: 96c9fc8c4bb62f0cf14d632b84670a33c2d9aa6e

---
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/nycha_spending/transactions_summary.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the Checkbook NYC financial transparency software.
+ *
+ * Copyright (C) 2012, 2013 New York City
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+$categoryId = RequestUtilities::get('category');
+$categoryName = NychaSpendingUtil::getCategoryName() . " Spending Amount";
+print '<div class="dollar-amounts"><div class="total-spending-amount">$' . custom_number_formatter_format($node->data[0]['check_amount_sum'],2).'<div class="amount-title">'. $categoryName .'</div></div></div>';
