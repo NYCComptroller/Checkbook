024ef3226def22f48f3e10fced4181cbb3fc8f8f
Alex Perfilov <6089683+vasili4@users.noreply.github.com>
2019-09-30 11:32:44 -0400

    NYCHA smart search: using K instead of thousands in facets

--- source/webapp/sites/all/modules/custom/checkbook_etl_status/cronjobs/files_logs2s3.sh
+++ source/webapp/sites/all/modules/custom/checkbook_etl_status/cronjobs/files_logs2s3.sh
@@ -2,7 +2,10 @@
 
 # runs on ETL server after all files were imported
 
+source ~/.bashrc
+
 DATE=`date +%Y%m%d`
 
-aws s3 cp $ETL_FISA_PATH/$DATE.log s3://$ETL_FISA_S3_BUCKET/FISA/
 aws s3 cp $ETL_FISA_PATH/$DATE.pgp.log s3://$ETL_FISA_S3_BUCKET/FISA-PGP/
+sleep 5m
+aws s3 cp $ETL_FISA_PATH/$DATE.log s3://$ETL_FISA_S3_BUCKET/FISA/
