1d6ec1f5da425cee77965628f35632b46f4ee681
Kavitha G <kgopalakrishnan@resisystems.com>
2019-10-11 17:03:08 -0400

    NYCCHKBK-9092 - Add invoiced amount links to nycha contracts.
    Former-commit-id: 014569ae5b59178fe3e6b10e4a1d4ee2624e13fa [formerly cbefd012a26a70178200b54a26fb393ea7509bdf]
    Former-commit-id: 9ec9ef42ec73268679c286b1b3fff6ef45448e7f
    Former-commit-id: 48db8ea810f8abc752b4ea1f6f122553c5d67000
    Former-commit-id: 198cd61ba9eac63fdc00de25027228812743c9ac

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/NychaContractsWidgetService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaContracts/NychaContractsWidgetService.php
@@ -38,6 +38,118 @@ class NychaContractsWidgetService extends WidgetDataService implements IWidgetSe
                 $url = NychaContractsUrlService::contractDetailsUrl($contract_id);
                 $value = "<a href='{$url}'>{$contract_id}</a>";
                 break;
+
+            ## NYCHA Contracts Invoiced Amount Links - linkc to nycha spending transactions
+            case "ba_inv_link":
+              $ba_inv_amount = $row['spend_to_date'];
+              $agreement_type='/agreement_type/BA';
+              $tcode = '/tcode/BA';
+              $dynamic_parameter = "/po_num_exact/" . $row["contract_id"];
+              //$class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$ba_inv_amount}</a>";
+              break;
+            case "bam_inv_link":
+              $bam_inv_amount = $row['spend_to_date'];
+              $agreement_type='/agreement_type/BA';
+              $tcode = '/tcode/BAM';
+              $dynamic_parameter = "/po_num_exact/" . $row["contract_id"];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$bam_inv_amount}</a>";
+              break;
+            case "pa_inv_link":
+              $pa_inv_amount = $row['spend_to_date'];
+              $agreement_type='/agreement_type/PA';
+              $tcode = '/tcode/PA';
+              $dynamic_parameter = "/po_num_exact/" . $row["contract_id"];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$pa_inv_amount}</a>";
+              break;
+            case "pam_inv_link":
+              $pam_inv_amount = $row['spend_to_date'];
+              $agreement_type='/agreement_type/PA';
+              $tcode = '/tcode/PAM';
+              $dynamic_parameter = "/po_num_exact/" . $row["contract_id"];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$pam_inv_amount}</a>";
+              break;
+            case "po_inv_link":
+              $po_inv_amount = $row['spend_to_date'];
+              $agreement_type='/agreement_type/PO';
+              $tcode = '/tcode/PO';
+              $dynamic_parameter = "/po_num_exact/" . $row["contract_id"];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$po_inv_amount}</a>";
+              break;
+            case "vendor_inv_link":
+              $vendor_inv_amount = $row['spend_to_date'];
+              $agreement_type="";
+              $tcode = '/tcode/VO';
+              $dynamic_parameter = "/vendor/" . $row['vendor_id'];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$vendor_inv_amount}</a>";
+              break;
+            case "awd_inv_link":
+              $awd_inv_amount = $row['spend_to_date'];
+              $agreement_type="";
+              $tcode = '/tcode/AWD';
+              $dynamic_parameter = "/awdmethod/" . $row['award_method_id'];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$awd_inv_amount}</a>";
+              break;
+            case "dpt_inv_link":
+              $dpt_inv_amount = $row['spend_to_date'];
+              $agreement_type="";
+              $tcode = '/tcode/DEP';
+              $dynamic_parameter = "/department/" . $row['department_id'];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$dpt_inv_amount}</a>";
+              break;
+            case "rsp_inv_link":
+              $rsp_inv_amount = $row['spend_to_date'];
+              $agreement_type="";
+              $tcode = '/tcode/RESC';
+              $dynamic_parameter = "/respcenter/" . $row['responsibility_center_id'];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$rsp_inv_amount}</a>";
+              break;
+            case "ind_inv_link":
+              $ind_inv_amount = $row['spend_to_date'];
+              $agreement_type="";
+              $tcode = '/tcode/IND';
+              $dynamic_parameter = "/industry/" . $row['industry_type_id'];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$ind_inv_amount}</a>";
+              break;
+            case "award_size_inv_link":
+              $award_size_inv_amount = $row['spend_to_date'];
+              $agreement_type="";
+              $tcode = '/tcode/SZ';
+              $dynamic_parameter = "/csize/" . $row['award_size_id'];
+              $class = "bottomContainerReload";
+              $class = "new_window";
+              $url = NYCHASpendingUrlService::invContractSpendingUrl($dynamic_parameter, 'inv_contract',$agreement_type,$tcode);
+              $value = "<a class='{$class}' href='{$url}'>{$award_size_inv_amount}</a>";
+              break;
         }
 
         if(isset($value)) {
