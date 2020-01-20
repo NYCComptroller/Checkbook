41291da837b82c8f397a1aee340f386980992b2f
Kavitha G <kgopalakrishnan@resisystems.com>
2020-01-20 07:43:28 -0500

    feature/NYCCHKBK-9560 -  Smart Search budget and revenue missing data and facet issue fix.

--- source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/budget.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/budget.tpl.php
@@ -1,26 +1,26 @@
 <?php
 /**
 * This file is part of the Checkbook NYC financial transparency software.
-* 
+*
 * Copyright (C) 2012, 2013 New York City
-* 
+*
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
-* 
+*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU Affero General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU Affero General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 $budget_parameter_mapping = CheckbookSolr::getSearchFields($solr_datasource, 'budget');
 
 $linkable_fields = array("agency_name" => "/budget/year/" . CheckbookDateUtil::getCurrentFiscalYearId() . "/yeartype/B/agency/".$budget_results["agency_id"],
-                         "expenditure_object_name" => "/budget/year/". CheckbookDateUtil::getCurrentFiscalYearId() . "/yeartype/B/expcategory/".$budget_results["expenditure_object_id"],
+                         "object_class_name" => "/budget/year/". CheckbookDateUtil::getCurrentFiscalYearId() . "/yeartype/B/expcategory/".$budget_results["object_class_id"],
                         );
 if($budget_results['fiscal_year'][0] < 2010){
     $linkable_fields = array();
