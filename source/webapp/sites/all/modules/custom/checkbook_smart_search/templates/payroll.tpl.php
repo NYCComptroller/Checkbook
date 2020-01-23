1f268871f0163d6c2c1c81df868a4abbce901191
Kavitha G <kgopalakrishnan@resisystems.com>
2020-01-23 10:09:00 -0500

    feature/NYCCHKBK-9696 NYCHA payroll smart search daily/annual wage link update.

--- source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/payroll.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_smart_search/templates/payroll.tpl.php
@@ -86,10 +86,10 @@ foreach ($payroll_parameter_mapping as $key => $title){
 //    $url = PayrollUrlService::annualSalaryPerAgencyUrl($agency_id, $emp_id);
     if (('Annual Salary' == $title && $salaried == 1)
       || (in_array($title, ['Hourly Rate', 'Daily Wage']) && $salaried !== 1 && $value)) {
-      $value = "<a  href='/payroll".$agencyLandingUrl."/yeartype/B/year/" . $fiscal_year_id . $dataSourceUrl
+      $value = "<a  href='/payroll".$agencyLandingUrl."/yeartype/C/year/" . $fiscal_year_id . $dataSourceUrl
         . "?expandBottomContURL=/panel_html/payroll_employee_transactions/payroll/employee/transactions/agency/"
         .$agency_id . $dataSourceUrl . "/abc/" .$emp_id. "/salamttype/".$salaried."/year/"
-        . $fiscal_year_id . "/yeartype/B'>". custom_number_formatter_format($value, 2 , '$') ."</a>";
+        . $fiscal_year_id . "/yeartype/C'>". custom_number_formatter_format($value, 2 , '$') ."</a>";
     } else {
       $value = '';
     }
