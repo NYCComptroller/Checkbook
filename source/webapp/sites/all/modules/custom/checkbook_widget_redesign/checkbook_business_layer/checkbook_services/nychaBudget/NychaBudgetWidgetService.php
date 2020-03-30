96a95f2b76b6e791a7145624c2087431e75fc340
Saroja Gade <sgade@reisystems.com>
2020-03-30 14:09:45 -0400

    NYCCHKBK-9841: Widget Utility class for NYCHA Budget

---
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaBudget/NychaBudgetWidgetService.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sgade
+ * Date: 02/22/17
+ * Time: 2:05 PM
+ */
+
+class NychaBudgetWidgetService extends WidgetDataService implements IWidgetService {
+
+  /**
+   * Function to allow the client to initialize the data service
+   * @return mixed
+   */
+  public function initializeDataService() {
+    return new NychaBudgetDataService();
+  }
+
+  public function implementDerivedColumn($column_name, $row) {
+    $value = null;
+    $legacy_node_id = $this->getLegacyNodeId();
+
+    if(isset($value)) {
+      return $value;
+    }
+    return $value;
+  }
+
+  public function getWidgetFooterUrl($parameters) {
+    return NychaBudgetUrlService::getFooterUrl($parameters,$this->getLegacyNodeId());
+  }
+}
