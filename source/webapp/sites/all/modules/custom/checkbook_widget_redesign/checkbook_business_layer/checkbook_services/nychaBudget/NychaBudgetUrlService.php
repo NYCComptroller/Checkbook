fc650855fab5957ddb5217896c3a083de80301c5
Kavitha G <kgopalakrishnan@resisystems.com>
2020-04-20 11:13:02 -0400

    feature-9862- Transactions Widgets details page.

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaBudget/NychaBudgetUrlService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaBudget/NychaBudgetUrlService.php
@@ -4,9 +4,16 @@
 class NychaBudgetUrlService {
 
     static function getFooterUrl($parameters = null) {
-        $url = '/panel_html/nycha_budget_transactions/nycha_budget/transactions'
-            .RequestUtilities::buildUrlFromParam('year')
-            .RequestUtilities::buildUrlFromParam('expcategory');
-        return $url;
+        // = '/panel_html/nycha_budget_transactions/nycha_budget/transactions'
+          //  .RequestUtilities::buildUrlFromParam('year')
+            //.RequestUtilities::buildUrlFromParam('expcategory');
+        //return $url;
+
+      $url = "/panel_html/nycha_budget_transactions/nycha_budget/transactions"
+        . RequestUtilities::buildUrlFromParam('year')
+        . RequestUtilities::buildUrlFromParam('fundsrc')
+        . RequestUtilities::buildUrlFromParam('datasource');
+      return $url;
     }
+
 }
