71318a540ee7e44f756801acd6db3454668d5325
Prashant Pillai <prashant.pillai@C02Z20WHLVDQ.local>
2020-04-27 17:38:00 -0400

    changes to percent difference widgets

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/BudgetConstants.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/BudgetConstants.php
@@ -80,22 +80,106 @@ abstract class BudgetCatExpensePercentWidgetViews {
 }
 
 abstract class NychaBudgetCatExpensePercentWidgetViews {
-  const EXPCAT_PERCENT_DIFF = "percent_difference_by_expense_categories";
-  const EXPCAT_PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_expense_categories_previous_1";
-  const EXPCAT_PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_expense_categories_previous_2";
+  const PERCENT_DIFF = "percent_difference_by_expense_categories";
+  const PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_expense_categories_previous_1";
+  const PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_expense_categories_previous_2";
 
   public static function getCurrent() {
-    $base_year = NychaBudgetConstants::getCurrent();log_error($base_year);
+    $base_year = NychaBudgetConstants::getCurrent();;
     $year = RequestUtilities::get(UrlParameter::YEAR);
-log_error($year);
+
     if($year == $base_year + 1) {
-      return self::EXPCAT_PERCENT_DIFF_PREVIOUS_1;
+      return self::PERCENT_DIFF_PREVIOUS_1;
     }
-    else if($year == $base_year + 2) {log_error(self::EXPCAT_PERCENT_DIFF_PREVIOUS_2);
-      return self::EXPCAT_PERCENT_DIFF_PREVIOUS_2;
+    else if($year == $base_year + 2) {
+      return self::PERCENT_DIFF_PREVIOUS_2;
     }
     else if($year > $base_year) {
-      return self::EXPCAT_PERCENT_DIFF;
+      return self::PERCENT_DIFF;
+    }
+  }
+}
+
+abstract class NychaBudgetRespCentersWidgetViews {
+  const PERCENT_DIFF = "percent_difference_by_resp_center";
+  const PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_resp_center_previous_1";
+  const PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_resp_center_previous_2";
+
+  public static function getCurrent() {
+    $base_year = NychaBudgetConstants::getCurrent();;
+    $year = RequestUtilities::get(UrlParameter::YEAR);
+
+    if($year == $base_year + 1) {
+      return self::PERCENT_DIFF_PREVIOUS_1;
+    }
+    else if($year == $base_year + 2) {
+      return self::PERCENT_DIFF_PREVIOUS_2;
+    }
+    else if($year > $base_year) {
+      return self::PERCENT_DIFF;
+    }
+  }
+}
+
+abstract class NychaBudgetFundingSourcesWidgetViews {
+  const PERCENT_DIFF = "percent_difference_by_fundsrc";
+  const PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_fundsrc_previous_1";
+  const PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_fundsrc_previous_2";
+
+  public static function getCurrent() {
+    $base_year = NychaBudgetConstants::getCurrent();;
+    $year = RequestUtilities::get(UrlParameter::YEAR);
+
+    if($year == $base_year + 1) {
+      return self::PERCENT_DIFF_PREVIOUS_1;
+    }
+    else if($year == $base_year + 2) {
+      return self::PERCENT_DIFF_PREVIOUS_2;
+    }
+    else if($year > $base_year) {
+      return self::PERCENT_DIFF;
+    }
+  }
+}
+
+abstract class NychaBudgetProgramsWidgetViews {
+  const PERCENT_DIFF = "percent_difference_by_programs";
+  const PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_programs_previous_1";
+  const PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_programs_previous_2";
+
+  public static function getCurrent() {
+    $base_year = NychaBudgetConstants::getCurrent();;
+    $year = RequestUtilities::get(UrlParameter::YEAR);
+
+    if($year == $base_year + 1) {
+      return self::PERCENT_DIFF_PREVIOUS_1;
+    }
+    else if($year == $base_year + 2) {
+      return self::PERCENT_DIFF_PREVIOUS_2;
+    }
+    else if($year > $base_year) {
+      return self::PERCENT_DIFF;
+    }
+  }
+}
+
+abstract class NychaBudgetProjectsViews {
+  const PERCENT_DIFF = "percent_difference_by_projects";
+  const PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_projects_previous_1";
+  const PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_projects_previous_2";
+
+  public static function getCurrent() {
+    $base_year = NychaBudgetConstants::getCurrent();;
+    $year = RequestUtilities::get(UrlParameter::YEAR);
+
+    if($year == $base_year + 1) {
+      return self::PERCENT_DIFF_PREVIOUS_1;
+    }
+    else if($year == $base_year + 2) {
+      return self::PERCENT_DIFF_PREVIOUS_2;
+    }
+    else if($year > $base_year) {
+      return self::PERCENT_DIFF;
     }
   }
 }
