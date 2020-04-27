770c476ad222c253d0e5c8d0b07bcc9584f2d00b
Prashant Pillai <prashant.pillai@C02Z20WHLVDQ.local>
2020-04-27 16:46:01 -0400

    percent difference updates

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/BudgetConstants.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/BudgetConstants.php
@@ -7,6 +7,13 @@ abstract class BudgetConstants{
      }
 }
 
+abstract class NychaBudgetConstants{
+  const BASE_YEAR_ID = 119;
+  public static function getCurrent() {
+    return self::BASE_YEAR_ID;
+  }
+}
+
 abstract class BudgetAgencyPercentWidgetViews {
 
     const AGENCIES_PERCENT_DIFF = "percent_difference_by_agencies";
@@ -70,5 +77,26 @@ abstract class BudgetCatExpensePercentWidgetViews {
             return self::EXPCAT_PERCENT_DIFF;
         }
      }
+}
+
+abstract class NychaBudgetCatExpensePercentWidgetViews {
+  const EXPCAT_PERCENT_DIFF = "percent_difference_by_expense_categories";
+  const EXPCAT_PERCENT_DIFF_PREVIOUS_1 = "percent_difference_by_expense_categories_previous_1";
+  const EXPCAT_PERCENT_DIFF_PREVIOUS_2 = "percent_difference_by_expense_categories_previous_2";
+
+  public static function getCurrent() {
+    $base_year = NychaBudgetConstants::getCurrent();log_error($base_year);
+    $year = RequestUtilities::get(UrlParameter::YEAR);
+log_error($year);
+    if($year == $base_year + 1) {
+      return self::EXPCAT_PERCENT_DIFF_PREVIOUS_1;
+    }
+    else if($year == $base_year + 2) {log_error(self::EXPCAT_PERCENT_DIFF_PREVIOUS_2);
+      return self::EXPCAT_PERCENT_DIFF_PREVIOUS_2;
+    }
+    else if($year > $base_year) {
+      return self::EXPCAT_PERCENT_DIFF;
+    }
+  }
 }
 
