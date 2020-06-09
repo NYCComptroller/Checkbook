1f6b7523c235a58027ab306d13cf5184c09b0f8f
Kavitha G <kgopalakrishnan@resisystems.com>
2020-06-08 20:06:11 -0400

    feature - NYCCHKBK-9994- Nycha Revenue Landing Page widgets for 5 segments and revenue categories.

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaRevenue/NychaRevenueUrlService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaRevenue/NychaRevenueUrlService.php
@@ -3,15 +3,39 @@
 
 class NychaRevenueUrlService {
 
-    static function getFooterUrl($parameters,$legacy_node_id = null) {
-        $legacy_node_id = isset($legacy_node_id) ? '/dtsmnid/'.$legacy_node_id : '';
-        $url = '/panel_html/nycha_budget_transactions/nycha_revenue/transactions'
-            . $legacy_node_id
-            .RequestUtilities::buildUrlFromParam('agency')
-            .RequestUtilities::buildUrlFromParam('dept')
-            .RequestUtilities::buildUrlFromParam('expcategory')
-            . _checkbook_project_get_year_url_param_string();
-
-        return $url;
+    static function getFooterUrl($parameters = null) {
+      $url = "/panel_html/nycha_revenue_transactions/nycha_revenue/transactions"
+        . RequestUtilities::buildUrlFromParam('year')
+        . RequestUtilities::buildUrlFromParam('expcategory')
+        . RequestUtilities::buildUrlFromParam('project')
+        . RequestUtilities::buildUrlFromParam('program')
+        . RequestUtilities::buildUrlFromParam('fundsrc')
+        . RequestUtilities::buildUrlFromParam('respcenter')
+        . RequestUtilities::buildUrlFromParam('datasource');
+      return $url;
     }
+
+  /**
+   * Returns NYCHA Revenue Landing page URL
+   * @param $urlParamName
+   * @param $urlParamValue
+   * @param $yearId parameter
+   * @return string
+   */
+  static function generateLandingPageUrl($urlParamName, $urlParamValue, $yearId = null)
+  {
+    $yearId = (isset($yearId)) ? $yearId : RequestUtilities::getRequestParamValue('year');
+    $yearURL = '/year/'. ((isset($yearId)) ? $yearId : CheckbookDateUtil::getCurrentFiscalYearId(Datasource::NYCHA));
+    $url = '/nycha_revenue'
+      . $yearURL
+      . RequestUtilities::buildUrlFromParam('expcategory')
+      . RequestUtilities::buildUrlFromParam('project')
+      . RequestUtilities::buildUrlFromParam('program')
+      . RequestUtilities::buildUrlFromParam('fundsrc')
+      . RequestUtilities::buildUrlFromParam('respcenter')
+      . RequestUtilities::buildUrlFromParam('datasource')
+      . '/'.$urlParamName.'/'. $urlParamValue;
+
+    return $url;
+  }
 }
