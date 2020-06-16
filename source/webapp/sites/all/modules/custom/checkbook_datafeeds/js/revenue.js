85e83f3ed721e8b5c1421f47977fdad9f03bda56
Saroja Gade <sgade@reisystems.com>
2020-06-16 16:57:28 -0400

    NYCCHKBK-9971: Refactoring - Moved Revenue citywide data-feeds form to Drupal behaviors

--- source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/revenue.js
+++ source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/revenue.js
@@ -1,48 +1,55 @@
 (function ($) {
-    $(document).ready(function () {
-        // Sets up multi-select/option transfer
-        $('#edit-column-select').multiSelect();
-        $('.ms-selectable .ms-list').after('<a class="select">Add All</a>');
-        $('.ms-selection .ms-list').after('<a class="deselect">Remove All</a>');
-        $('a.select').click(function(){
-            $('#edit-column-select').multiSelect('select_all');
+  Drupal.behaviors.budgetDataFeeds = {
+    attach: function (context, settings) {
+      //DataSource Filter Formatter
+      $.fn.formatDatafeedsDatasourceRadio();
+
+      //Sets up jQuery UI autocompletes and autocomplete filtering functionality
+      let year = ($('#edit-fiscal-year', context).val() === 'All Years') ? 0 : $('#edit-fiscal-year', context).val();
+      let fundclass = emptyToZero($('#edit-fund-class', context).val());
+      let agency = emptyToZero($('#edit-agency', context).val());
+      let budgetyear = ($('#edit-budget-fiscal-year', context).val() === 'All Years') ? 0 : $('#edit-budget-fiscal-year', context).val();
+      let revcat = emptyToZero($('#edit-revenue-category', context).val());
+      let revclass = emptyToZero($('#edit-revenue-class', context).val());
+      let revsrc = emptyToZero($('#edit-revenue-source', context).val());
+      let fundingsrc = emptyToZero($('#edit-funding-class', context).val());
+      $('#edit-revenue-class', context).autocomplete({source:'/autocomplete/revenue/revenueclass/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revsrc + '/' + fundingsrc});
+      $('#edit-revenue-source', context).autocomplete({source:'/autocomplete/revenue/revenuesource/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revclass + '/' + fundingsrc});
+      $('.watch:input', context).each(function () {
+        $(this).focusin(function () {
+          year = ($('#edit-fiscal-year', context).val() === 'All Years') ? 0 : $('#edit-fiscal-year', context).val();
+          fundclass = emptyToZero($('#edit-fund-class', context).val());
+          agency = emptyToZero($('#edit-agency', context).val());
+          budgetyear = ($('#edit-budget-fiscal-year', context).val() === 'All Years') ? 0 : $('#edit-budget-fiscal-year', context).val();
+          revcat = emptyToZero($('#edit-revenue-category', context).val());
+          revclass = emptyToZero($('#edit-revenue-class', context).val());
+          revsrc = emptyToZero($('#edit-revenue-source', context).val());
+          fundingsrc = emptyToZero($('#edit-funding-class', context).val());
+          $("#edit-revenue-class").autocomplete("option", "source", '/autocomplete/revenue/revenueclass/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revsrc + '/' + fundingsrc);
+          $("#edit-revenue-source").autocomplete("option", "source", '/autocomplete/revenue/revenuesource/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revclass + '/' + fundingsrc);
         });
-        $('a.deselect').click(function(){
-            $('#edit-column-select').multiSelect('deselect_all');
-        });
-        //Sets up autocompletes and autocomplete filtering functionality
-        var year = ($('#edit-fiscal-year').val() === 'All Years') ? 0 : $('#edit-fiscal-year').val();
-        var fundclass = emptyToZero($('#edit-fund-class').val());
-        var agency = emptyToZero($('#edit-agency').val());
-        var budgetyear = ($('#edit-budget-fiscal-year').val() === 'All Years') ? 0 : $('#edit-budget-fiscal-year').val();
-        var revcat = emptyToZero($('#edit-revenue-category').val());
-        var revclass = emptyToZero($('#edit-revenue-class').val());
-        var revsrc = emptyToZero($('#edit-revenue-source').val());
-        var fundingsrc = emptyToZero($('#edit-funding-class').val());
-        $('#edit-revenue-class').autocomplete({source:'/autocomplete/revenue/revenueclass/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revsrc + '/' + fundingsrc});
-        $('#edit-revenue-source').autocomplete({source:'/autocomplete/revenue/revenuesource/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revclass + '/' + fundingsrc});
-        $('.watch:input').each(function () {
-            $(this).focusin(function () {
-                year = ($('#edit-fiscal-year').val() === 'All Years') ? 0 : $('#edit-fiscal-year').val();
-                fundclass = emptyToZero($('#edit-fund-class').val());
-                agency = emptyToZero($('#edit-agency').val());
-                budgetyear = ($('#edit-budget-fiscal-year').val() === 'All Years') ? 0 : $('#edit-budget-fiscal-year').val();
-                revcat = emptyToZero($('#edit-revenue-category').val());
-                revclass = emptyToZero($('#edit-revenue-class').val());
-                revsrc = emptyToZero($('#edit-revenue-source').val());
-                fundingsrc = emptyToZero($('#edit-funding-class').val());
-                $("#edit-revenue-class").autocomplete("option", "source", '/autocomplete/revenue/revenueclass/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revsrc + '/' + fundingsrc);
-                $("#edit-revenue-source").autocomplete("option", "source", '/autocomplete/revenue/revenuesource/' + year + '/' + fundclass + '/' + agency + '/' + budgetyear + '/' + revcat + '/' + revclass + '/' + fundingsrc);
-            });
-        });
-        //Function to retrieve values enclosed in brackets or return zero if none
-        function emptyToZero(input) {
-          const p = /\[(.*?)]$/;
-          const code = p.exec(input.trim());
-          if (code) {
-            return code[1];
-          }
-          return 0;
-        }
-    })
+      });
+
+      // Sets up multi-select/option transfer for CityWide
+      $('#edit-column-select',context).multiSelect();
+      $('#ms-edit-column-select .ms-selectable .ms-list',context).after('<a class="select">Add All</a>');
+      $('#ms-edit-column-select .ms-selection .ms-list',context).after('<a class="deselect">Remove All</a>');
+      $('#ms-edit-column-select a.select',context).click(function(){
+        $('#edit-column-select',context).multiSelect('select_all');
+      });
+      $('#ms-edit-column-select a.deselect',context).click(function(){
+        $('#edit-column-select',context).multiSelect('deselect_all');
+      });
+    }
+  }
+
+  //Function to retrieve values enclosed in brackets or return zero if none
+  function emptyToZero(input) {
+    const p = /\[(.*?)]$/;
+    const code = p.exec(input.trim());
+    if (code) {
+      return code[1];
+    }
+    return 0;
+  }
 }(jQuery));
