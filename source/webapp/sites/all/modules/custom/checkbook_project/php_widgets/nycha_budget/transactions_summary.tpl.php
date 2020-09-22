aad0e3a9206213bdfc7d6feaf02590baa95b05e2
Saroja Gade <sgade-reisys@users.noreply.github.com>
2020-09-22 14:05:25 -0400

    NYCCHKBK-10147: Use adopted amount for modified

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/nycha_budget/transactions_summary.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/nycha_budget/transactions_summary.tpl.php
@@ -41,7 +41,7 @@ echo $titleSummary;
 // Print amount values
 print '<div class="dollar-amounts">';
 print '<div class="total-spending-amount">' . custom_number_formatter_format($node->data[0]['budget_committed'],2,'$')."<div class='amount-title'>Total Committed<br />Expense Budget</div>".'</div>';
-print '<div class="total-spending-amount">' . custom_number_formatter_format($node->data[0]['budget_modified'],2,'$')."<div class='amount-title'>Total Modified<br />Expense Budget</div>".'</div>';
+print '<div class="total-spending-amount">' . custom_number_formatter_format($node->data[0]['budget_adopted_amount'],2,'$')."<div class='amount-title'>Total Modified<br />Expense Budget</div>".'</div>';
 print '<div class="total-spending-amount">' . custom_number_formatter_format($node->data[0]['budget_adopted_amount'],2,'$')."<div class='amount-title'>Total Adopted<br />Expense Budget</div>" .'</div>' ;
 print '<div class="total-spending-amount">' . custom_number_formatter_format($node->data[0]['budget_remaining'],2,'$')."<div class='amount-title'>Total Remaining<br />Expense Budget</div>" .'</div>' ;
 print '</div></div>';
