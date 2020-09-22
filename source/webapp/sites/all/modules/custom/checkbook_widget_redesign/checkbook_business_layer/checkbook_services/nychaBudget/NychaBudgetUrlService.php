ab1f00ff7b6bc2da4900bc9083acef71245d18ce
Saroja Gade <sgade-reisys@users.noreply.github.com>
2020-09-22 16:48:39 -0400

    NYCCHKBK-10149: Refactored NYCHA Budget URL service and Widget Service functions

--- source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaBudget/NychaBudgetUrlService.php
+++ source/webapp/sites/all/modules/custom/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/nychaBudget/NychaBudgetUrlService.php
@@ -48,12 +48,14 @@ class NychaBudgetUrlService {
 
   }
 
-  /* Gets the Committed budget link in a generic way
+  /**
+  * Gets the Committed budget link in a generic way
   * @param $dynamic_parameter - custom dynamic parameters to be used in the URL
-  * @param null $legacy_node_id
+  * @param  $widget
+  * @param $budgetype
   * @return string
   */
-  static function committedBudgetUrl($dynamic_parameter, $widget,$budgetype) {
+  static function committedBudgetUrl($dynamic_parameter, $widget, $budgetype) {
     $dynamic_parameter = isset($dynamic_parameter) ? $dynamic_parameter : '';
     $url = "/panel_html/nycha_budget_transactions/nycha_budget/transactions"
       . RequestUtilities::buildUrlFromParam('year')
@@ -70,63 +72,26 @@ class NychaBudgetUrlService {
     return $url;
   }
 
-  public static function expenseCategoryURL($expenditure_type_type){
-    $url =   "/nycha_budget"
-      .RequestUtilities::buildUrlFromParam('year')
-      .RequestUtilities::buildUrlFromParam('datasource')
-      .RequestUtilities::buildUrlFromParam('respcenter')
-      .RequestUtilities::buildUrlFromParam('fundsrc')
-      .RequestUtilities::buildUrlFromParam('program')
-      .RequestUtilities::buildUrlFromParam('project')
-      .'/expcategory/'.$expenditure_type_type;
-    return $url;
-  }
 
-  public static function responsibilityCenterURL($responsibilty_center_type){
-    $url =   "/nycha_budget"
+  /**
+   * Returns NYCHA Budget Landing page URL for the given Parameter
+   * @param $urlParamName
+   * @param $urlParamValue
+   * @return string
+   */
+  static function generateLandingPageUrl($urlParamName, $urlParamValue)
+  {
+    $url = '/nycha_budget'
       .RequestUtilities::buildUrlFromParam('year')
       .RequestUtilities::buildUrlFromParam('datasource')
-      .RequestUtilities::buildUrlFromParam('fundsrc')
-      .RequestUtilities::buildUrlFromParam('program')
-      .RequestUtilities::buildUrlFromParam('expcategory')
-      .RequestUtilities::buildUrlFromParam('project')
-      .'/respcenter/'.$responsibilty_center_type;
-    return $url;
-  }
-
-  public static function fundingSourceURL($funding_source_type){
-    $url =   "/nycha_budget"
-      .RequestUtilities::buildUrlFromParam('year')
-      .RequestUtilities::buildUrlFromParam('datasource')
-      .RequestUtilities::buildUrlFromParam('respcenter')
-      .RequestUtilities::buildUrlFromParam('program')
-      .RequestUtilities::buildUrlFromParam('expcategory')
-      .RequestUtilities::buildUrlFromParam('project')
-      .'/fundsrc/'.$funding_source_type;
-    return $url;
-  }
-
-  public static function programNameLink($program_phase_type){
-    $url =   "/nycha_budget"
-      .RequestUtilities::buildUrlFromParam('year')
-      .RequestUtilities::buildUrlFromParam('datasource')
-      .RequestUtilities::buildUrlFromParam('respcenter')
-      .RequestUtilities::buildUrlFromParam('fundsrc')
-      .RequestUtilities::buildUrlFromParam('expcategory')
-      .RequestUtilities::buildUrlFromParam('project')
-      .'/program/'.$program_phase_type;
-    return $url;
-  }
-
-  public static function projectNameLink($gl_project_type){
-    $url =   "/nycha_budget"
-      .RequestUtilities::buildUrlFromParam('year')
-      .RequestUtilities::buildUrlFromParam('datasource')
+      .RequestUtilities::buildUrlFromParam('agency')
       .RequestUtilities::buildUrlFromParam('respcenter')
       .RequestUtilities::buildUrlFromParam('fundsrc')
       .RequestUtilities::buildUrlFromParam('expcategory')
       .RequestUtilities::buildUrlFromParam('program')
-      .'/project/'.$gl_project_type;
+      .RequestUtilities::buildUrlFromParam('project')
+      . '/'.$urlParamName.'/'. $urlParamValue;
     return $url;
   }
+
 }
