f8341a4d6c49d267d1c297658936c54c763c75a5
Saroja Gade <sgade-reisys@users.noreply.github.com>
2020-11-04 19:39:33 -0500

    NYCCHKBK-9925: Cron setup for ETL Email notification

--- source/webapp/sites/all/modules/custom/checkbook_etl_notification/includes/CheckbookEtlStatistics.class.php
+++ source/webapp/sites/all/modules/custom/checkbook_etl_notification/includes/CheckbookEtlStatistics.class.php
@@ -30,6 +30,8 @@ class CheckbookEtlStatistics
    */
   const SUCCESS_IF_RUN_LESS_THAN_X_SECONDS_AGO = 60 * 60 * 12;
 
+  const CRON_LAST_RUN_DRUPAL_VAR = 'checkbook_etl_status_last_run';
+
   /**
    * @param $format
    * @return false|string
@@ -40,15 +42,6 @@ class CheckbookEtlStatistics
   }
 
   /**
-   * @param $environment
-   * @return array
-   */
-  public function get_contents($url)
-  {
-    return file_get_contents($url);
-  }
-
-  /**
    * @return int
    */
   public function timeNow()
@@ -56,13 +49,17 @@ class CheckbookEtlStatistics
     return time();
   }
 
-
+  /**
+   * @param $environment
+   * @return array
+   */
   public function getEtlStatistics($environment){
+    //Get ETL Statistics in the last 12 hours
     $sql = "SELECT *,
             CASE WHEN (process_abort_flag_yn = 'N' AND shard_refresh_flag_yn = 'Y' AND index_refresh_flag_yn = 'Y') 
                   THEN 'Success'
 	               ELSE 'Fail'
-            END AS status FROM jobs WHERE DATE(load_end_time) = CURRENT_DATE AND host_environment = '{$environment}'";
+            END AS status FROM jobs WHERE load_end_time >= (NOW() - INTERVAL '12 hours' ) AND host_environment = '{$environment}'";
     $results = _checkbook_project_execute_sql_by_data_source($sql, 'etl_statistics');
     $databases = array('checkbook' => 'Citywide', 'checkbook_ogent' => 'NYCEDC', 'checkbook_nycha' => 'NYCHA');
     $etlStatus = [];
@@ -95,7 +92,7 @@ class CheckbookEtlStatistics
 
   /**
    * @param $message
-   * @return bool
+   * @return string
    */
   public function gatherData(&$message)
   {
@@ -129,5 +126,75 @@ class CheckbookEtlStatistics
 
     return $message;
   }
+
+    /**
+     * @return bool
+     */
+    public function run_cron()
+    {
+      global $conf;
+
+      date_default_timezone_set('America/New_York');
+
+      //always run cron for developer
+      if (defined('CHECKBOOK_DEV')) {
+        return self::sendmail();
+      }
+      if (!isset($conf['checkbook_dev_group_email'])) {
+        //error_log("ETL STATUS MAIL CRON skips. Reason: \$conf['checkbook_dev_group_email'] not defined");
+        return false;
+      }
+
+      if (empty($conf['CHECKBOOK_ENV']) || !in_array($conf['CHECKBOOK_ENV'], ['UAT','PHPUNIT'])) {
+        // we run this cron only on UAT and PHPUNIT
+        return false;
+      }
+
+      $today = $this->get_date('Y-m-d');
+      $current_hour = (int)$this->get_date('H');
+
+      if (variable_get(self::CRON_LAST_RUN_DRUPAL_VAR) == $today) {
+        //error_log("ETL STATUS MAIL CRON skips. Reason: already ran today :: $today :: ".variable_get($variable_name));
+        return false;
+      }
+
+      if ($current_hour < 9 || $current_hour > 10) {
+        //error_log("ETL STATUS MAIL CRON skips. Reason: will run between 9 AM and 11 AM EST :: current hour: $current_hour");
+        return false;
+      }
+
+      variable_set(self::CRON_LAST_RUN_DRUPAL_VAR, $today);
+      //return $this->sendmail();
+    }
+
+    /**
+     * @return bool
+     */
+    public function sendmail()
+    {
+      global $conf;
+
+      $from = $conf['email_from'];
+
+      if (isset($conf['checkbook_dev_group_email'])){
+        $to_dev = $conf['checkbook_dev_group_email'];
+
+        try{
+          drupal_mail('checkbook_etl_notification', 'send-dev-status', $to_dev, null, ['dev_mode'=> true], $from);
+        } catch(Exception $ex1){
+          error_log($ex1->getMessage());
+        }
+      }
+
+      if (isset($conf['checkbook_ETL_emails'])) {
+        $to_client = $conf['checkbook_ETL_emails'];
+        try{
+          drupal_mail('checkbook_etl_notification', 'send-client-status', $to_client, null, ['dev_mode'=> false], $from);
+        } catch(Exception $ex2){
+          error_log($ex2->getMessage());
+        }
+      }
+      return true;
+    }
 
 }
