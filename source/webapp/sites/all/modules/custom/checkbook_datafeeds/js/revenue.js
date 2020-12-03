830ff1fd9b1a66d9496b489e79cc86649711ee25
Kavitha G <kgopalakrishnan@resisystems.com>
2020-12-03 14:09:09 -0500

    feature - NYCCHKBK - 10215 - Nycha revenue and budget (budget type and name facet autocomplete) and data reload in datafeeds and advanced search drop down.

--- source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/revenue.js
+++ source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/revenue.js
@@ -45,12 +45,12 @@
   }
 
   let reloadBudgetType = function(){
-    let budget_name = encodeURIComponent($('#edit-nycha-budget-name').val());
+    let budget_name = $('#edit-nycha-budget-name').val();
     let budget_type_hidden = $('input:hidden[name="nycha_budget_type_hidden"]').val();
     let data_source = 'checkbook_nycha';
 
     $.ajax({
-      url: 'data-feeds/revenue/budget_type/' + data_source + '/' + budget_name + '/'  + true,
+      url: 'data-feeds/revenue/budget_type/' + data_source + '/' + budget_name.toString().replace(/\//g, "__") + '/' + true,
       success: function(data) {
         let html = '<option value="" >Select Budget Type</option>';
         if(data[0]){
@@ -67,12 +67,12 @@
   }
 
   let reloadBudgetName = function(){
-    let budget_type = encodeURIComponent($('#edit-nycha-budget-type').val());
+    let budget_type = $('#edit-nycha-budget-type').val();
     let budget_name_hidden = $('input:hidden[name="nycha_budget_name_hidden"]').val();
     let data_source = 'checkbook_nycha';
 
     $.ajax({
-      url: 'data-feeds/revenue/budget_name/' + data_source + '/' + budget_type + '/'  + true,
+      url: 'data-feeds/revenue/budget_name/' + data_source + '/' +  budget_type.toString().replace(/\//g, "__") + '/' + true,
       success: function(data) {
         let html = '<option value="" >Select Budget Name</option>';
         if(data[0]){
