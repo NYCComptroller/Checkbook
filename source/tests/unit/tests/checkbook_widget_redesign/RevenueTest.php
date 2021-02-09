ac1f85a533354e63b3a8c5f66394caf45ec72cba
Kavitha G <kgopalakrishnan@resisystems.com>
2021-02-09 09:33:47 -0500

    feature - 10313- Revenue php unit test cases.

--- source/tests/unit/tests/checkbook_widget_redesign/RevenueTest.php
+++ source/tests/unit/tests/checkbook_widget_redesign/RevenueTest.php
@@ -1,7 +1,7 @@
 <?php
 include_once CUSTOM_MODULES_DIR . '/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/revenue/RevenueUrlService.php';
 include_once CUSTOM_MODULES_DIR . '/checkbook_widget_redesign/checkbook_infrastructure_layer/utilities/RequestUtilities.php';
-include_once CUSTOM_MODULES_DIR . '/checkbook_project/customclasses/constants/Constants.php';
+include_once CUSTOM_MODULES_DIR . '/checkbook_project/customclasses/util/CheckbookDateUtil.php';
 include_once CUSTOM_MODULES_DIR . '/checkbook_project/includes/checkbook_project.inc';
 
 use PHPUnit\Framework\TestCase;
