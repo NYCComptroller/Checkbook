234de822b66a9c6cbf7d880869554edecab28684
Nitish Rane <rane.nitish@gmail.com>
2021-02-09 01:44:21 -0800

    Add required PHP files

--- source/tests/unit/tests/checkbook_widget_redesign/ContractsUrlServiceTest.php
+++ source/tests/unit/tests/checkbook_widget_redesign/ContractsUrlServiceTest.php
@@ -4,6 +4,8 @@ include_once CUSTOM_MODULES_DIR . '/checkbook_widget_redesign/checkbook_business
 include_once CUSTOM_MODULES_DIR . '/checkbook_project/customclasses/constants/Constants.php';
 include_once CUSTOM_MODULES_DIR . '/checkbook_project/customclasses/RequestUtil.php';
 include_once CUSTOM_MODULES_DIR . '/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/ContractConstants.php';
+include_once CUSTOM_MODULES_DIR . '/checkbook_widget_redesign/checkbook_business_layer/checkbook_services/common/MinorityTypeService.php';
+include_once CUSTOM_MODULES_DIR . '/checkbook_widget_redesign/checkbook_infrastructure_layer/constants/CommonConstants.php';
 
 use PHPUnit\Framework\TestCase;
 
