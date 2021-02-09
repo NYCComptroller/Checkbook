0c6f2a95748313d005228744ffbfc1c30f608131
Nitish Rane <rane.nitish@gmail.com>
2021-02-08 22:06:24 -0800

    Rectify parameters

--- source/tests/unit/tests/checkbook_widget_redesign/ContractsUrlServiceTest.php
+++ source/tests/unit/tests/checkbook_widget_redesign/ContractsUrlServiceTest.php
@@ -17,7 +17,7 @@ class ContractsUrlServiceTest extends TestCase
     public function test_contractIdUrl(){
         $result = ContractsUrlService::contractIdUrl("4224034", "CT1");
         $this->assertEquals("/panel_html/contract_transactions/agid/4224034", substr($result, 0, 46));
-        $this->assertEquals("/doctype/CT1", substr($result, 0, 12));
+        $this->assertEquals("/doctype/CT1", substr($result, -12));
     }
 
     /**
@@ -26,7 +26,7 @@ class ContractsUrlServiceTest extends TestCase
     public function test_masterContractIdUrl(){
         $result = ContractsUrlService::masterContractIdUrl("5868332", "MMA1");
         $this->assertEquals("/panel_html/contract_transactions/contract_details/magid/5868332", substr($result, 0, 64));
-        $this->assertEquals("/doctype/MMA1", substr($result, 0, 13));
+        $this->assertEquals("/doctype/MMA1", substr($result, -13));
     }
 
     /**
@@ -34,13 +34,14 @@ class ContractsUrlServiceTest extends TestCase
      */
     public function test_pendingMasterContractIdUrl(){
         $result = ContractsUrlService::pendingMasterContractIdUrl("5868332", "MA1", NULL, NULL, NULL, NULL);
-        $this->assertEquals("/panel_html/contract_transactions/contract_details/magid/5868332", substr($result, 0, 64));
-        $this->assertEquals("/doctype/MA1", substr($result, 0, 12));
+        $this->assertEquals("/panel_html/contract_transactions/magid/5868332", substr($result, 0, 64));
+        $this->assertEquals("/doctype/MA1", substr($result, -12));
         
         $result = ContractsUrlService::pendingMasterContractIdUrl(NULL, NULL, "RCT184620218200842", NULL, "0", NULL);
         $this->assertEquals("/minipanels/pending_contract_transactions/contract/RCT184620218200842");
         $this->assertEquals("/version/0", substr($result, 0, 10));
     }   
+
 
 
 
