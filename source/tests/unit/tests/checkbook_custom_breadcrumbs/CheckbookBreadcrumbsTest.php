3957d1948769fc7b3c78582630ebe4de9d700869
Kavitha G <kgopalakrishnan@resisystems.com>
2021-04-05 17:13:28 -0400

    feature - 10313 - Breadcrumbs php unit test cases.

--- source/tests/unit/tests/checkbook_custom_breadcrumbs/CheckbookBreadcrumbsTest.php
+++ source/tests/unit/tests/checkbook_custom_breadcrumbs/CheckbookBreadcrumbsTest.php
@@ -16,6 +16,12 @@ use PHPUnit\Framework\TestCase;
  */
 class CustomBreadcrumbsTest extends TestCase
 {
+
+    function _getLastRequestParamValue($url)
+    {
+        return '5' . $url;
+    }
+
     /**
      * Test GetBudgetBreadcrumbTitle function
      */
@@ -98,10 +104,82 @@ class CustomBreadcrumbsTest extends TestCase
 
         $_REQUEST['expandBottomContURL'] = 'nycha_revenue/search/transactions/datasource/checkbook_nycha/agency/162/year/121';
         $_GET['q'] = 'nycha_revenue/search/transactions/datasource/checkbook_nycha/agency/162/year/121';
-        //unset($_REQUEST['expandBottomContURL']);
+
         $this->assertEquals('NYCHA RevenueTransactions', CustomBreadcrumbs::getNYCHARevenueBreadcrumbTitle());
 
         $_GET['q'] = 'nycha_revenue/datasource/checkbook_nycha/agency/162/year/121';
         $this->assertEquals(' Revenue Transactions', CustomBreadcrumbs::getNYCHARevenueBreadcrumbTitle());
+
+        unset($_REQUEST['expandBottomContURL']);
+    }
+
+
+    /**
+     * Test getSpendingBreadcrumbTitle function
+     *
+     */
+    public function testgetSpendingBreadcrumbTitle()
+    {
+        function  _get_spending_breadcrumb_title_drilldown()
+        {
+            return '5' ;
+        }
+
+        $_GET['q']  = 'spending_landing/yeartype/B/year/122';
+        $_REQUEST['expandBottomContURL'] = '/panel_html/spending_transactions/spending/transactions/yeartype/B/year/122/dtsmnid/22';
+        $this->assertEquals('getInitNodeSummaryTitle :: 22', CustomBreadcrumbs::getSpendingBreadcrumbTitle());
+
+        $_REQUEST['expandBottomContURL'] = '/panel_html/spending_transactions/spending/transactions/yeartype/B/year/122/expcategorycode/2110/smnid/22';
+        $this->assertEquals('getInitNodeSummaryTitle :: 22', CustomBreadcrumbs::getSpendingBreadcrumbTitle());
+        unset($_REQUEST['expandBottomContURL']);
+
+        $_GET['q'] = 'spending/search/transactions/';
+        $this->assertEquals('5  Total Spending', CustomBreadcrumbs::getSpendingBreadcrumbTitle());
+
+        $_GET['q'] = 'spending/search/transactions/datasource/checkbook_oge/agency/9000';
+        $this->assertEquals('5  Total Spending', CustomBreadcrumbs::getSpendingBreadcrumbTitle());
+
+    }
+
+    /**
+     * Test getNYCHASpendingBreadcrumbTitle function
+     *
+     */
+    public function testgetNYCHASpendingBreadcrumbTitle()
+    {
+        $_GET['q']  = 'nycha_spending/datasource/checkbook_nycha/year/121/agency/162';
+        $_REQUEST['expandBottomContURL'] = '/panel_html/nycha_spending_transactions/nycha_spending/transactions/year/121/agency/162/datasource/checkbook_nycha/widget/wt_vendors';
+        $this->assertEquals('Vendors Total Spending Transactions', CustomBreadcrumbs::getNYCHASpendingBreadcrumbTitle());
+
+        $_REQUEST['expandBottomContURL'] = '/panel_html/nycha_spending_transactions/nycha_spending/transactions/year/121/agency/162/datasource/checkbook_nycha/widget/ytd_resp_center/resp_center/178';
+        $this->assertEquals('Responsibility Center Total Spending Transactions', CustomBreadcrumbs::getNYCHASpendingBreadcrumbTitle());
+
+        unset($_REQUEST['expandBottomContURL']);
+        $_GET['q'] = 'nycha_spending/search/transactions/datasource/checkbook_nycha/agency/162/yeartype/B/year/122';
+        $this->assertEquals('NYCHA Total Spending Transactions', CustomBreadcrumbs::getNYCHASpendingBreadcrumbTitle());
+
+        $_GET['q'] = 'nycha_spending/year/122/agency/162/datasource/checkbook_nycha';
+        $this->assertEquals('New York City Housing Authority Total Spending', CustomBreadcrumbs::getNYCHASpendingBreadcrumbTitle());
+    }
+
+    /**
+     * Test getNYCHASpendingBreadcrumbTitle function
+     *
+     */
+    public function testgetNYCHAContractBreadcrumbTitle()
+    {
+        $_GET['q']  = 'nycha_contracts/datasource/checkbook_nycha/year/122/agency/162';
+        $_REQUEST['expandBottomContURL'] = '/panel_html/nycha_contracts_transactions_page/nycha_contracts/transactions/year/122/agency/162/datasource/checkbook_nycha/agreement_type/BA/tCode/BAM/modamt/0';
+        $this->assertEquals('Blanket Agreement Modifications Contracts Transactions', CustomBreadcrumbs::getNYCHAContractBreadcrumbTitle());
+
+        $_REQUEST['expandBottomContURL'] = 'nycha_spending/transactions/year/122/agency/162/datasource/checkbook_nycha/syear/122/widget/inv_contract/po_num_inv/BA1404493/agg_type/BA/tcode/BA/newwindow';
+        $this->assertEquals(' Contracts Transactions', CustomBreadcrumbs::getNYCHAContractBreadcrumbTitle());
+
+        unset($_REQUEST['expandBottomContURL']);
+        $_GET['q'] = '/nycha_contracts/all/transactions/agency/162/datasource/checkbook_nycha';
+        $this->assertEquals('NYCHA Contracts Transactions', CustomBreadcrumbs::getNYCHAContractBreadcrumbTitle());
+
+        $_GET['q'] = '/nycha_contracts/search/transactions/agency/162/yeartype/B/year/122/datasource/checkbook_nycha';
+        $this->assertEquals('NYCHA Contracts Transactions', CustomBreadcrumbs::getNYCHAContractBreadcrumbTitle());
     }
 }
