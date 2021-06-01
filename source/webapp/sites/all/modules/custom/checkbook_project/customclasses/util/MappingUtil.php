51ff14c9c21fbed1e39e530872cee992153d29db
Nitish Rane <rane.nitish@gmail.com>
2021-06-01 15:19:40 -0700

    Feature NYCCHKBK-10471 - Add conditions for native and emerging categories

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/MappingUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/MappingUtil.php
@@ -307,8 +307,13 @@ class MappingUtil {
                 $mwbe_filters_html .=  "<li class='no-click'><a href='/" . $active_domain_link . "/mwbe/3'>Hispanic American</a></li>";
             }
 
-              $mwbe_filters_html .=  "<li class='no-click'><a href='/" . $active_domain_link . "/mwbe/6'>Native</a></li>";
-              $mwbe_filters_html .=  "<li class='no-click'><a href='/" . $active_domain_link . "/mwbe/99'>Emerging</a></li>";
+            if(array_intersect($applicable_minority_types,array(6)) &&  ){
+                $mwbe_filters_html .=  "<li class='no-click'><a href='/" . $active_domain_link . "/mwbe/6'>Native</a></li>";
+            }
+    
+            if(array_intersect($applicable_minority_types,array(99))){
+                $mwbe_filters_html .=  "<li class='no-click'><a href='/" . $active_domain_link . "/mwbe/99'>Emerging</a></li>";
+            }
 
             if($mwbe_filters_html != "") {
               $mwbe_filters_html =  "<li class='no-click title'>M/WBE Category</li>" . $mwbe_filters_html;
