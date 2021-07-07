c3d55399736dbcd96c74d51011760e5e05849133
Saroja Gade <sgade-reisys@users.noreply.github.com>
2021-07-06 20:54:26 -0400

    NYCCHKBK-10563: Handle default value selection for drop-downs instead of first option always

--- source/webapp/sites/all/modules/custom/checkbook_advanced_search/includes/classes/Field.php
+++ source/webapp/sites/all/modules/custom/checkbook_advanced_search/includes/classes/Field.php
@@ -125,7 +125,9 @@ class Field
 
     public function getDropDownDefault()
     {
-        if (isset($this->options[0]) && !(is_null($this->options[0]))) {
+        if(isset($this->default_value)){
+          return $this->default_value;
+        }if (isset($this->options[0]) && !(is_null($this->options[0]))) {
             return $this->options[0];
         }
         else {
