25305a99bdb854bd0ec102c35ea5ac6358de053e
Kavitha G <kgopalakrishnan@resisystems.com>
2021-08-16 17:24:16 -0400

    feature - 10709- Using common constants for domain values.

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/contracts_navigation.tpl.php
@@ -67,7 +67,7 @@ if($has_vendor_parameter && (!$contracts_advanced_search && !$spending_advanced_
     }
 }
 
-if($spending_amount  == 0 || (($has_vendor_parameter && Dashboard::isMWBE()) && (!$contracts_advanced_search && !$spending_advanced_search && $current_domain != 'contracts'))){
+if($spending_amount  == 0 || (($has_vendor_parameter && Dashboard::isMWBE()) && (!$contracts_advanced_search && !$spending_advanced_search && $current_domain != Domain::$CONTRACTS))){
     $spending_amount = isset($spending_amount) ? $spending_amount:0;
     $spending_link =  l('<span class="nav-title">Spending</span><br>'. custom_number_formatter_format($spending_amount  ,1,'$'),'',$options_disabled);
 }else{
@@ -77,7 +77,7 @@ if($spending_amount  == 0 || (($has_vendor_parameter && Dashboard::isMWBE()) && 
 $current_dashboard = RequestUtilities::get("dashboard");
 
 if ($contract_amount > 0) {
-  if(($has_vendor_parameter && Dashboard::isMWBE()) && (!$contracts_advanced_search && !$spending_advanced_search) && $current_domain != 'spending') {
+  if(($has_vendor_parameter && Dashboard::isMWBE()) && (!$contracts_advanced_search && !$spending_advanced_search) && $current_domain != Domain::$SPENDING) {
     $contracts_link = l('<span class="nav-title">Contracts</span><br>' . custom_number_formatter_format($contract_amount, 1, '$'), RequestUtil::getTopNavURL("contracts"), $options_disabled);
   }
   else {
@@ -88,7 +88,7 @@ if ($contract_amount > 0) {
   $contracts_url = RequestUtil::getContractsBottomSliderPath();
   if(isset($contracts_url)){
     $contracts_url = RequestUtil::getTopNavURL("contracts", $contracts_url);
-    if(($has_vendor_parameter && Dashboard::isMWBE()) && (!$contracts_advanced_search && !$spending_advanced_search) && $current_domain != 'spending'){
+    if(($has_vendor_parameter && Dashboard::isMWBE()) && (!$contracts_advanced_search && !$spending_advanced_search) && $current_domain != Domain::$SPENDING){
       $contracts_link = l('<span class="nav-title">Contracts</span><br>' . custom_number_formatter_format(0, 1, '$'), $contracts_url, $options_disabled);
     }
     else {
