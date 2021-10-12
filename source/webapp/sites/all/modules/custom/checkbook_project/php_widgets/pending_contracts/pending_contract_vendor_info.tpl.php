de266a611930518a6bfcc154df5a32ccefa5f00e
Kavitha G <kgopalakrishnan@resisystems.com>
2021-10-12 15:19:37 -0400

    feature - 10912 - Mwbe category displayed twice in the contracts details page.

--- source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/pending_contracts/pending_contract_vendor_info.tpl.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/php_widgets/pending_contracts/pending_contract_vendor_info.tpl.php
@@ -102,7 +102,7 @@ if($node->data[0]["vendor_id"]){
           $ethnicities[] =MappingUtil::getMinorityCategoryById($minority_type_id);
         }
       }
-      $ethnicity = implode(',',$ethnicities);
+      $ethnicity = implode(',',array_unique($ethnicities));
      if($minority_type_id == "4" || $minority_type_id == "5"){
       $minority_type_id = "4~5";
        }
