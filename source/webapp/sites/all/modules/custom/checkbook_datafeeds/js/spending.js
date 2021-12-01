421c91abb59cf0a6bb7cc160274034e6ad9ed085
Saroja Gade <sgade-reisys@users.noreply.github.com>
2021-12-01 16:14:19 -0500

    NYCCHKBK-11018: Fixed JS syntax which breaks the functionality in IE browser

--- source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/spending.js
+++ source/webapp/sites/all/modules/custom/checkbook_datafeeds/js/spending.js
@@ -1,7 +1,7 @@
 (function ($) {
 
   //Helper function to remove "FY", and remove "~" if present from string
-  removeFY = (year) => {
+  let removeFY = function(year){
     return year.replace(/fy[~]*/ig,'').trim();
   }
 
