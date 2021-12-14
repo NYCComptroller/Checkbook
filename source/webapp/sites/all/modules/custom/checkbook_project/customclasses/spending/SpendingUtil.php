22e4e77ed6ac409a34149ebe04e85662561c10d6
Saroja Gade <sgade-reisys@users.noreply.github.com>
2021-12-14 10:41:02 -0500

    NYCCHKBK-11067: Updated title for Contract Spending transactions

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/spending/SpendingUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/spending/SpendingUtil.php
@@ -1135,13 +1135,16 @@ class SpendingUtil{
             $dashboard_title = MappingUtil::getCurrenEthnicityName();
         } elseif(($widgetTitle == "Contracts" || $widgetTitle == "Contract") && $category == 1) {//Contract Exception
           $catName = "Spending";
-        }else if(RequestUtilities::get('mocs') == 'Yes'){
+        }
+
+        if(RequestUtilities::get('mocs') == 'Yes'){
           if($category != 1) {
             return "MOCS Registered COVID-19 Contract " . $catName . " Transactions";
           }else{
             return "MOCS Registered COVID-19 Contract Spending Transactions";
           }
         }
+
         //Visualization - Sub Vendors (M/WBE) "Ethnicity" Exception
         elseif($smnid == "723" && $dashboard == "sp") {
             $dashboard_title = MappingUtil::getCurrenEthnicityName();
