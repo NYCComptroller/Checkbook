59fa45eac7fde9ab0e9b109865185dd5da54235f
sgade-reisys <sgade@reisystems.com>
2022-10-27 16:51:37 -0400

    NYCCHKBK-11954: Removed duplicate checks

--- source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/CheckbookDateUtil.php
+++ source/webapp/sites/all/modules/custom/checkbook_project/customclasses/util/CheckbookDateUtil.php
@@ -112,13 +112,12 @@ class CheckbookDateUtil{
       return $year;
     }
     $year = variable_get($key, FALSE);
-    isset($year) ? $year : null;
     if (FALSE === $year) {
       LogHelper::log_warn('Drush variable ' . $key . ' not found!');
       $year = self::$currentFiscalYear;
     }
     _checkbook_dmemcache_set($key, $year);
-    return isset($year) ? $year : null;
+    return $year ?? NULL;
   }
 
   /**
@@ -138,13 +137,12 @@ class CheckbookDateUtil{
       return $year;
     }
     $year = variable_get($key, FALSE);
-    isset($year) ? $year : null;
     if (FALSE === $year) {
       LogHelper::log_warn('Drush variable ' . $key . ' not found!');
       $year = self::$currentFiscalYear;
     }
     _checkbook_dmemcache_set($key, $year);
-    return isset($year) ? $year : null;
+    return $year ?? NULL;
   }
 
   /**
